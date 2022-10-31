4219dd1701d9b8b5e0dbebbc7aa73df896db8fab
notgne2 <gen2@gen2.space>
2022-10-31 12:51:49 -0700

    Supply wrapper env vars cleanly in Nix

--- shells/rust-env/default.nix
+++ shells/rust-env/default.nix
@@ -11,25 +11,23 @@
   hostPlatform,
   nix,
   rustPlatform,
-  cacert,
+  cargo,
+  rustc,
+  rust,
 }:
-mkShell {
-  inputsFrom = [self'.packages.flox-cli];
-  packages = [rustfmt clippy rust-analyzer];
-  shellHook = ''
-    ${self'.checks.pre-commit-check.shellHook}
-    # TODO factor out to share env vars with flox-cli
-    export NIX_BIN="${nix}/bin/nix"
-    export FLOX_SH="${flox}/libexec/flox/flox"
-    export NIXPKGS_CACERT_BUNDLE_CRT="${cacert}/etc/ssl/certs/ca-bundle.crt"
-    ${lib.optionalString hostPlatform.isLinux ''
-      export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
-    ''}
-    ${lib.optionalString hostPlatform.isDarwin ''
-      export NIX_COREFOUNDATION_RPATH="${darwin.CF}/Library/Frameworks"
-      export PATH_LOCALE="${darwin.locale}/share/locale"
-    ''}
-    # For use with rust-analyzer
-    export RUST_SRC_PATH="${rustPlatform.rustLibSrc}"
-  '';
-}
+mkShell ({
+    inputsFrom = [self'.packages.flox-cli];
+    RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+    packages = [
+      rustfmt
+      clippy
+      rust-analyzer
+      cargo
+      rustc
+      rust.packages.stable.rustPlatform.rustLibSrc
+    ];
+    shellHook = ''
+      ${self'.checks.pre-commit-check.shellHook}
+    '';
+  }
+  // self'.packages.flox-cli.envs)
