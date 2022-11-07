05710708a67f827baac68c994fa1589d787f905c
Yannik Sander <yannik@floxdev.com>
2022-11-07 12:51:43 +0100

    chore: configure flox instance

--- crates/flox-cli/src/main.rs
+++ crates/flox-cli/src/main.rs
@@ -34,7 +34,16 @@ mod commands {
     }
 
     impl FloxArgs {
-        pub async fn handle(&self) -> Result<()> {
+        /// Initialize the command line by creating an initial FloxBuilder
+        pub async fn handle(&self, config: crate::config::Config) -> Result<()> {
+            let flox = FloxBuilder::default()
+                .collect_metrics(config.flox.allow_telemetry)
+                .cache_dir(config.flox.cache_dir)
+                .data_dir(config.flox.data_dir)
+                .config_dir(config.flox.config_dir)
+                .extra_nix_args(self.nix_args.clone()) // TODO: consume Self?
+                .build()?;
+
             match self.command {
                 // Commands::Support(ref f) => f.run(self).await?,
                 // Commands::Build(ref f) => f.run(&self).await?,
