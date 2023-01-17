bc857d3d7555d6e090a4cc7ef41df546be05a817
Yannik Sander <yannik@floxdev.com>
2023-01-17 22:07:45 +0100

    refactor(cli): make init module folder (#173)
    * refactor(cli): make `init` module folder
    
    * refactor(cli): split init module into subsystem inits

---
+++ crates/flox/src/utils/init/logger.rs
@@ -0,0 +1,100 @@
+use log::{debug, error};
+use once_cell::sync::OnceCell;
+use tracing_subscriber::prelude::*;
+
+use crate::commands::Verbosity;
+use crate::utils::logger::{self, LogFormatter};
+use crate::utils::metrics::PosthogLayer;
+
+// If we use just `std::io::stderr()` I don't think we can make a valid type signature for LOGGER_HANDLE
+struct JustStderr;
+impl<'a> tracing_subscriber::fmt::MakeWriter<'a> for JustStderr {
+    type Writer = std::io::Stderr;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        std::io::stderr()
+    }
+}
+
+type LayerType = tracing_subscriber::layer::Layered<PosthogLayer, tracing_subscriber::Registry>;
+type ReloadHandle<T> = tracing_subscriber::reload::Handle<T, LayerType>;
+
+#[allow(clippy::type_complexity)]
+static LOGGER_HANDLE: OnceCell<(
+    ReloadHandle<tracing_subscriber::EnvFilter>,
+    ReloadHandle<
+        tracing_subscriber::fmt::Layer<
+            LayerType,
+            tracing_subscriber::fmt::format::DefaultFields,
+            LogFormatter,
+            JustStderr,
+        >,
+    >,
+)> = OnceCell::new();
+
+pub fn init_logger(verbosity: Option<Verbosity>, debug: Option<bool>) {
+    let verbosity = verbosity.unwrap_or_default();
+    let debug = debug.unwrap_or(false);
+
+    let log_filter = match (debug, verbosity) {
+        // Show only errors
+        (false, Verbosity::Quiet) => "off,flox=error",
+        // Show our own info logs
+        (false, Verbosity::Verbose(0)) => "off,flox=info",
+        // Also show POSIX debug
+        (false, Verbosity::Verbose(1)) => "off,flox=info,posix=debug",
+        // Also show info from our libraries
+        (false, Verbosity::Verbose(2)) => {
+            "off,flox=debug,flox-rust-sdk=info,runix=info,posix=debug"
+        },
+        // Also show debug from our libraries
+        (true, Verbosity::Quiet) | (false, Verbosity::Verbose(3)) => {
+            "off,flox=debug,flox-rust-sdk=debug,runix=debug,posix=debug"
+        },
+        // Also show debug from everything
+        (true, Verbosity::Verbose(0)) | (false, Verbosity::Verbose(4)) => "debug",
+        // Also show trace from everything
+        (true, Verbosity::Verbose(_)) | (false, Verbosity::Verbose(_)) => "trace",
+    };
+
+    let (filter_handle, fmt_handle) = LOGGER_HANDLE.get_or_init(|| {
+        debug!("Initializing logger (how are you seeing this?)");
+
+        let filter = tracing_subscriber::filter::EnvFilter::try_from_default_env()
+            .or_else(|_| tracing_subscriber::filter::EnvFilter::try_new(log_filter))
+            .unwrap();
+        let (filter_reloadable, filter_reload_handle) =
+            tracing_subscriber::reload::Layer::new(filter);
+
+        let fmt = tracing_subscriber::fmt::layer()
+            .with_writer(JustStderr)
+            .event_format(logger::LogFormatter { debug });
+
+        let (fmt_reloadable, fmt_reload_handle) = tracing_subscriber::reload::Layer::new(fmt);
+
+        let fmt_filtered = fmt_reloadable.with_filter(filter_reloadable);
+
+        tracing_subscriber::registry()
+            .with(PosthogLayer::new())
+            .with(fmt_filtered)
+            .init();
+
+        (filter_reload_handle, fmt_reload_handle)
+    });
+
+    if let Err(err) = filter_handle.modify(|layer| {
+        *layer = tracing_subscriber::filter::EnvFilter::try_from_default_env()
+            .or_else(|_| tracing_subscriber::filter::EnvFilter::try_new(log_filter))
+            .unwrap();
+    }) {
+        error!("Updating logger filter failed: {}", err);
+    }
+
+    if let Err(err) = fmt_handle.modify(|layer| {
+        *layer = tracing_subscriber::fmt::layer()
+            .with_writer(JustStderr)
+            .event_format(logger::LogFormatter { debug });
+    }) {
+        error!("Updating logger filter failed: {}", err);
+    }
+}
---
+++ crates/flox/src/utils/init/metrics.rs
@@ -0,0 +1,117 @@
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use crossterm::tty::IsTty;
+use fslock::LockFile;
+use indoc::indoc;
+use log::{debug, info, trace};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+use crate::utils::dialog::InquireExt;
+use crate::utils::metrics::{METRICS_LOCK_FILE_NAME, METRICS_UUID_FILE_NAME};
+
+async fn write_metrics_uuid(uuid_path: &Path, consent: bool) -> Result<()> {
+    let mut file = tokio::fs::File::create(&uuid_path).await?;
+    if consent {
+        let uuid = uuid::Uuid::new_v4();
+        file.write_all(uuid.to_string().as_bytes()).await?;
+    }
+    Ok(())
+}
+
+pub async fn init_telemetry_consent(data_dir: &Path, cache_dir: &Path) -> Result<()> {
+    tokio::fs::create_dir_all(data_dir).await?;
+
+    if !std::io::stderr().is_tty() || !std::io::stdin().is_tty() {
+        // Can't prompt user now, do it another time
+        return Ok(());
+    }
+
+    let mut metrics_lock = LockFile::open(&cache_dir.join(METRICS_LOCK_FILE_NAME))?;
+    tokio::task::spawn_blocking(move || metrics_lock.lock()).await??;
+
+    let uuid_path = data_dir.join(METRICS_UUID_FILE_NAME);
+
+    match tokio::fs::File::open(&uuid_path).await {
+        Ok(_) => return Ok(()),
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {},
+            _ => return Err(err.into()),
+        },
+    }
+
+    debug!("Metrics consent not recorded");
+
+    let bash_flox_dirs =
+        xdg::BaseDirectories::with_prefix("flox").context("Unable to find config dir")?;
+    let bash_user_meta_path = bash_flox_dirs.get_config_home().join("floxUserMeta.json");
+
+    if let Ok(mut file) = tokio::fs::File::open(&bash_user_meta_path).await {
+        trace!("Attempting to extract metrics consent value from bash flox");
+
+        let mut bash_user_meta_json = String::new();
+        file.read_to_string(&mut bash_user_meta_json).await?;
+
+        let json: serde_json::Value = serde_json::from_str(&bash_user_meta_json)?;
+
+        if let Some(x) = json["floxMetricsConsent"].as_u64() {
+            debug!("Using metrics consent value from bash flox");
+            write_metrics_uuid(&uuid_path, x == 1).await?;
+            return Ok(());
+        }
+    }
+
+    trace!("Prompting user for metrics consent");
+
+    let consent = inquire::Confirm::new("Do you consent to the collection of basic usage metrics?")
+        .with_help_message(indoc! {"
+            flox collects basic usage metrics in order to improve the user experience,
+            including a record of the subcommand invoked along with a unique token.
+            It does not collect any personal information."})
+        .with_flox_theme()
+        .prompt()?;
+
+    if consent {
+        write_metrics_uuid(&uuid_path, true).await?;
+        info!("\nThank you for helping to improve flox!\n");
+    } else {
+        let _consent_refusal =
+            inquire::Confirm::new("Can we log your refusal?")
+                .with_help_message("Doing this helps us keep track of our user count, it would just be a single anonymous request")
+                .with_flox_theme()
+                .prompt()?;
+
+        // TODO log if Refuse
+
+        write_metrics_uuid(&uuid_path, false).await?;
+        info!("\nUnderstood. If you change your mind you can change your election\nat any time with the following command: flox reset-metrics\n");
+    }
+
+    Ok(())
+}
+
+pub async fn init_uuid(data_dir: &Path) -> Result<uuid::Uuid> {
+    tokio::fs::create_dir_all(data_dir).await?;
+
+    let uuid_file_path = data_dir.join("uuid");
+
+    match tokio::fs::File::open(&uuid_file_path).await {
+        Ok(mut uuid_file) => {
+            debug!("Reading uuid from file");
+            let mut uuid_str = String::new();
+            uuid_file.read_to_string(&mut uuid_str).await?;
+            Ok(uuid::Uuid::try_parse(&uuid_str)?)
+        },
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                debug!("Creating new uuid");
+                let uuid = uuid::Uuid::new_v4();
+                let mut file = tokio::fs::File::create(&uuid_file_path).await?;
+                file.write_all(uuid.to_string().as_bytes()).await?;
+
+                Ok(uuid)
+            },
+            _ => Err(err.into()),
+        },
+    }
+}
