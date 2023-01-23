26e64b0278f23fe06e03baf3df5141619f721552
Yannik Sander <yannik@floxdev.com>
2023-01-23 15:23:53 +0100

    ci: add ci shell

---
+++ shells/ci/default.nix
@@ -0,0 +1,14 @@
+{
+  mkShell,
+  self,
+
+  # commitizen,
+}:
+mkShell ({
+    inputsFrom = [];
+    packages = [
+      # temporary until https://github.com/commitizen-tools/commitizen/pull/644 is merged
+      self.checks.pre-commit-check.passthru.commitizen
+      self.packages.flox.passthru.rustPlatform.rust.cargo
+    ];
+  })
