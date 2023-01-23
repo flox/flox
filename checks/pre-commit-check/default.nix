1099cdc5e32f98c87b397ca49087632ef1e93f37
Yannik Sander <yannik@floxdev.com>
2023-01-23 14:35:26 +0100

    ci: fix commitizen commit ref

--- checks/pre-commit-check/default.nix
+++ checks/pre-commit-check/default.nix
@@ -4,13 +4,14 @@
   lib,
 }: let
   # temporary, until https://github.com/commitizen-tools/commitizen/pull/644 is merged
-  commitizen = nixpkgs.commitizen.overridePythonAttrs (_: {
+  commitizen = nixpkgs.commitizen.overridePythonAttrs (old: {
     doCheck = false;
     src = builtins.fetchGit {
       url = "https://github.com/skoef/commitizen/";
       ref = "add-hooks-for-bump-command";
       rev = "26b38beb8d507e4a4ee3c062639a96230c33dd92";
     };
+    meta = (old.meta or {}) // {mainProgram = "cz";};
   });
 
   rustfmt = nixpkgs.rustfmt.override {asNightly = true;};
