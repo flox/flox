e632de240ec40a8e09993710ea1b78af1dc4e9db
Tom Bereknyei <tomberek@gmail.com>
2023-01-24 13:39:02 -0500

    fix: use SSH to ensure rev count is available

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Floxpkgs/Project Template";
   nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
-  inputs.flox-bash.url = "github:flox/flox-bash";
+  inputs.flox-bash.url = "git+ssh://git@github.com/flox/flox-bash";
   inputs.nixpkgs.url = "dummy";
   inputs.nixpkgs.follows = "flox-floxpkgs/nixpkgs/nixpkgs";
 
