b404fc93776dac82eaec11caf390dafc9ca50d16
Tom Bereknyei <tomberek@gmail.com>
2023-01-27 14:15:12 -0500

    fix: re-arrange nixpkgs dep

--- flake.nix
+++ flake.nix
@@ -3,16 +3,10 @@
   nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
   inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
   inputs.flox-bash.url = "git+ssh://git@github.com/flox/flox-bash";
-  inputs.nixpkgs.url = "dummy";
-  inputs.nixpkgs.follows = "flox-floxpkgs/nixpkgs/nixpkgs";
 
   # Declaration of external resources
   # =================================
-  inputs.shellHooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.nixpkgs-stable.follows = "nixpkgs";
-  };
+  inputs.shellHooks.url = "github:cachix/pre-commit-hooks.nix";
   # =================================
 
   outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
