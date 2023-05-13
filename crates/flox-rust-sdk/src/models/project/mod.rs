e4929aa5cafa7898db9f44adaa12bacebacb8c60
Matthew Kenigsberg <matthew@floxdev.com>
2023-05-12 18:59:43 -0600

    feat: add support for shells directory

--- crates/flox-rust-sdk/src/models/project/mod.rs
+++ crates/flox-rust-sdk/src/models/project/mod.rs
@@ -231,36 +231,38 @@ impl<'flox, Git: GitProvider, Access: GitAccess<Git>> Project<'flox, Git, Access
         .await
         .map_err(InitFloxPackageError::NixInit)?;
 
-        let old_proto_pkg_path = root.join("pkgs").join(PACKAGE_NAME_PLACEHOLDER);
+        // Comment this out since we're using mkShell instead of
+        // root-level flox.nix
+        // TODO: really find a better way to not hardcode this
+        // if template.to_string() == "flake:flox#.\"templates\".\"project\"" {
+        //     repo.add(&[&root.join("flox.nix")])
+        //         .await
+        //         .map_err(InitFloxPackageError::GitAdd)?;
+        // }
+        for dir_name in ["pkgs", "shells"] {
+            let old_path = root.join(dir_name).join(PACKAGE_NAME_PLACEHOLDER);
+            if old_path.exists() {
+                let new_path = root.join(dir_name).join(name);
 
-        if !old_proto_pkg_path.exists() {
-            // TODO: really find a better way to not hardcode this
-            if template.to_string() == "flake:flox#.\"templates\".\"project\"" {
-                repo.add(&[&root.join("flox.nix")])
+                repo.mv(&old_path, &new_path)
+                    .await
+                    .map_err(InitFloxPackageError::GitMv)?;
+                info!(
+                    "moved: {} -> {}",
+                    old_path.to_string_lossy(),
+                    new_path.to_string_lossy()
+                );
+
+                // our minimal "templating" - Replace any occurrences of
+                // PACKAGE_NAME_PLACEHOLDER with name
+                find_and_replace(&new_path, PACKAGE_NAME_PLACEHOLDER, name)
+                    .await
+                    .map_err(InitFloxPackageError::<Nix, Git>::ReplacePackageName)?;
+
+                repo.add(&[&new_path])
                     .await
                     .map_err(InitFloxPackageError::GitAdd)?;
             }
-        } else {
-            let new_proto_pkg_path = root.join("pkgs").join(name);
-
-            repo.mv(&old_proto_pkg_path, &new_proto_pkg_path)
-                .await
-                .map_err(InitFloxPackageError::GitMv)?;
-            info!(
-                "moved: {} -> {}",
-                old_proto_pkg_path.to_string_lossy(),
-                new_proto_pkg_path.to_string_lossy()
-            );
-
-            // our minimal "templating" - Replace any occurrences of
-            // PACKAGE_NAME_PLACEHOLDER with name
-            find_and_replace(&new_proto_pkg_path, PACKAGE_NAME_PLACEHOLDER, name)
-                .await
-                .map_err(InitFloxPackageError::<Nix, Git>::ReplacePackageName)?;
-
-            repo.add(&[&new_proto_pkg_path])
-                .await
-                .map_err(InitFloxPackageError::GitAdd)?;
         }
 
         Ok(())
