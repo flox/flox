c0b70aed92fabef11edf840db5194e03de70a4a6
Michael Brantley <brantley@floxdev.com>
2023-05-27 18:43:29 +0100

    fix: disable inputs.nixpkgs.follows in flake.nix
    The statement was breaking builds of the floxpkgs-internal pkgset,
    and is not necessary when building with flox.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
     owner = "aakropotkin";
     repo = "floco";
     rev = "e1231f054258f7d62652109725881767765b1efb";
-    inputs.nixpkgs.follows = "/flox-floxpkgs/nixpkgs";
+    # MFB: commented 20230527, breaks the floxpkgs-internal pkgset.
+    # inputs.nixpkgs.follows = "/flox-floxpkgs/nixpkgs";
   };
 
   outputs = args @ {flox-floxpkgs, ...}: flox-floxpkgs.project args (_: {});
