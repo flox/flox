a0c6e4caf1d400c65d4655d4b4c292d1293cbc6b
Tom Bereknyei <tomberek@gmail.com>
2023-06-01 17:42:26 -0400

    fix: sanitize source to minimize rebuild

---
+++ pkgs/flox-src/default.nix
@@ -0,0 +1,14 @@
+{self}:
+builtins.path {
+  name = "flox-src";
+  path = self;
+  filter = path: type:
+    ! builtins.elem path [
+      (self.outPath + "/flake.nix")
+      (self.outPath + "/flake.lock")
+      (self.outPath + "/pkgs")
+      (self.outPath + "/checks")
+      (self.outPath + "/tests")
+      (self.outPath + "/shells")
+    ];
+}
