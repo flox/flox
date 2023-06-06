flox/flox

commit 7d16e9bb63646f2afec45f05e0422231ef7a630c

fix: don't attempt to print upgrades for flakes (#74)
flox list --json currently fails with
ERROR: encountered object of type array
when the environment contains a package from a flake.

Instead:

- use publish_element as a marker for whether a package is from a
catalog or flake
- change recursion to emit empty rather than error when a flake is
encountered

flox-bash/lib/diff-catalogs.jq
@@ -26,16 +26,19 @@ $c2[0] as $b |
 #               ...
 # }
 def isPackage:
-  if ((type == "object") | not) then (
-    # Uh oh ... something gone wrong, not a dict.
-    "ERROR: encountered object of type \(type)" | halt_error(1)
+  if (type != "object") then (
+    false
   )
   elif (has("type") and .["type"] == "catalogRender") then (
     true
   )
   # XXX TEMPORARY transition code while we wait for catalogs
   # to be rewritten in latest format with "type" field.
-  elif (has("element") and has("eval")) then (
+  # Require publish_element which will be present for packages from catalogs but
+  # not flakes. Admittedly that's a bit hacky and at some point perhaps we
+  # should support upgrades for flake packages, but for now only support them
+  # for packages from catalogs.
+  elif (has("element") and has("eval") and has("publish_element")) then (
     true
   )
   else (
@@ -44,12 +47,16 @@ def isPackage:
 
 def packagePNames(keys):
   # Recurse, adding to keys as we go.
-  to_entries | map(
-    (keys + [.key]) as $newkeys |
-    if (.value | isPackage) then keys else (
-      .value | packagePNames($newkeys)[]
-    ) end
-  );
+  if (type == "object") then (
+    to_entries | map(
+      (keys + [.key]) as $newkeys |
+      if (.value | isPackage) then keys else (
+          .value | packagePNames($newkeys)[]
+      ) end
+    )
+  ) else (
+    empty
+  ) end;
 
 def packagePaths(catalog):
   catalog | to_entries | map(

commit 6b1b92b0ad0b7eec426033752af36d0bd84e21d5

fix: fix catalog parsing for flake upgrades
This commit partially reverts commit 7d16e9bb which broke the ability
to detect available catalog entry upgrades and fixes the ability to
detect available flake upgrades. It is being rolled out as a hotfix
to flox-prerelease as this is currently breaking the functionality
of `flox upgrade` in the majority of cases.

flox-bash/lib/diff-catalogs.jq
@@ -12,65 +12,88 @@ $c2[0] as $b |
 # Identify package paths in catalog by recursing through structure and
 # popping the traversed path whenever encountering an attibute:
 #
-# - at least 4 deep (to account for channel, system, and stability), and
-# - containing the attribute: "type": "catalogRender"
-#
-# Example structure:
+# - at least 3 deep (to account for channel and system), and
+# - containing the package attributes build, element, eval and version
+
+# Example catalog element:
 # {
 #   "nixpkgs-flox": {     # channel
 #     "aarch64-darwin": { # system
 #       "stable": {       # stability
 #         "xorg": {       # pname (part 1 of n)
-#           "xeyes": {    # pname (part 2 of n), can be more
+#           "xeyes": {    # pname (part 2 of n, can be more)
 #             "latest": { # catalogVersion
-#               ...
+#               "build": [ ... ],
+#               "cache": [ ... ],
+#               "element": { ... },
+#               "eval": { ... },
+#               "publish_element": { ... },
+#               "source": { ... },
+#               "version": 1
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
 # }
-def isPackage:
-  if (type != "object") then (
-    false
-  )
-  elif (has("type") and .["type"] == "catalogRender") then (
-    true
-  )
-  # XXX TEMPORARY transition code while we wait for catalogs
-  # to be rewritten in latest format with "type" field.
-  # Require publish_element which will be present for packages from catalogs but
-  # not flakes. Admittedly that's a bit hacky and at some point perhaps we
-  # should support upgrades for flake packages, but for now only support them
-  # for packages from catalogs.
-  elif (has("element") and has("eval") and has("publish_element")) then (
-    true
-  )
-  else (
-    false
-  ) end;
+def isCatalogPackage:
+  (
+    has("build") and has("element") and has("eval") and has("version") and
+    has("cache") and has("publish_element") and has("source")
+  ) or (
+    # TODO: remove, "type" is deprecated?
+    has("type") and .["type"] == "catalogRender"
+  );
+
+# Example flake element:
+# {
+#   "nixpkgs": {          # channel
+#     "x86_64-linux": {   # system
+#       "xorg": {         # pname (part 1 of n)
+#         "xeyes": {      # pname (part 2 of n, can be more)
+#           "build": [ ... ],
+#           "element": [ ... ],
+#           "eval": [ ... ],
+#           "version": 1
+#         }
+#       }
+#     }
+#   }
+# }
+#
+def isFlakePackage:
+  has("build") and has("element") and has("eval") and has("version");
 
 def packagePNames(keys):
   # Recurse, adding to keys as we go.
-  if (type == "object") then (
-    to_entries | map(
-      (keys + [.key]) as $newkeys |
-      if (.value | isPackage) then keys else (
-          .value | packagePNames($newkeys)[]
-      ) end
+  to_entries | map(
+    (keys + [.key]) as $newkeys |
+    if ((.value | type) != "object") then (
+      # Uh oh ... something gone wrong, not a dict.
+      "ERROR: encountered object of type \(type)" | halt_error(1)
     )
-  ) else (
-    empty
-  ) end;
+    elif (.value | isCatalogPackage) then
+      # Don't return the final catalogVersion key (usually "latest").
+      keys
+    elif (.value | isFlakePackage) then
+      # Return all keys including the last one.
+      $newkeys
+    else (
+      .value | packagePNames($newkeys)[]
+    ) end
+  );
 
 def packagePaths(catalog):
   catalog | to_entries | map(
     .key as $channel |
     .value | to_entries | map(
       .key as $system |
-      .value | to_entries | map(
-        .key as $stability |
-        .value | packagePNames([]) | map(
-          [ $channel, $system, $stability ] + . | flatten
-        )
+      .value | packagePNames([]) | map(
+        [ $channel, $system ] + . | flatten
       )
     )
-  ) | flatten(3);
+  ) | flatten(2);
 
 # Identify package paths in each catalog.
 packagePaths($a) as $a_paths |