accef93d52a7bd1348ea1814f303df6acfbc42d6
Michael Brantley <brantley@floxdev.com>
2023-06-14 14:05:14 +0100

    feat: source floxEnv `etc/profile` upon activation
    This commit updates the default floxEnv template to enable logic to
    source the etc/profile file if it exists within the flox environment,
    and adds instructions for installing the "etc-profiles" package that
    provides the language-specific modules which drive the process of
    configuring the environment.

--- flox-bash/lib/templateFloxEnv/pkgs/default/flox.nix
+++ flox-bash/lib/templateFloxEnv/pkgs/default/flox.nix
@@ -4,16 +4,31 @@
   # packages.nixpkgs-flox.figlet = {};
   # packages.nixpkgs-flox.bat = { version = "0.22.1"; };
 
+  # Activation Extensions
+  # Provides an extensible `<env>/etc/profile` script.
+  # packages.flox.etc-profiles = {
+  #   # All language packs are installed by default, but you can also
+  #   # select individual packs by uncommenting the line below.
+  #   # Invoke `flox search -c flox etc-profiles -l` to see
+  #   # a list of all supported language pack outputs.
+  #   # Please note that all/most language packs depend on including
+  #   # the "base" and "common_paths" output.
+  #   # meta.outputsToInstall = [ "base" "common_paths" "python3" ];
+  # };
+
   # Aliases available when environment is active
   # shell.aliases.cat = "bat";
 
   # Script run upon environment activation
   # Warning: Be careful when using `${}` in shell hook.
-  #          Due to conflicts with Nix language you have to escape it with '' (two single quotes)
+  #          Due to conflicts with Nix language you have to
+  #          first escape it with '' (two single quotes).
   #          Example: ` ''${ENV_VARIABLE} `
-  # shell.hook = ''
-  #   echo Flox Environment | figlet
-  # '';
+  shell.hook = ''
+    # Source `<env>/etc/profile` if it exists.
+    [ -r "$FLOX_ENV/etc/profile" ] && . "$FLOX_ENV/etc/profile";
+    # echo Flox Environment | figlet
+  '';
 
   # Environment variables
   # environmentVariables.LANG = "en_US.UTF-8";
