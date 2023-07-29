3141346e9cfd9ce47740bd5fcb8115e7ba1ff63d
Michael Brantley <brantley@floxdev.com>
2023-07-29 07:05:03 +0100

    chore: split flox-gh package out into its own derivation

--- pkgs/flox-bash/default.nix
+++ pkgs/flox-bash/default.nix
@@ -12,6 +12,7 @@
   diffutils,
   expect,
   findutils,
+  flox-gh,
   gawk,
   gh,
   gnugrep,
@@ -86,20 +87,6 @@
   # read commitizen config file as the single source of version
   czToml = lib.importTOML (flox-src + "/.cz.toml");
 
-  flox-gh = gh.overrideAttrs (oldAttrs: {
-    pname = "flox-${oldAttrs.pname}";
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ makeWrapper ];
-    patches = (oldAttrs.patches or []) ++ [ ./flox-gh.patch ];
-    postInstall = ''
-      mv $out/bin/gh $out/bin/flox-gh
-      wrapProgram $out/bin/flox-gh \
-        --run '# This script should only be invoked by flox with $FLOX_*_HOME defined.' \
-        --run 'set -eu' \
-        --run 'export XDG_CONFIG_HOME="$FLOX_CONFIG_HOME"' \
-        --run 'export XDG_STATE_HOME="$FLOX_STATE_HOME"' \
-        --run 'export XDG_DATA_HOME="$FLOX_DATA_HOME"'
-    '';
-  });
 in
   stdenv.mkDerivation rec {
     pname = "flox-bash";
---
+++ pkgs/flox-gh/default.nix
@@ -0,0 +1,14 @@
+{ gh, makeWrapper }: gh.overrideAttrs (oldAttrs: {
+    pname = "flox-${oldAttrs.pname}";
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ makeWrapper ];
+    patches = (oldAttrs.patches or []) ++ [ ./flox-gh.patch ];
+    postInstall = ''
+      mv $out/bin/gh $out/bin/flox-gh
+      wrapProgram $out/bin/flox-gh \
+        --run '# This script should only be invoked by flox with $FLOX_*_HOME defined.' \
+        --run 'set -eu' \
+        --run 'export XDG_CONFIG_HOME="$FLOX_CONFIG_HOME"' \
+        --run 'export XDG_STATE_HOME="$FLOX_STATE_HOME"' \
+        --run 'export XDG_DATA_HOME="$FLOX_DATA_HOME"'
+    '';
+  })
