ca98b719c30cb7fa4c4f483d4dd4c371f41407fb
Matthew Kenigsberg <matthew@floxdev.com>
2023-08-01 18:21:30 -0600

    chore: test floxpkgs branch

--- flox-bash/lib/templateFloxEnv/flake.nix
+++ flox-bash/lib/templateFloxEnv/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "flox environment";
-  inputs.flox-floxpkgs.url = "github:flox/floxpkgs";
+  inputs.flox-floxpkgs.url = "github:flox/floxpkgs?ref=pure-store-path";
 
   outputs = args @ {flox-floxpkgs, ...}:
     flox-floxpkgs.project args ({self, ...}: {});
