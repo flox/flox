0a1aa2639fcdc9809c6681b43c59ee231bf903f0
Zach Mitchell <zmitchell@fastmail.com>
2023-10-23 11:45:46 -0600

    feat: add decode method

--- crates/flox-rust-sdk/src/models/environment/managed_environment.rs
+++ crates/flox-rust-sdk/src/models/environment/managed_environment.rs
@@ -22,7 +22,7 @@ const GENERATION_LOCK_FILENAME: &str = "env.lock";
 
 #[derive(Debug)]
 pub struct ManagedEnvironment {
-    /// Path to the project's .flox directory
+    /// Path to the directory containing `env.json`
     _path: PathBuf,
     _pointer: ManagedPointer,
     _system: String,
@@ -55,6 +55,14 @@ pub enum ManagedEnvironmentError {
     ReverseLink(std::io::Error),
     #[error("couldn't create links directory: {0}")]
     CreateLinksDir(std::io::Error),
+    #[error("couldn't canonicalize path '{path}': {err}")]
+    Canonicalize { path: PathBuf, err: std::io::Error },
+    #[error("attempted to open the empty path ''")]
+    EmptyPath,
+    #[error("floxmeta branch name was malformed: {0}")]
+    BadBranchName(String),
+    #[error("project wasn't found at path {path}: {err}")]
+    ProjectNotFound { path: PathBuf, err: std::io::Error },
 }
 
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
@@ -154,28 +162,54 @@ impl Environment for ManagedEnvironment {
 
 impl ManagedEnvironment {
     /// Returns a unique identifier for the location of the project.
-    pub fn encode(path: impl AsRef<Path>) -> String {
+    fn encode(path: impl AsRef<Path>) -> Result<String, ManagedEnvironmentError> {
         let mut hasher = std::collections::hash_map::DefaultHasher::new();
-        path.as_ref().hash(&mut hasher);
-        format!("{:X}", hasher.finish())
+        let path =
+            std::fs::canonicalize(&path).map_err(|e| ManagedEnvironmentError::Canonicalize {
+                path: path.as_ref().to_path_buf(),
+                err: e,
+            })?;
+        path.as_path().hash(&mut hasher);
+        Ok(format!("{:X}", hasher.finish()))
+    }
+
+    /// Returns the path to an environment given the branch name in the floxmeta repository
+    #[allow(unused)]
+    fn decode(flox: &Flox, branch: &impl AsRef<str>) -> Result<PathBuf, ManagedEnvironmentError> {
+        let branch_name = branch.as_ref();
+        branch_name
+            .split('.')
+            .next_back()
+            .map(|hash| {
+                let links_dir = reverse_links_dir(flox);
+                let link = links_dir.join(hash);
+                std::fs::read_link(&link)
+                    .map_err(|e| ManagedEnvironmentError::ProjectNotFound { path: link, err: e })
+            })
+            .unwrap_or(Err(ManagedEnvironmentError::BadBranchName(String::from(
+                branch_name,
+            ))))
     }
 
     /// Creates a symlink pointing from the `FloxMeta` back to the project environment
     /// using this managed environment if the symlink doesn't already exist.
-    pub fn ensure_reverse_link(
+    fn ensure_reverse_link(
         flox: &Flox,
         path: impl AsRef<Path>,
     ) -> Result<(), ManagedEnvironmentError> {
         let links_dir = reverse_links_dir(flox);
+        let encoded = ManagedEnvironment::encode(&path)?;
+        let link = links_dir.join(encoded);
         if !links_dir.exists() {
             std::fs::create_dir_all(&links_dir).map_err(ManagedEnvironmentError::CreateLinksDir)?;
-        }
-        let encoded = ManagedEnvironment::encode(&path);
-        let link = links_dir.join(encoded);
-        // Do not use `Path.exists` to check whether the link exists. It will return `false` if
-        // the symlink is broken or if you can't read the file metadata due to permissions errors.
-        if !link.is_symlink() {
             std::os::unix::fs::symlink(path, link).map_err(ManagedEnvironmentError::ReverseLink)?;
+        } else {
+            // Do not use `Path.exists` to check whether the link exists. It will return `false` if
+            // the symlink is broken or if you can't read the file metadata due to permissions errors.
+            if !link.is_symlink() {
+                std::os::unix::fs::symlink(path, link)
+                    .map_err(ManagedEnvironmentError::ReverseLink)?;
+            }
         }
         Ok(())
     }
@@ -214,7 +248,7 @@ impl ManagedEnvironment {
 
         let lock = Self::ensure_locked(flox, &pointer, &dot_flox_path, &floxmeta)?;
         Self::ensure_branch(
-            &branch_name(&flox.system, &pointer, &dot_flox_path),
+            &branch_name(&flox.system, &pointer, &dot_flox_path)?,
             &lock,
             &floxmeta,
         )?;
@@ -370,26 +404,31 @@ impl ManagedEnvironment {
     }
 }
 
-fn branch_name(system: &str, pointer: &ManagedPointer, dot_flox_path: impl AsRef<Path>) -> String {
-    format!(
+fn branch_name(
+    system: &str,
+    pointer: &ManagedPointer,
+    dot_flox_path: impl AsRef<Path>,
+) -> Result<String, ManagedEnvironmentError> {
+    Ok(format!(
         "{}.{}.{}",
         system,
         pointer.name,
-        ManagedEnvironment::encode(dot_flox_path)
-    )
+        ManagedEnvironment::encode(dot_flox_path)?
+    ))
 }
 
 pub fn remote_branch_name(system: &str, pointer: &ManagedPointer) -> String {
     format!("{}.{}", system, pointer.name)
 }
 
-pub fn reverse_links_dir(flox: &Flox) -> PathBuf {
+fn reverse_links_dir(flox: &Flox) -> PathBuf {
     flox.data_dir.join("links")
 }
 
 #[cfg(test)]
 mod test {
     use std::str::FromStr;
+    use std::time::Duration;
 
     use once_cell::sync::Lazy;
 
@@ -872,16 +911,49 @@ mod test {
 
     #[test]
     fn stable_encode_name() {
-        let path = PathBuf::from_str("foo").unwrap();
-        let encode1 = ManagedEnvironment::encode(&path);
-        let encode2 = ManagedEnvironment::encode(&path);
+        // Ensure that running the encode function gives you the same results
+        // with the same input e.g. doesn't depend on time, etc
+        let (_flox, tmp_dir) = flox_instance();
+        let path = tmp_dir.path().join("foo");
+        std::fs::File::create(&path).unwrap();
+        let encode1 = ManagedEnvironment::encode(&path).unwrap();
+        std::thread::sleep(Duration::from_millis(1_000));
+        let encode2 = ManagedEnvironment::encode(&path).unwrap();
         assert_eq!(encode1, encode2);
     }
 
     #[test]
+    fn canonicalized_paths_encode_the_same() {
+        let (_flox, tmp) = flox_instance();
+        // std::fs::canonicalize requires that the path being checked actually exists,
+        // so we're going to create a directory structure under a temporary directory.
+        let parent_dir = tmp.into_path();
+        let subdir = parent_dir.join("foo/bar/baz");
+        std::fs::create_dir_all(&subdir).unwrap();
+        let path = subdir.join("file.txt");
+        let _file = std::fs::File::create(&path).unwrap();
+        // Make sure the path is canonicalized internally before hashing
+        let canonicalized = std::fs::canonicalize(&path).unwrap();
+        let c_encoded = ManagedEnvironment::encode(canonicalized).unwrap();
+        let encoded = ManagedEnvironment::encode(&path).unwrap();
+        assert_eq!(
+            c_encoded, encoded,
+            "sane path is canonicalized before encoding"
+        );
+        // Now do the same thing with cursed paths
+        let up_down = path.parent().unwrap().join("../../bar/baz/file.txt");
+        let encoded = ManagedEnvironment::encode(up_down).unwrap();
+        assert_eq!(
+            c_encoded, encoded,
+            "cursed path is canonicalized before encoding"
+        );
+    }
+
+    #[test]
     fn creates_reverse_links_dir() {
-        let (flox, _) = flox_instance();
-        let path = PathBuf::from_str("foo").unwrap();
+        let (flox, tmp_dir) = flox_instance();
+        let path = tmp_dir.path().join("foo");
+        std::fs::File::create(&path).unwrap();
         let links_dir = reverse_links_dir(&flox);
         assert!(!links_dir.exists());
         ManagedEnvironment::ensure_reverse_link(&flox, path).unwrap();
@@ -890,13 +962,15 @@ mod test {
 
     #[test]
     fn creates_reverse_link() {
-        let (flox, _) = flox_instance();
+        let (flox, tmp_dir) = flox_instance();
         let links_dir = reverse_links_dir(&flox);
-        let path = PathBuf::from_str("foo").unwrap();
+        let path = tmp_dir.path().join("foo");
+        std::fs::File::create(&path).unwrap();
         // There are no links if the directory hasn't been created
         assert!(!links_dir.exists());
         // Create the reverse link
         ManagedEnvironment::ensure_reverse_link(&flox, &path).unwrap();
+        // Ensure that only one symlink was created.
         assert_eq!(links_dir.read_dir().unwrap().count(), 1);
         let link_name = links_dir
             .read_dir()
@@ -905,19 +979,21 @@ mod test {
             .unwrap()
             .unwrap()
             .file_name();
-        let expected_link_name = ManagedEnvironment::encode(&path);
+        let expected_link_name = ManagedEnvironment::encode(&path).unwrap();
         assert_eq!(link_name.to_str().unwrap(), &expected_link_name);
     }
 
     #[test]
     fn noop_when_symlink_exists() {
-        let (flox, _) = flox_instance();
+        let (flox, tmp_dir) = flox_instance();
         let links_dir = reverse_links_dir(&flox);
-        let path = PathBuf::from_str("foo").unwrap();
+        let path = tmp_dir.path().join("foo");
+        std::fs::File::create(&path).unwrap();
         // There are no links if the directory hasn't been created
         assert!(!links_dir.exists());
         // Create the reverse link
         ManagedEnvironment::ensure_reverse_link(&flox, &path).unwrap();
+        // Ensure that only one symlink was created.
         assert_eq!(links_dir.read_dir().unwrap().count(), 1);
         let link_name = links_dir
             .read_dir()
@@ -926,7 +1002,7 @@ mod test {
             .unwrap()
             .unwrap()
             .file_name();
-        let expected_link_name = ManagedEnvironment::encode(&path);
+        let expected_link_name = ManagedEnvironment::encode(&path).unwrap();
         assert_eq!(link_name.to_str().unwrap(), &expected_link_name);
         // Ensure that the link exists, checking that another one hasn't been created
         ManagedEnvironment::ensure_reverse_link(&flox, &path).unwrap();
@@ -939,5 +1015,30 @@ mod test {
             .unwrap()
             .file_name();
         assert_eq!(link_name.to_str().unwrap(), &expected_link_name);
+    }
+
+    #[test]
+    fn decode_branch_name_to_path() {
+        let (flox, tmp_dir) = flox_instance();
+        let links_dir = reverse_links_dir(&flox);
+        let path = tmp_dir.path().join("foo");
+        std::fs::File::create(&path).unwrap();
+        // There are no links if the directory hasn't been created
+        assert!(!links_dir.exists());
+        // Create the reverse link
+        ManagedEnvironment::ensure_reverse_link(&flox, &path).unwrap();
+        // Ensure that only one symlink was created.
+        assert_eq!(links_dir.read_dir().unwrap().count(), 1);
+        // Decode the branch name and assert that it's the same path we created before
+        let pointer = ManagedPointer {
+            owner: EnvironmentOwner::from_str("owner").unwrap(),
+            name: EnvironmentName::from_str("name").unwrap(),
+            version: Version::<1>,
+        };
+        let branch_name = branch_name(&flox.system, &pointer, &path).unwrap();
+        let decoded_path = ManagedEnvironment::decode(&flox, &branch_name).unwrap();
+        let canonicalized_decoded_path = std::fs::canonicalize(decoded_path).unwrap();
+        let canonicalized_path = std::fs::canonicalize(&path).unwrap();
+        assert_eq!(canonicalized_path, canonicalized_decoded_path);
     }
 }
