940a3564c33a18f7fb0327e0de66157e8a81c23c
Matthew Kenigsberg <matthew@floxdev.com>
2023-10-23 06:41:21 -0400

    Revert "fix: flush logger (#306)" (#312)
    This reverts commit 2d525e7a7b454b6b9a015d3505d2b28d5b23dd58.
    
    As this commit did not fix flaky tests as first thought, revert it.
    Removing these bit of code seems clearer than adding a comment explaining why it exists at all.

--- crates/flox/src/main.rs
+++ crates/flox/src/main.rs
@@ -106,7 +106,7 @@ async fn main() -> ExitCode {
     let args = args.unwrap();
 
     // Run flox. Print errors and exit with status 1 on failure
-    let exit_code = match run(args).await {
+    match run(args).await {
         Ok(()) => ExitCode::from(0),
         Err(e) => {
             // Do not print any error if caused by wrapped flox (sh)
@@ -118,9 +118,7 @@ async fn main() -> ExitCode {
 
             ExitCode::from(1)
         },
-    };
-    utils::init::flush_logger();
-    exit_code
+    }
 }
 
 #[derive(Debug)]
