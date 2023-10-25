96d3ea86fa823dfdf8cfacbea5e53580fa2049b0
localecosr <sam@floxdev.com>
2023-10-25 12:00:57 -0400

    feat: working "auth2" command implements gh auth login in rust

--- crates/flox/src/commands/auth.rs
+++ crates/flox/src/commands/auth.rs
@@ -336,6 +336,7 @@ impl Auth2 {
     pub async fn handle(self, _config: Config, _flox: Flox) -> Result<()> {
         subcommand_metric!("auth2");
         let mut client_id = String::new();
+        //TODO put this ID in nix env
         client_id.push_str("Iv1.3b00a7bb5f910259");
         let host = None;
         let cred: std::result::Result<Credential, DeviceFlowError>;
