d543dfee68fe7b4da34f2e5e7b136632f955f72d
Yannik Sander <yannik@floxdev.com>
2023-10-31 13:58:39 +0100

    chore: remove `legacy_environment_ref`

--- crates/flox-rust-sdk/src/models/mod.rs
+++ crates/flox-rust-sdk/src/models/mod.rs
@@ -6,7 +6,6 @@ pub mod environment_ref;
 pub mod flox_installable;
 pub mod flox_package;
 pub mod legacy_environment;
-pub mod legacy_environment_ref;
 pub mod root;
 pub use runix::{flake_ref, registry};
 pub mod floxmeta;
