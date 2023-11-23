a3592fc2a53c4a0fe810d839a8297f9e8adeaf72
Yannik Sander <yannik@floxdev.com>
2023-11-23 15:03:08 +0100

    chore: remove unused sources

--- env-builder/src/Include.mk
+++ env-builder/src/Include.mk
@@ -9,12 +9,12 @@ SRC_DIR := $(call getMakefileDir)
 
 # ---------------------------------------------------------------------------- #
 
-_BUILT_SRCS := buildenv.nix get-env.sh
-_BUILT      := $(patsubst %,$(SRC_DIR)/%.gen.hh,$(_BUILT_SRCS))
-_BUILT_SRCS =
+# _BUILT_SRCS := buildenv.nix get-env.sh
+# _BUILT      := $(patsubst %,$(SRC_DIR)/%.gen.hh,$(_BUILT_SRCS))
+# _BUILT_SRCS =
 
-BUILT_SRCS       += $(_BUILT)
-env_builder_SRCS += $(_BUILT)
+BUILT_SRCS       +=
+env_builder_SRCS +=
 env_builder_SRCS += $(wildcard $(SRC_DIR)/*.cc)
 env_builder_LDLIBS += -lflox-pkgdb
 libenvbuilder_LDLIBS += -lsqlite3
