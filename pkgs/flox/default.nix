0aa2b300270a710c055c4bb71c243ccd541b466d
Rok Garbas <rok@floxdev.com>
2023-12-13 00:02:47 +0100

    build: flox output is now just a shell wrapper (#612)
    * possible to set version via env variable at runtime which is then used
    in flox shell wrapper
    * respect `git describe` format
    * this should further reduce CI time
    
    ---------
    
    Co-authored-by: Yannik Sander <7040031+ysndr@users.noreply.github.com>
    Co-authored-by: Matthew Kenigsberg <matthew@floxdev.com>
    Co-authored-by: Alex Ameen <alex.ameen.tx@gmail.com>

---
+++ pkgs/flox/default.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  self,
+  lib,
+  symlinkJoin,
+  makeBinaryWrapper,
+  flox-pkgdb,
+  flox-env-builder,
+  flox-cli,
+}: let
+  # Inherit version from Cargo.toml, aligning with the CLI version.
+  # We also inject some indication about the `git' revision of the repository.
+  cargoToml = lib.importTOML (
+    if builtins.pathExists "${inputs.flox-latest}/cli/flox/Cargo.toml"
+    then "${inputs.flox-latest}/cli/flox/Cargo.toml"
+    else "${inputs.flox-latest}/crates/flox/Cargo.toml"
+  );
+  revCountDiff = self.revCount - inputs.flox-latest.revCount;
+  suffix =
+    if self ? revCount && self ? shortRev
+    then "${builtins.toString revCountDiff}-g${self.shortRev}"
+    else "dirty";
+  version = "${cargoToml.package.version}-${suffix}";
+in
+  symlinkJoin {
+    name = "${flox-cli.pname}-${version}";
+
+    paths = [flox-cli];
+    nativeBuildInputs = [makeBinaryWrapper];
+
+    postBuild = ''
+      wrapProgram $out/bin/flox \
+        --set PKGDB_BIN       "${flox-pkgdb}/bin/pkgdb" \
+        --set ENV_BUILDER_BIN "${flox-env-builder}/bin/env-builder" \
+        --set FLOX_BIN        "${flox-cli}/bin/flox" \
+        --set FLOX_VERSION    "${version}"
+    '';
+  }
