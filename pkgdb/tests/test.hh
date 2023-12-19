4cde1944a87ce67b17eb102512c04b6194e8b767
Yannik Sander <7040031+ysndr@users.noreply.github.com>
2023-12-19 19:00:38 +0100

    ci: add `clang-format` commit-hook (#670)
    * add `clang-format` commit-hook
    * ci: change git hooks to run on `push` rather than `commit`
      manually run them with 
        ```
        $ pre-commit run --hook-stage manual -a
        ``` 	
    * chore: format c/c++ files

--- pkgdb/tests/test.hh
+++ pkgdb/tests/test.hh
@@ -6,9 +6,9 @@
 
 #pragma once
 
-#include <string>
-#include <iostream>
 #include <cstddef>
+#include <iostream>
+#include <string>
 
 
 /* -------------------------------------------------------------------------- */
@@ -16,20 +16,20 @@
 /* This shouldn't happen, but it's a sane fallback for running from the
  * project root. */
 #ifndef TEST_DATA_DIR
-#  define TEST_DATA_DIR  "./tests/data"
-#endif  /* End `ifndef TEST_DATA_DIR' */
+#  define TEST_DATA_DIR "./tests/data"
+#endif /* End `ifndef TEST_DATA_DIR' */
 
 
 /* -------------------------------------------------------------------------- */
 
-static const std::string nixpkgsRev =
-  "e8039594435c68eb4f780f3e9bf3972a7399c4b1";
+static const std::string nixpkgsRev
+  = "e8039594435c68eb4f780f3e9bf3972a7399c4b1";
 
-static const std::string nixpkgsRef =
-  "github:NixOS/nixpkgs/e8039594435c68eb4f780f3e9bf3972a7399c4b1";
+static const std::string nixpkgsRef
+  = "github:NixOS/nixpkgs/e8039594435c68eb4f780f3e9bf3972a7399c4b1";
 
-static const std::string nixpkgsFingerprintStr =
-  "5fde12e3424840cc2752dae09751b09b03f5a33c3ec4de672fc89d236720bdc7";
+static const std::string nixpkgsFingerprintStr
+  = "5fde12e3424840cc2752dae09751b09b03f5a33c3ec4de672fc89d236720bdc7";
 
 
 /**
@@ -47,9 +47,9 @@ static const size_t fullPkgCount     = 64040;
 /* -------------------------------------------------------------------------- */
 
 /** @brief Wrap a test function pretty printing its name on failure. */
-template <typename F, typename ... Args>
-  static int
-runTest( std::string_view name, F f, Args && ... args )
+template<typename F, typename... Args>
+static int
+runTest( std::string_view name, F f, Args &&... args )
 {
   try
     {
@@ -59,7 +59,7 @@ runTest( std::string_view name, F f, Args && ... args )
           return EXIT_FAILURE;
         }
     }
-  catch( const std::exception & e )
+  catch ( const std::exception & e )
     {
       std::cerr << "  ERROR: " << name << ": " << e.what() << std::endl;
       return EXIT_FAILURE;
@@ -76,18 +76,16 @@ runTest( std::string_view name, F f, Args && ... args )
  *
  * This pattern allows early tests to still run later ones, while preserving
  * a "global" exit status.
- * 
+ *
  * This emits a warning on clang until we start using clang 12
  * "must specify at least one argument for '...' parameter of variadic macro"
  * https://github.com/llvm/llvm-project/issues/50951
  */
-#define _RUN_TEST( _EXIT_CODE, _NAME, ... )                       \
-  {                                                               \
-    int _exitCode = runTest(             ( # _NAME )              \
-                       ,             ( test_ ## _NAME )           \
-           __VA_OPT__( , ) __VA_ARGS__                            \
-                       );                                         \
-    if ( _exitCode != EXIT_SUCCESS ) { _EXIT_CODE = _exitCode; }  \
+#define _RUN_TEST( _EXIT_CODE, _NAME, ... )                               \
+  {                                                                       \
+    int _exitCode                                                         \
+      = runTest( ( #_NAME ), (test_##_NAME) __VA_OPT__(, ) __VA_ARGS__ ); \
+    if ( _exitCode != EXIT_SUCCESS ) { _EXIT_CODE = _exitCode; }          \
   }
 
 
@@ -98,13 +96,13 @@ runTest( std::string_view name, F f, Args && ... args )
  *
  * Assert that and expression is `true', otherwise print it and return `false'.
  */
-#define EXPECT( EXPR )                      \
-  if ( ! ( EXPR ) )                         \
-    {                                       \
-      std::cerr << "Expectation failed: ";  \
-      std::cerr << ( # EXPR );              \
-      std::cerr << std::endl;               \
-      return false;                         \
+#define EXPECT( EXPR )                     \
+  if ( ! ( EXPR ) )                        \
+    {                                      \
+      std::cerr << "Expectation failed: "; \
+      std::cerr << ( #EXPR );              \
+      std::cerr << std::endl;              \
+      return false;                        \
     }
 
 
@@ -118,20 +116,20 @@ runTest( std::string_view name, F f, Args && ... args )
  * std::string foo( "foo" );
  * EXPECT_EQ( "foo", foo.c_str() );
  */
-#define EXPECT_EQ( EXPR_A, EXPR_B )                                            \
-  {                                                                            \
-    auto valA = ( EXPR_A );                                                    \
-    auto valB = ( EXPR_B );                                                    \
-    if ( valA != valB )                                                        \
-      {                                                                        \
-        std::cerr << "Expectation failed: ( ";                                 \
-        std::cerr << ( # EXPR_A );                                             \
-        std::cerr << " ) == ( ";                                               \
-        std::cerr << ( # EXPR_B );                                             \
-        std::cerr << " ). Got '" << valA << "' != '" << valB << "'"            \
-                  << std::endl;                                                \
-        return false;                                                          \
-      }                                                                        \
+#define EXPECT_EQ( EXPR_A, EXPR_B )                                 \
+  {                                                                 \
+    auto valA = ( EXPR_A );                                         \
+    auto valB = ( EXPR_B );                                         \
+    if ( valA != valB )                                             \
+      {                                                             \
+        std::cerr << "Expectation failed: ( ";                      \
+        std::cerr << ( #EXPR_A );                                   \
+        std::cerr << " ) == ( ";                                    \
+        std::cerr << ( #EXPR_B );                                   \
+        std::cerr << " ). Got '" << valA << "' != '" << valB << "'" \
+                  << std::endl;                                     \
+        return false;                                               \
+      }                                                             \
   }
 
 
