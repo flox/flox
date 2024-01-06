96f95f1a8c8b2d98179e41f6f64f97933566912c
Alex Ameen <alex.ameen.tx@gmail.com>
2024-01-06 08:59:27 -0600

    docs: extend docs on FloxpkgsFlake

--- pkgdb/include/flox/registry/floxpkgs.hh
+++ pkgdb/include/flox/registry/floxpkgs.hh
@@ -25,6 +25,11 @@ namespace flox {
  *        to a `flake' before it is evaluated.
  *
  * This is used to implement the `floxpkgs' catalog.
+ *
+ * The fingerprint hash associated with this flake to be that of the wrapper so
+ * that changes to rules invalidate previously cached databases.
+ * However, the flake-refs which appear in lockfiles will be
+ * `nixpkgs` _flake-refs_.
  */
 class FloxpkgsFlake : public FloxFlake
 {
