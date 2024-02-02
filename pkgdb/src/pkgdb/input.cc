86e73ac4c2381808f88151046f5809ab53fbebf4
Bill LeVine <bill@floxdev.com>
2024-02-02 08:40:49 -0500

    fix: WIP addressing memory consumption

--- pkgdb/src/pkgdb/input.cc
+++ pkgdb/src/pkgdb/input.cc
@@ -230,6 +230,7 @@ PkgDbRegistryMixin::scrapeIfNeeded()
   assert( this->registry != nullptr );
   for ( auto & [name, input] : *this->registry )
     {
+      std::cout << "WML: scraping systems for input." << std::endl;
       input->scrapeSystems( this->getSystems() );
       input->freeFlake();
     }
