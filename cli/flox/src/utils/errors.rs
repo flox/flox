315a10ba2e4f89fe8be471bb7aa8509c172601d2
Yannik Sander <yannik@floxdev.com>
2024-02-05 17:11:44 +0100

    chore: wording

--- cli/flox/src/utils/errors.rs
+++ cli/flox/src/utils/errors.rs
@@ -278,7 +278,7 @@ pub fn format_managed_error(err: &ManagedEnvironmentError) -> String {
         // todo: communicate reasons for this error
         // git auth errors may be catched separately or reported
         ManagedEnvironmentError::OpenFloxmeta(err) => formatdoc! {"
-            Failed to fetch updates environment: {err}
+            Failed to fetch environment: {err}
         "},
 
         // todo: merge errors or make more specific
