6dfe5c8f8dcf8d840efd84a558493e2c5acbd24e
Yannik Sander <yannik@floxdev.com>
2024-02-15 18:15:12 +0100

    fix: `readability-magic-numbers` in pkgdb/include/flox/resolver/descriptor.hh
    https://releases.llvm.org/17.0.1/tools/clang/tools/extra/docs/clang-tidy/checks/readability/magic-numbers.html

--- pkgdb/include/flox/resolver/descriptor.hh
+++ pkgdb/include/flox/resolver/descriptor.hh
@@ -27,6 +27,10 @@ namespace flox::resolver {
 
 /* -------------------------------------------------------------------------- */
 
+const unsigned DEFAULT_PRIORITY = 5;
+
+/* -------------------------------------------------------------------------- */
+
 /** @brief A named group which a descriptor/package can be a member of. */
 using GroupName = std::string;
 
@@ -255,7 +259,7 @@ public:
    * Packages with higher @a priority values will take precendence over those
    * with lower @a priority values.
    */
-  unsigned priority = 5;
+  unsigned priority = DEFAULT_PRIORITY;
 
 
   ManifestDescriptor() = default;
