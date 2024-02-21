ffd99acc327d276c31842e5262368f7abf74f545
Alex Ameen <alex.ameen.tx@gmail.com>
2024-02-21 06:24:55 -0600

    feat: implement most of 'WrappedNixpkgsInputScheme::fetch()'

--- pkgdb/src/registry/wrapped-nixpkgs-input.cc
+++ pkgdb/src/registry/wrapped-nixpkgs-input.cc
@@ -7,7 +7,9 @@
  *
  * -------------------------------------------------------------------------- */
 
+#include <nix/cache.hh>
 #include <nix/fetchers.hh>
+#include <nix/store-api.hh>
 #include <nix/url-parts.hh>
 
 #include "flox/core/exceptions.hh"
@@ -54,6 +56,10 @@ struct WrappedNixpkgsInputScheme : nix::fetchers::InputScheme
                   std::optional<std::string>   ref,
                   std::optional<nix::Hash>     rev ) const override;
 
+  void
+  clone( const nix::fetchers::Input & input,
+         const nix::Path &            destDir ) const override;
+
   std::pair<nix::StorePath, nix::fetchers::Input>
   fetch( nix::ref<nix::Store>         store,
          const nix::fetchers::Input & _input ) override;
@@ -61,10 +67,6 @@ struct WrappedNixpkgsInputScheme : nix::fetchers::InputScheme
   nix::Hash
   getRevFromRef( nix::ref<nix::Store>         store,
                  const nix::fetchers::Input & input ) const;
-
-  void
-  clone( const nix::fetchers::Input & input,
-         const nix::Path &            destDir ) const override;
 
 
 }; /* End class `WrappedNixpkgsInputScheme' */
@@ -216,6 +218,160 @@ WrappedNixpkgsInputScheme::inputFromURL( const nix::ParsedURL & url ) const
     }
 
   return input;
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+nix::ParsedURL
+WrappedNixpkgsInputScheme::toURL( const nix::fetchers::Input & input ) const
+{
+  nix::ParsedURL url;
+  url.scheme = type();
+
+  if ( auto rulesVersion
+       = nix::fetchers::maybeGetIntAttr( input.attrs, "rulesVersion" ) )
+    {
+      url.path = "v" + std::to_string( *rulesVersion );
+    }
+  else { throw nix::Error( "missing 'rulesVersion' attribute in input" ); }
+
+  if ( auto rev = nix::fetchers::maybeGetStrAttr( input.attrs, "rev" ) )
+    {
+      url.path += "/" + *rev;
+    }
+  else if ( auto ref = nix::fetchers::maybeGetStrAttr( input.attrs, "ref" ) )
+    {
+      url.path += "/" + *ref;
+    }
+  else { throw nix::Error( "missing 'rev' or 'ref' attribute in input" ); }
+
+  return url;
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+bool
+WrappedNixpkgsInputScheme::hasAllInfo(
+  const nix::fetchers::Input & input ) const
+{
+  return nix::fetchers::maybeGetStrAttr( input.attrs, "rev" ).has_value()
+         && nix::fetchers::maybeGetIntAttr( input.attrs, "rulesVersion" )
+              .has_value();
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+nix::fetchers::Input
+WrappedNixpkgsInputScheme::applyOverrides( const nix::fetchers::Input & _input,
+                                           std::optional<std::string>   ref,
+                                           std::optional<nix::Hash> rev ) const
+{
+  auto input = _input;
+
+  if ( rev && ref )
+    {
+      throw nix::BadURL(
+        "cannot apply both a commit hash (%s) and a branch/tag "
+        "name ('%s') to input '%s'",
+        rev->gitRev(),
+        *ref,
+        input.to_string() );
+    }
+  if ( rev.has_value() )
+    {
+      input.attrs.insert_or_assign( "rev", rev->gitRev() );
+      input.attrs.erase( "ref" );
+    }
+  else if ( ref.has_value() )
+    {
+      input.attrs.insert_or_assign( "ref", *ref );
+      input.attrs.erase( "rev" );
+    }
+
+  return input;
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+/** @brief Clones the repository for analysis, but does not modify/patch it. */
+void
+WrappedNixpkgsInputScheme::clone( const nix::fetchers::Input & input,
+                                  const nix::Path &            destDir ) const
+{
+  auto githubInput = nix::fetchers::Input::fromAttrs(
+    floxNixpkgsAttrsToGithubAttrs( input.attrs ) );
+  githubInput.clone( destDir );
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+std::pair<nix::StorePath, nix::fetchers::Input>
+WrappedNixpkgsInputScheme::fetch( nix::ref<nix::Store>         store,
+                                  const nix::fetchers::Input & _input )
+{
+  nix::fetchers::Input input( _input );
+
+  if ( ! nix::fetchers::maybeGetStrAttr( input.attrs, "ref" ).has_value() )
+    {
+      input.attrs.insert_or_assign( "ref", "HEAD" );
+    }
+
+  auto rev = input.getRev();
+  if ( ! rev.has_value() )
+    {
+      /* Use existing GitHub fetcher in `nix` to lookup `rev`. */
+      auto githubInput = nix::fetchers::Input::fromAttrs(
+        floxNixpkgsAttrsToGithubAttrs( input.attrs ) );
+      rev = githubInput.fetch( store ).second.getRev();
+    }
+
+  input.attrs.erase( "ref" );
+  input.attrs.insert_or_assign( "rev", rev->gitRev() );
+
+  nix::fetchers::Attrs lockedAttrs(
+    { { "type", "flox-nixpkgs" },
+      { "rulesVersion",
+        nix::fetchers::getIntAttr( _input.attrs, "rulesVersion" ) },
+      { "rev", rev->gitRev() } } );
+
+  if ( auto res = nix::fetchers::getCache()->lookup( store, lockedAttrs ) )
+    {
+      input.attrs.insert_or_assign(
+        "lastModified",
+        nix::fetchers::getIntAttr( res->first, "lastModified" ) );
+      return { std::move( res->second ), input };
+    }
+
+  auto githubInput = nix::fetchers::Input::fromAttrs(
+    floxNixpkgsAttrsToGithubAttrs( input.attrs ) );
+  auto [githubTree, lockedGithubInput] = githubInput.fetch( store );
+
+  input.attrs.insert_or_assign(
+    "lastModified",
+    nix::fetchers::getIntAttr( lockedGithubInput.attrs, "lastModified" ) );
+
+  // TODO: replace `flake.nix' and do whatever other patching you want.
+
+  // TODO: change `githubTree.actualPath' to the top level of your patched tree
+  nix::StorePath storePath
+    = store->addToStore( "source", githubTree.actualPath );
+
+  nix::fetchers::getCache()->add(
+    store,
+    lockedAttrs,
+    { { "rev", rev->gitRev() },
+      { "lastModified",
+        nix::fetchers::getIntAttr( lockedGithubInput.attrs,
+                                   "lastModified" ) } },
+    storePath,
+    true );
+
+  return { storePath, input };
 }
 
 
