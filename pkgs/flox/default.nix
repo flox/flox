2e5474dcba8c6af5ad5d6ed903ae7f60b3e47a7f
Yannik Sander <yannik@floxdev.com>
2024-02-28 14:26:08 +0100

    fix: set correct sentry variables in flox package

--- pkgs/flox/default.nix
+++ pkgs/flox/default.nix
@@ -37,8 +37,8 @@ in
 
     postBuild = ''
       wrapProgram $out/bin/flox \
-        ${lib.optionalString (SENTRY_DSN != null) "--set SENTRY_DSN \"${SENTRY_DSN}\" "} \
-        ${lib.optionalString (SENTRY_ENV != null) "--set SENTRY_ENV \"${SENTRY_ENV}\" "} \
+        ${lib.optionalString (SENTRY_DSN != null) "--set FLOX_SENTRY_DSN \"${SENTRY_DSN}\" "} \
+        ${lib.optionalString (SENTRY_ENV != null) "--set FLOX_SENTRY_ENV \"${SENTRY_ENV}\" "} \
         --set PKGDB_BIN       "${flox-pkgdb}/bin/pkgdb" \
         --set LD_FLOXLIB      "${flox-pkgdb}/lib/ld-floxlib.so" \
         --set FLOX_BIN        "${flox-cli}/bin/flox" \
