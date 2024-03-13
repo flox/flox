6755f1e7b6a977570e3452fa1513b850983d7ea4
Yannik Sander <yannik@floxdev.com>
2024-03-13 14:17:34 +0100

    fix: set default nix vars process wide
    follow up on https://github.com/flox/flox/pull/1153,
    which mitigated this for pkgdb but not git
    
    This PR is setting nix related env vars globally for the entire process at startup,
    as well explicitly for activated environments.

--- cli/flox/src/commands/environment.rs
+++ cli/flox/src/commands/environment.rs
@@ -77,7 +77,7 @@ use crate::utils::errors::{
     format_core_error,
     format_locked_manifest_error,
 };
-use crate::utils::message;
+use crate::utils::{default_nix_env_vars, message};
 use crate::{subcommand_metric, utils};
 
 // Edit declarative environment configuration
@@ -592,7 +592,7 @@ impl Activate {
             ("FLOX_PROMPT_COLOR_2", prompt_color_2),
         ]);
 
-        exports.extend(Self::default_subprocess_env_vars());
+        exports.extend(default_nix_env_vars());
 
         if let Some(fixed_up_original_path_joined) = fixed_up_original_path_joined {
             exports.insert(
@@ -871,55 +871,6 @@ impl Activate {
             .iter()
             .map(|arg| format!(r#""{}""#, arg.replace('"', r#"\""#)))
             .join(" ")
-    }
-
-    /// Explicitly set environment for nix calls
-    ///
-    /// Nixpkgs itself is broken in that the packages it creates depends
-    /// upon a variety of environment variables at runtime.  On NixOS
-    /// these are convenient to set on a system-wide basis but that
-    /// essentially masks the problem, and it's not uncommon to see Nix
-    /// packages trip over the absence of environment variables when
-    /// invoked on other Linux distributions.
-    ///
-    /// For flox specifically, set Nix-provided defaults for certain
-    /// environment variables that we know to be required on the various
-    /// operating systems.
-    /// Setting buildtime variants of these environment variables
-    /// will bundle them in flox' package closure
-    /// and ensure that subprocesses are run with valid known values.
-    fn default_subprocess_env_vars() -> HashMap<&'static str, String> {
-        let mut env_map: HashMap<&str, String> = HashMap::new();
-
-        // use buildtime NIXPKGS_CACERT_BUNDLE_CRT
-        let ssl_cert_file = match env::var("SSL_CERT_FILE") {
-            Ok(v) => v,
-            Err(_) => {
-                let nixpkgs_cacert_bundle_crt = env!("NIXPKGS_CACERT_BUNDLE_CRT");
-                env_map.insert("SSL_CERT_FILE", nixpkgs_cacert_bundle_crt.to_string());
-                nixpkgs_cacert_bundle_crt.to_string()
-            },
-        };
-
-        env_map.insert("NIX_SSL_CERT_FILE", ssl_cert_file);
-
-        // on macos use buildtime NIX_COREFOUNDATION_RPATH and PATH_LOCALE
-        #[cfg(target_os = "macos")]
-        {
-            env_map.insert(
-                "NIX_COREFOUNDATION_RPATH",
-                env!("NIX_COREFOUNDATION_RPATH").to_string(),
-            );
-            env_map.insert("PATH_LOCALE", env!("PATH_LOCALE").to_string());
-        }
-
-        // on linux use buildtime LOCALE_ARCHIVE
-        #[cfg(target_os = "linux")]
-        {
-            env_map.insert("LOCALE_ARCHIVE", env!("LOCALE_ARCHIVE").to_string());
-        }
-
-        env_map
     }
 }
 
