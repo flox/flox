2f9fad42f2b7f38090534540bda6c96e6e55c65e
Yannik Sander <yannik@floxdev.com>
2024-03-22 18:19:38 +0100

    refactor: extract registry creation and filter updates

--- cli/flox/src/utils/init/logger.rs
+++ cli/flox/src/utils/init/logger.rs
@@ -2,6 +2,7 @@ use log::{debug, error};
 use once_cell::sync::OnceCell;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::reload::Handle;
+use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
 use crate::commands::Verbosity;
@@ -34,9 +35,9 @@ impl std::io::Write for LockingTerminalStderr {
     }
 }
 
-#[allow(clippy::type_complexity)]
 static LOGGER_HANDLE: OnceCell<Handle<EnvFilter, Registry>> = OnceCell::new();
-pub fn init_logger(verbosity: Option<Verbosity>) {
+
+pub(crate) fn init_logger(verbosity: Option<Verbosity>) {
     let verbosity = verbosity.unwrap_or_default();
 
     let log_filter = match verbosity {
@@ -56,46 +57,17 @@ pub fn init_logger(verbosity: Option<Verbosity>) {
     };
 
     let filter_handle = LOGGER_HANDLE.get_or_init(|| {
-        debug!("Initializing logger (how are you seeing this?)");
-
-        // The first time this layer is set it establishes an upper boundary for `log` verbosity.
-        // If you try to `modify` this layer later, `log` will not accept any higher verbosity events.
-        //
-        // Before we used to replace both the fmt layer _and_ this layer.
-        // That purged enough internal state to reset the `log` verbosity filter.
-        // For simplicity, we'll now just set the filter to `trace`,
-        // and then modify it later to the actual level below.
-        // Logs are being passed through by the `log` crate and correctly filtered by `tracing`.
-        let filter = tracing_subscriber::filter::EnvFilter::try_new("trace").unwrap();
-
-        let (filter, filter_reload_handle) = tracing_subscriber::reload::Layer::new(filter);
-
-        let log_layer = tracing_subscriber::fmt::layer()
-            .with_writer(LockingTerminalStderr)
-            .event_format(tracing_subscriber::fmt::format())
-            .with_filter(filter);
-
-        let metrics_layer = MetricsLayer::new();
-        let sentry_layer = sentry::integrations::tracing::layer();
-
-        // Filtered layer must come first.
-        // This appears to be the only way to avoid logs of the `flox_command` trace
-        // which is processed by the `log_layer` irrepective of the filter applied to it.
-        // My current understanding is, that it because the `metrics_layer` (at least) is
-        // registering `Interest` for the event and that somehow bypasses the filter?!
-        tracing_subscriber::registry()
-            .with(log_layer)
-            .with(metrics_layer)
-            .with(sentry_layer)
-            .init();
-
-        filter_reload_handle
+        let (subscriber, reload_handle) = create_registry_and_filter_reload_handle();
+        subscriber.init();
+        reload_handle
     });
 
+    update_filters(filter_handle, log_filter);
+}
+
+pub fn update_filters(filter_handle: &Handle<EnvFilter, Registry>, log_filter: &str) {
     let result = filter_handle.modify(|layer| {
-        match tracing_subscriber::filter::EnvFilter::try_from_default_env()
-            .or_else(|_| tracing_subscriber::EnvFilter::try_new(log_filter))
-        {
+        match EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new(log_filter)) {
             Ok(new_filter) => *layer = new_filter,
             Err(err) => {
                 error!("Updating logger filter failed: {}", err);
@@ -105,4 +77,38 @@ pub fn init_logger(verbosity: Option<Verbosity>) {
     if let Err(err) = result {
         error!("Updating logger filter failed: {}", err);
     }
+}
+
+pub fn create_registry_and_filter_reload_handle() -> (
+    impl tracing_subscriber::layer::SubscriberExt,
+    Handle<EnvFilter, Registry>,
+) {
+    debug!("Initializing logger (how are you seeing this?)");
+    // The first time this layer is set it establishes an upper boundary for `log` verbosity.
+    // If you try to `modify` this layer later, `log` will not accept any higher verbosity events.
+    //
+    // Before we used to replace both the fmt layer _and_ this layer.
+    // That purged enough internal state to reset the `log` verbosity filter.
+    // For simplicity, we'll now just set the filter to `trace`,
+    // and then modify it later to the actual level below.
+    // Logs are being passed through by the `log` crate and correctly filtered by `tracing`.
+    let filter = tracing_subscriber::filter::EnvFilter::try_new("trace").unwrap();
+    let (filter, filter_reload_handle) = tracing_subscriber::reload::Layer::new(filter);
+    let log_layer = tracing_subscriber::fmt::layer()
+        .with_writer(LockingTerminalStderr)
+        .event_format(tracing_subscriber::fmt::format())
+        .with_filter(filter);
+    let metrics_layer = MetricsLayer::new();
+    let sentry_layer = sentry::integrations::tracing::layer();
+    // Filtered layer must come first.
+    // This appears to be the only way to avoid logs of the `flox_command` trace
+    // which is processed by the `log_layer` irrepective of the filter applied to it.
+    // My current understanding is, that it because the `metrics_layer` (at least) is
+    // registering `Interest` for the event and that somehow bypasses the filter?!
+    let registry = tracing_subscriber::registry()
+        .with(log_layer)
+        .with(metrics_layer)
+        .with(sentry_layer);
+
+    (registry, filter_reload_handle)
 }
