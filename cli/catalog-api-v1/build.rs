db3274253c99b3a4cb550a97b12d7bb2903adc8e
Yannik Sander <yannik@floxdev.com>
2024-04-08 16:30:32 +0200

    build: initialize `catalog-api-v1` crate
    Generate an API client and Mock server from an OpenAPI 3.0 spec
    using the `progenitor` crate.

---
+++ cli/catalog-api-v1/build.rs
@@ -0,0 +1,39 @@
+use std::fs;
+use std::path::PathBuf;
+
+use openapiv3::OpenAPI;
+
+fn main() {
+    let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+
+    let spec_src = manifest_dir.join("openapi.json");
+
+    println!("cargo:rerun-if-changed={}", spec_src.display());
+
+    let file = std::fs::File::open(spec_src).unwrap();
+    let spec = serde_json::from_reader(file).expect("Failed to parse openapi spec");
+
+    let client = generate_client(&spec);
+    let client_dst = out_dir.join("client.rs");
+    fs::write(client_dst, client).unwrap();
+
+    let mock = gnereate_mock(&spec);
+    let mock_dst = out_dir.join("mock.rs");
+    fs::write(mock_dst, mock).unwrap();
+}
+
+fn generate_client(spec: &OpenAPI) -> String {
+    let mut generator = progenitor::Generator::default();
+    let tokens = generator.generate_tokens(spec).unwrap();
+    let ast = syn::parse2(tokens).unwrap();
+    prettyplease::unparse(&ast)
+}
+
+fn gnereate_mock(spec: &OpenAPI) -> String {
+    let mut generator = progenitor::Generator::default();
+    let tokens = generator.httpmock(spec, "crate").unwrap();
+
+    let ast = syn::parse2(tokens).unwrap();
+    prettyplease::unparse(&ast)
+}
