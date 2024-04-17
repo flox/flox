0683eac192ded968d016d7664806fbe19045188b
Matthew Kenigsberg <matthew@floxdev.com>
2024-04-17 09:52:01 -0600

    fix: default for feature flag

--- cli/flox/src/config/features.rs
+++ cli/flox/src/config/features.rs
@@ -9,6 +9,7 @@ pub struct Features {
     /// Which matching logic to use when searching for packages
     #[serde(default)]
     pub search_strategy: SearchStrategy,
+    #[serde(default)]
     pub use_catalog: bool,
 }
 
