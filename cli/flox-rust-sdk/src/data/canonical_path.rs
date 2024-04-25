878c23d0dc4c58631c6c797a01899fd2e002d961
Yannik Sander <yannik@floxdev.com>
2024-04-25 20:37:58 +0200

    fix: remove unused canonical path methods

--- cli/flox-rust-sdk/src/data/canonical_path.rs
+++ cli/flox-rust-sdk/src/data/canonical_path.rs
@@ -33,23 +33,8 @@ impl CanonicalPath {
         Ok(Self(canonicalized))
     }
 
-    /// Create a [`CanonicalPath`] without checking if the path exists
-    /// or is canonical.
-    ///
-    /// This should only be used when the path is known to be canonical already.
-    pub fn new_unchecked(path: impl AsRef<Path>) -> Self {
-        Self(path.as_ref().to_path_buf())
-    }
-
     /// Destruct the [`CanonicalPath`] and return the inner [`PathBuf`]
     pub fn into_inner(self) -> PathBuf {
         self.0
-    }
-
-    /// Get the parent directory of the path as a [`CanonicalPath`]
-    ///
-    /// Returns [`None`] if the path has no parent.
-    pub fn parent(inst: &CanonicalPath) -> Option<Self> {
-        inst.0.parent().map(Self::new_unchecked)
     }
 }
