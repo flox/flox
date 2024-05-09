c6e5e342929a19aa60caa5540967694c63c2fc15
Óscar Carrasco <me@oxca.be>
2024-05-09 17:05:06 +0100

    refactor: remove `ParseTemplateManifest` error
    Co-authored-by: Yannik Sander <7040031+ysndr@users.noreply.github.com>

--- cli/flox-rust-sdk/src/models/environment/mod.rs
+++ cli/flox-rust-sdk/src/models/environment/mod.rs
@@ -444,9 +444,6 @@ pub enum EnvironmentError {
 
     #[error("could not read manifest")]
     ReadManifest(#[source] std::io::Error),
-    // Should be unreachable since we control the template
-    #[error("Failed parsing contents of template manifest")]
-    ParseTemplateManifest(#[source] toml_edit::de::Error),
     #[error("Failed editing contents of manifest")]
     EditManifest,
     #[error("couldn't write manifest")]
