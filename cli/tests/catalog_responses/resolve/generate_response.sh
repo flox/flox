802f99d015ffbca123a70afba36e8fc58f7c73da
Matthew Kenigsberg <matthew@floxdev.com>
2024-05-28 19:30:09 -0600

    test: add script to generate catalog responses (#1503)
    We're currently throwing JSON files in `catalog_responses`, but it's
    hard to know what each file contains. This introduces a bit more
    structured approach:
    - Add a resolve subdirectory
    - Add a script to dump a response for a package or list of responses
    - Port `hello_resolution.json` to `resolve/hello.json` using this
    approach
    
    Other files can be ported if we like this approach.

---
+++ cli/tests/catalog_responses/resolve/generate_response.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Call this script with TESTING_FLOX_CATALOG_URL set and a list of packages.
+# It will generate a JSON response file produced by running
+# `flox install -i pkg1_install_id pkg1 -i pkg2_install_id pkg2 ...`
+# and save it in a file `pkg1_pkg2.json`.
+# If the dump file already exists, it will be deleted.
+
+set -euo pipefail
+
+if [ -z "${TESTING_FLOX_CATALOG_URL:-}" ]; then
+  echo "TESTING_FLOX_CATALOG_URL is not set"
+  exit 1
+fi
+export FLOX_CATALOG_URL="$TESTING_FLOX_CATALOG_URL"
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <pkg1> [pkg2] ..."
+  exit 1
+fi
+
+# generate install command and filename
+first_pkg="$1"
+
+install_args=(-i "${first_pkg}_install_id" "$first_pkg")
+filename="$first_pkg"
+
+for pkg in "${@:2}"; do
+  install_args+=(-i "${pkg}_install_id" "$pkg")
+  filename+="_$pkg"
+done
+
+filename+=".json"
+
+rm -f "$filename"
+
+# dump
+export FLOX_FEATURES_USE_CATALOG=true
+export _FLOX_CATALOG_DUMP_RESPONSE_FILE="$PWD/$filename"
+mkdir tmp
+pushd tmp
+flox init
+
+# TODO: should be able to drop this once we default to all systems
+flox list -c |
+  tomlq --toml-output '.options.systems = ["aarch64-darwin", "x86_64-darwin", "aarch64-linux", "x86_64-linux"]' |
+  flox edit -f -
+
+if flox install -v "${install_args[@]}"; then
+  rc=0
+else
+  rc="$?"
+fi
+
+flox delete -f -d .
+popd
+rm -r tmp
+exit "$rc"
+
