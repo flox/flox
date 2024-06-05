a7b56ce3a801daef68f0875ac9ab3c3d5524312d
Dan Carley <dan.carley@gmail.com>
2024-06-05 10:12:11 +0100

    fix(activate): Integration test prompt
    Partially address the comment that was carried over from `hello.exp` by
    checking the intermediate environment variable that we set to determine
    the prompt because we don't have `PS1`, presumably because it's
    non-interactive.

--- cli/tests/hello-check.sh
+++ cli/tests/hello-check.sh
@@ -1,6 +1,6 @@
 # When a project is activated with --dir check
 # - hello is installed
-# - prompt is set ### XXX how exactly? I don't see it doing this.
+# - prompt is set
 # Assume throughout that the project is named project-\d+
 set -euxo pipefail
 
@@ -10,3 +10,6 @@ set -euxo pipefail
 # check for hello after changing directory
 cd ..
 [[ "$({ command -v hello||which hello||type -P hello; } 2>&1)" =~ bin\/hello ]]
+
+# closest we can get because PS1 isn't exported
+[[ "${FLOX_PROMPT_ENVIRONMENTS}" =~ project-[0-9]+ ]]
