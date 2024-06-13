794f6b1ab48f9f2b5e78dec9942237af7e6eef53
Zach Mitchell <zmitchell@fastmail.com>
2024-06-13 15:57:55 -0600

    feat: add initial support for resolution messages (#1621)
    ## Proposed Changes
    
    <!-- Describe the changes proposed in this pull request. -->
    <!-- Please provide links to any issue(s) which are expected to be
    resolved. -->
    This adds initial support for resolution messages returned by
    `catalog-server`.
    
    We now display the fallback message returned by `catalog-server`
    whenever there is a resolution error rather than displaying `package
    group had no page`.
    ```text
    ❌ ERROR: resolution failed: AttrPath 'systemd' not found for some systems, valid systems are (['aarch64-linux', 'x86_64-linux']).
    ```
    
    More user-friendly error messages and tests for certain messages will be
    delivered as part of https://github.com/flox/flox/issues/1482.
    
    ## Release Notes
    
    <!-- Describe any user facing changes. Use "N/A" if not applicable. -->
    N/A
    
    <!-- Many thanks! -->

--- cli/mk_data/src/generate.rs
+++ cli/mk_data/src/generate.rs
@@ -401,9 +401,6 @@ pub fn run_cmd(
         }
     }
 
-    // Don't want to accidentally merge response files
-    let _ = std::fs::remove_file(output_file);
-
     cmd.env("_FLOX_CATALOG_DUMP_RESPONSE_FILE", output_file);
     debug!("cmd: {:?}", cmd);
     let output = cmd.output().context("couldn't call command")?;
