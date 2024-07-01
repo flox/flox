e4051968e03dc3077656175d64fab862ee48f763
Dan Carley <dan.carley@gmail.com>
2024-07-01 15:51:06 +0100

    test(services): Cleanup output
    So that we don't spam the output for successful runs:
    
    - https://bats-core.readthedocs.io/en/stable/writing-tests.html#printing-to-the-terminal

--- test_data/input_data/services/check_python_http_server.sh
+++ test_data/input_data/services/check_python_http_server.sh
@@ -3,37 +3,37 @@
 set -eo pipefail
 
 function cleanup() {
-    echo "shutting down" >&3
+    echo "shutting down"
     "$PROCESS_COMPOSE_BIN" down -u "$SOCKET_FILE"
 }
 
-echo "activating" >&3
+echo "activating"
 eval $("$FLOX_BIN" activate)
 CONFIG_FILE="$FLOX_ENV/service-config.yaml"
 SOCKET_FILE="$PWD/service.sock"
 
-echo "checking python" >&3
-which python3 >&3
-python3 --version >&3
+echo "checking python"
+which python3
+python3 --version
 
-echo "work_dir: $PWD" >&3
-echo "socket_file: $SOCKET_FILE" >&3
-echo "config_file: $CONFIG_FILE" >&3
+echo "work_dir: $PWD"
+echo "socket_file: $SOCKET_FILE"
+echo "config_file: $CONFIG_FILE"
 
 # Start the server
 # https://bats-core.readthedocs.io/en/stable/writing-tests.html#file-descriptor-3-read-this-if-bats-hangs
-"$PROCESS_COMPOSE_BIN" up -f "$CONFIG_FILE" --tui=false -u "$SOCKET_FILE" 3>&- 2>&1 &
+"$PROCESS_COMPOSE_BIN" up -f "$CONFIG_FILE" --tui=false --use-uds --unix-socket "$SOCKET_FILE" 3>&- &
 trap cleanup EXIT
 
-echo "waiting for socket" >&3
+echo "waiting for socket"
 timeout 2s bash -c "while [ ! -e \"$SOCKET_FILE\" ]; do sleep 0.1; done"
 
 # Check if everything is up and running
-echo "checking socket" >&3
+echo "checking socket"
 [ -a "$SOCKET_FILE" ]
-echo "checking server" >&3
-curl --head --fail --silent localhost:7890 >&3
-echo "checking process-compose" >&3
+echo "checking server"
+curl --head --fail --silent localhost:7890
+echo "checking process-compose"
 status_output=$("$PROCESS_COMPOSE_BIN" process list -o json -u "$SOCKET_FILE")
 status=$(echo "$status_output" | jq -r -c '.[0].status')
 pid=$(echo "$status_output" | jq -r -c '.[0].pid')
