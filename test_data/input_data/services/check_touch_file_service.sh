f4dbc47a793ba7d2d0d1de05ed1ed72ce83b6ccb
Dan Carley <dan.carley@gmail.com>
2024-07-04 10:47:33 +0100

    test(services): Don't bg+sleep+cleanup
    We don't need to background `process-compose`, wait for it to have
    started, and then cleanup afterwards now that we've switched to using a
    non-network "service" because `process-compose` will exit on completion.
    
    It's wrapped in a 2s timeout, which will issue a kill signal if
    exceeded, to prevent the test hanging if `process-compose` does
    something unexpected.
    
    We still set the socket, even though we don't access it, because it
    exercises the CLI code path for setting the environment variable and if
    not set `process-compose` will default to binding a TCP socket which
    will conflict with multiple runs on the same host.

--- test_data/input_data/services/check_touch_file_service.sh
+++ test_data/input_data/services/check_touch_file_service.sh
@@ -2,11 +2,6 @@
 
 set -eo pipefail
 
-function cleanup() {
-    echo "shutting down"
-    "$PROCESS_COMPOSE_BIN" down -u "$SOCKET_FILE" || true
-}
-
 echo "activating"
 eval $("$FLOX_BIN" activate)
 CONFIG_FILE="$FLOX_ENV/service-config.yaml"
@@ -14,9 +9,7 @@ SOCKET_FILE="${_FLOX_SERVICES_SOCKET}"
 
 echo "starting the service"
 # https://bats-core.readthedocs.io/en/stable/writing-tests.html#file-descriptor-3-read-this-if-bats-hangs
-"$PROCESS_COMPOSE_BIN" up -f "$CONFIG_FILE" --tui=false --unix-socket "$SOCKET_FILE" 3>&- &
-sleep 1 # wait for the service to run
-trap cleanup EXIT
+timeout 2s "$PROCESS_COMPOSE_BIN" up -f "$CONFIG_FILE" --tui=false --unix-socket "$SOCKET_FILE" 3>&-
 
 echo "looking for file"
 [ -e hello.txt ]
