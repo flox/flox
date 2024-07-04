71f598e9435ea8990f2111c46b3fb1a57ea31797
Yannik Sander <yannik@floxdev.com>
2024-07-04 16:11:39 -0400

    feat: replace generated `MessageType` with a less strict one

---
+++ cli/catalog-api-v1/src/error.rs
@@ -0,0 +1,16 @@
+use std::hash::Hash;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub enum MessageType {
+    #[serde(rename = "general")]
+    General,
+    #[serde(rename = "resolution_trace")]
+    ResolutionTrace,
+    #[serde(rename = "attr_path_not_found")]
+    AttrPathNotFound,
+    #[serde(rename = "constraints_too_tight")]
+    ConstraintsTooTight,
+    Unknown(String),
+}
--- cli/catalog-api-v1/src/lib.rs
+++ cli/catalog-api-v1/src/lib.rs
@@ -4,4 +4,10 @@
 //! The spec is managed by the Catalog API team and is updated when the upstream API changes.
 
 mod client;
+mod error;
 pub use client::*;
+
+pub mod types {
+    pub use crate::error::MessageType;
+    pub use crate::client::types::*;
+}
