4350e9a0087c81dea072aacdba5e61d26357df87
Dan Carley <dan.carley@gmail.com>
2024-07-12 09:48:18 +0100

    test(services): Remove --keep-tui flag
    I left this in by accident when exploring ways to prevent
    `process-compose` from exiting with the last service. The flag is only
    used by the codepath that starts the TUI.

--- cli/tests/services/start_and_cleanup.sh
+++ cli/tests/services/start_and_cleanup.sh
@@ -15,7 +15,7 @@ function cleanup() {
 # TODO: Replace when we have `flox activate --start-services`.
 echo "Starting process-compose"
 # https://bats-core.readthedocs.io/en/stable/writing-tests.html#file-descriptor-3-read-this-if-bats-hangs
-"$PROCESS_COMPOSE_BIN" up --config "$SERVICE_CONFIG" --tui=false --keep-tui 3>&- &
+"$PROCESS_COMPOSE_BIN" up --config "$SERVICE_CONFIG" --tui=false 3>&- &
 
 # TODO: Replace when exiting the activation stops `process-compose`.
 trap cleanup EXIT
