913cab7106a8c20008333240dcc1c3a3fb2281dd
leigh capili <leigh@null.net>
2024-07-16 21:47:11 -0600

    fix: don't execute directories as editors

--- cli/flox/src/commands/edit.rs
+++ cli/flox/src/commands/edit.rs
@@ -355,7 +355,7 @@ impl Edit {
 
         let (path, editor) = env::split_paths(&path_var)
             .cartesian_product(["vim", "vi", "nano", "emacs"])
-            .find(|(path, editor)| path.join(editor).exists())
+            .find(|(path, editor)| path.join(editor).is_file())
             .context("no known editor found in $PATH")?;
 
         debug!("Using default editor {:?} from {:?}", editor, path);
