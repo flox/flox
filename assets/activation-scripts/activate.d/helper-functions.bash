26f957d743f070918c701075d492e4d447a7038d
Dan Carley <dan.carley@gmail.com>
2024-07-22 12:22:31 +0100

    fix(activate): Fix flox_portgrab args for nc
    The version of netcat that we get takes the port as a positional
    argument. The test was failing because it was unable to match the
    expected output against the usage output:
    
        % /nix/store/aj3i0mqh3cfp2yr16kn034s9c6p1mzbi-libressl-3.8.2-nc/bin/nc 0.0.0.0 -l 0 -n -v
        usage: nc [-46cDdFhklNnrStUuvz] [-C certfile] [-e name] [-H hash] [-I length]
    	     [-i interval] [-K keyfile] [-M ttl] [-m minttl] [-O length]
    	     [-o staplefile] [-P proxy_username] [-p source_port] [-R CAfile]
    	     [-s sourceaddr] [-T keyword] [-V rtable] [-W recvlimit] [-w timeout]
    	     [-X proxy_protocol] [-x proxy_address[:port]] [-Z peercertfile]
    	     [destination] [port]
    
    This was a bit of a pain to debug because:
    
    - changes to the scripts require a rebuild of the environment
    - the errors can swallowed by the redirect
    - debugging via `flox activate -v` is quite limited
    
    Which, combined with `on-activate` being run multiple times, makes me
    feel like we shouldn't release this helper just yet.

--- assets/activation-scripts/activate.d/helper-functions.bash
+++ assets/activation-scripts/activate.d/helper-functions.bash
@@ -7,15 +7,16 @@ function flox_portgrab() {
   # Usage: port="$(portgrab)"
   #
   # - invokes nc as _server_ in background with args:
-  #   0.0.0.0: bind to all interfaces
-  #   -l 0: listen on ephemeral port
+  #   -l: listen, rather than connect, mode
   #   -n: do not perform name resolution
   #   -v: be verbose (so we can identify bound port number)
+  #   0.0.0.0: bind to all interfaces
+  #   0: get an ephemeral port
   # - parses port number from expected output to stderr, e.g.:
   #     Listening on 0.0.0.0 41669
   # - invokes nc as _client_ to connect to server, which shuts it down
   local -a words
-  ( $_netcat/bin/nc 0.0.0.0 -l 0 -n -v & ) 2>&1 | while read -r -a words; do
+  ( $_netcat/bin/nc -lnv 0.0.0.0 0 & ) 2>&1 | while read -r -a words; do
     if [ ${#words[@]} -eq 4 ] && \
        [ "${words[0]}" = "Listening" ] && \
        [ "${words[1]}" = "on" ] && \
