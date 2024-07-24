3c1b215476aa1d423c5f82a4d9e253de18fbf85f
Tanja Ulianova <tanja@floxdev.com>
2024-07-25 01:43:28 +0200

    refactor(wording): Clarify messaging to specify install id
    Updated code and logs to use "install id" instead of "package" for better clarity.

--- cli/flox-rust-sdk/src/models/manifest.rs
+++ cli/flox-rust-sdk/src/models/manifest.rs
@@ -897,7 +897,7 @@ pub enum TomlEditError {
     /// The `[install]` table was missing entirely
     #[error("'install' table not found")]
     MissingInstallTable,
-    #[error("couldn't find package '{0}' in the manifest")]
+    #[error("couldn't find package with install id '{0}' in the manifest")]
     PackageNotFound(String),
     #[error("'options' must be a table, but found {0} instead")]
     MalformedOptionsTable(String),
@@ -1202,10 +1202,10 @@ pub fn insert_packages(
     })
 }
 
-/// Remove package names from the `[install]` table of a manifest
+/// Remove package names from the `[install]` table of a manifest based on their install IDs.
 pub fn remove_packages(
     manifest_contents: &str,
-    pkgs: &[String],
+    install_ids: &[String],
 ) -> Result<DocumentMut, TomlEditError> {
     debug!("attempting to remove packages from the manifest");
     let mut toml = manifest_contents
@@ -1216,7 +1216,7 @@ pub fn remove_packages(
     let installs_table = {
         let installs_field = toml
             .get_mut("install")
-            .ok_or(TomlEditError::PackageNotFound(pkgs[0].clone()))?;
+            .ok_or(TomlEditError::PackageNotFound(install_ids[0].clone()))?;
 
         let type_name = installs_field.type_name().into();
 
@@ -1225,14 +1225,14 @@ pub fn remove_packages(
             .ok_or(TomlEditError::MalformedInstallTable(type_name))?
     };
 
-    for pkg in pkgs {
-        debug!("checking for presence of package '{pkg}'");
-        if !installs_table.contains_key(pkg) {
-            debug!("package '{pkg}' wasn't found");
-            return Err(TomlEditError::PackageNotFound(pkg.clone()));
+    for id in install_ids {
+        debug!("checking for presence of package '{id}'");
+        if !installs_table.contains_key(id) {
+            debug!("package with install id '{id}' wasn't found");
+            return Err(TomlEditError::PackageNotFound(id.clone()));
         } else {
-            installs_table.remove(pkg);
-            debug!("package '{pkg}' was removed");
+            installs_table.remove(id);
+            debug!("package with install id '{id}' was removed");
         }
     }
 
