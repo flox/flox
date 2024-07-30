0b66f6ef2857b1055bff5aeba983017e255215f3
Zach Mitchell <zmitchell@fastmail.com>
2024-07-30 10:28:36 -0600

    fix: include flox-klaus in flox pkg build

--- pkgs/flox/default.nix
+++ pkgs/flox/default.nix
@@ -5,6 +5,7 @@
   symlinkJoin,
   makeBinaryWrapper,
   flox-pkgdb,
+  flox-klaus,
   flox-cli,
   flox-manpages,
   process-compose,
@@ -30,18 +31,12 @@ in
     nativeBuildInputs = [makeBinaryWrapper];
 
     postBuild = ''
-      wrapProgram $out/bin/klaus \
-        ${lib.optionalString (SENTRY_DSN != null) "--set FLOX_SENTRY_DSN \"${SENTRY_DSN}\" "} \
-        ${lib.optionalString (SENTRY_ENV != null) "--set FLOX_SENTRY_ENV \"${SENTRY_ENV}\" "} \
-        --set PROCESS_COMPOSE_BIN "${process-compose}/bin/process-compose" \
-        --set FLOX_VERSION    "${version}"
-
       wrapProgram $out/bin/flox \
         ${lib.optionalString (SENTRY_DSN != null) "--set FLOX_SENTRY_DSN \"${SENTRY_DSN}\" "} \
         ${lib.optionalString (SENTRY_ENV != null) "--set FLOX_SENTRY_ENV \"${SENTRY_ENV}\" "} \
         --set PKGDB_BIN       "${flox-pkgdb}/bin/pkgdb" \
         --set FLOX_BIN        "${flox-cli}/bin/flox" \
-        --set KLAUS_BIN       "${flox-cli}/bin/klaus" \
+        --set KLAUS_BIN       "${flox-klaus}/bin/klaus" \
         --set PROCESS_COMPOSE_BIN "${process-compose}/bin/process-compose" \
         --set FLOX_VERSION    "${version}"
     '';
