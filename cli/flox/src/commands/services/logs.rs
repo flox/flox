f3ecc10a165024b36f7c73a7b5b4b4a8d0d933df
Matthew Kenigsberg <matthew@floxdev.com>
2024-08-16 15:49:51 -0600

    Revert "fix: flush stdout"
    This reverts commit 91484795233097590e311dbc93a6f45d57e2d40a.

--- cli/flox/src/commands/services/logs.rs
+++ cli/flox/src/commands/services/logs.rs
@@ -1,5 +1,3 @@
-use std::io::{self, Write};
-
 use anyhow::{bail, Result};
 use bpaf::Bpaf;
 use flox_rust_sdk::flox::Flox;
@@ -53,7 +51,6 @@ impl Logs {
             for log in log_stream {
                 let ProcessComposeLogLine { process, message } = log?;
                 println!("{process:<max_name_length$}: {message}",);
-                io::stdout().flush().unwrap();
             }
         } else {
             let [ref name] = self.names.as_slice() else {
