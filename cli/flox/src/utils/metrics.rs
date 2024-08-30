4a354a55ba2782c32912999c4c5c738d933dab4a
Dan Carley <dan.carley@gmail.com>
2024-08-30 14:39:45 +0100

    fix(activate): Prevent stray TRACE log
    Fixes the following since e7b0e893:
    
        (2) ~/projects/flox/flox on HEAD (eabea829) [$]
        % flox activate -r dcarley/tailscale
        2024-08-30T13:45:57.839901Z TRACE flox_command: subcommand="activate#version" lockfile_version=1
        ✅ You are now using the environment 'dcarley/tailscale (remote)'.
        To stop using this environment, type 'exit'
    
    This would happen for any environment that requires building at the same
    time as activation, such as a remote environment, where the calls within
    or below `environment.lockfile()` generate their own tracing events.
    
    It can also be reproduced with a much simpler:
    
        fn test() -> &'static str {
            tracing::debug!("test");
            "test"
        }
        subcommand_metric!("activate#version", lockfile_version = test());
    
    Moving the function outside of the macro fixes this:
    
        (2) ~/projects/flox/flox on dcarley/2017-fix-activate-stray-log [$>]
        % flox activate -r dcarley/tailscale
        ✅ You are now using the environment 'dcarley/tailscale (remote)'.
        To stop using this environment, type 'exit'

--- cli/flox/src/utils/metrics.rs
+++ cli/flox/src/utils/metrics.rs
@@ -34,6 +34,9 @@ pub const METRICS_EVENTS_API_KEY: &str = env!("METRICS_EVENTS_API_KEY");
 
 /// Creates a trace event for the given subcommand.
 ///
+/// Do NOT inline functions as fields values because any child tracing events
+/// will cause this event to be logged at TRACE level for reasons unknown.
+///
 /// We set the target to `flox_command` so that we can filter for these exact events.
 #[macro_export]
 macro_rules! subcommand_metric {
