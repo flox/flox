8b1ed2496ca5048f2e42e216523526d43be99c68
Tom Bereknyei <tomberek@gmail.com>
2024-09-02 18:54:59 -0400

    chore: move pre-commit-check into pkgs

---
+++ pkgs/pre-commit-check/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  pre-commit-hooks,
+  symlinkJoin,
+  system,
+  rustfmt,
+  fenix,
+  rust-toolchain,
+  clang-tools_16,
+  makeWrapper,
+}:
+pre-commit-hooks.lib.${system}.run {
+  src = builtins.path {path = ./.;};
+  default_stages = [
+    "manual"
+    "push"
+  ];
+  hooks = {
+    alejandra.enable = true;
+    clang-format = {
+      enable = true;
+      types_or = lib.mkForce [
+        "c"
+        "c++"
+      ];
+    };
+    rustfmt = let
+      wrapper = symlinkJoin {
+        name = "rustfmt-wrapped";
+        paths = [rustfmt];
+        nativeBuildInputs = [makeWrapper];
+        postBuild = let
+          # Use nightly rustfmt
+          PATH = lib.makeBinPath [
+            fenix.stable.cargo
+            rustfmt
+          ];
+        in ''
+          wrapProgram $out/bin/cargo-fmt --prefix PATH : ${PATH};
+        '';
+      };
+    in {
+      enable = true;
+      entry = lib.mkForce "${wrapper}/bin/cargo-fmt fmt --all --manifest-path 'cli/Cargo.toml' -- --color always";
+    };
+    clippy.enable = true;
+    commitizen.enable = true;
+    shfmt.enable = false;
+    # shellcheck.enable = true; # disabled until we have time to fix all the warnings
+  };
+  settings = {
+    clippy.denyWarnings = true;
+    alejandra.verbosity = "quiet";
+    rust.cargoManifestPath = "cli/Cargo.toml";
+  };
+  tools = {
+    # use fenix provided clippy
+    clippy = rust-toolchain.clippy;
+    cargo = rust-toolchain.cargo;
+    clang-tools = clang-tools_16;
+  };
+}
