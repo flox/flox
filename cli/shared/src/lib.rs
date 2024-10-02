866bc1ab67668d2641f0da2882859b1107e16169
Matthew Kenigsberg <matthew@floxdev.com>
2024-10-02 15:15:17 -0600

    refactor: move traceable_path to shared crate

--- cli/shared/src/lib.rs
+++ cli/shared/src/lib.rs
@@ -63,3 +63,9 @@ where
         .map_err(SerializeError::RenameTmpFile)?;
     Ok(())
 }
+
+/// Returns a `tracing`-compatible form of a [Path]
+pub fn traceable_path(p: impl AsRef<Path>) -> impl tracing::Value {
+    let path = p.as_ref();
+    path.display().to_string()
+}
