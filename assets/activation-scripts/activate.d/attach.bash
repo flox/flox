114fe9409900e03fd9d1d2dd475d5cc3085d6b68
Dan Carley <dan.carley@gmail.com>
2024-10-07 16:31:25 +0100

    refactor(activate): Extract {start/attach}.bash
    To make it easier to reason about and match the architecture in:
    
    - https://docs.google.com/document/d/1XLpxgJx-x_yBFCQVyRy6QKDMa27UkrB27QssMXhqL7M/edit#heading=h.t56y6l97pj85
    - https://www.mermaidchart.com/raw/349e994c-c9ec-4dbf-8c74-454872f2a55c?theme=light&version=v0.1&format=svg
    
    Other options considered:
    
    - functions in the existing `activate` script would have still made the
      script very long and hard to read
    - calling the files as separate scripts would have required a large
      refactoring because there are a lot of implicit variables
    
    No changes were made to the extracted script other than stripping the
    leading two space indents now that they're not nested within conditions.
    
    Newly sourced scripts are validated by ShellCheck after the changes in a
    preceding commit.

---
+++ assets/activation-scripts/activate.d/attach.bash
@@ -0,0 +1,23 @@
+# "Reactivation" of this environment.
+
+# If we're attempting to launch an interactive shell then just print a
+# message to say that the environment has already been activated.
+if [ -t 1 ] && [ $# -eq 0 ]; then
+  # TODO: should this be in Rust using message::error?
+  echo "❌ ERROR: Environment '$FLOX_ENV_DESCRIPTION' is already active." >&2
+  exit 1
+fi
+
+# Assert that the expected _{add,del}_env variables are present.
+if [ -z "$_add_env" ] || [ -z "$_del_env" ]; then
+  echo "ERROR (activate): \$_add_env and \$_del_env not found in environment" >&2;
+  if [ -h "$FLOX_ENV" ]; then
+    echo "moving $FLOX_ENV link to $FLOX_ENV.$$ - please try again" >&2;
+    $_coreutils/bin/mv "$FLOX_ENV" "$FLOX_ENV.$$"
+  fi
+  exit 1;
+fi
+
+# Replay the environment for the benefit of this shell.
+eval "$($_gnused/bin/sed -e 's/^/unset /' -e 's/$/;/' "$_del_env")"
+eval "$($_gnused/bin/sed -e 's/^/export /' -e 's/$/;/' "$_add_env")"
