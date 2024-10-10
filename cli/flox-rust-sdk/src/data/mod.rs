da5a23621b957a674d72cc4b1d1f7af10b97d4f2
Matthew Kenigsberg <matthew@floxdev.com>
2024-10-10 13:46:16 -0600

    fix: bad rebase

--- cli/flox-rust-sdk/src/data/mod.rs
+++ cli/flox-rust-sdk/src/data/mod.rs
@@ -1,12 +1,9 @@
-mod canonical_path;
 mod flox_version;
-mod version;
 
 use std::fmt::Display;
 
-pub use canonical_path::{CanonicalPath, CanonicalizeError};
+pub use flox_core::canonical_path::{CanonicalPath, CanonicalizeError};
 pub use flox_version::FloxVersion;
-pub use version::Version;
 pub type System = String;
 
 /// Different representations of the same attribute path
