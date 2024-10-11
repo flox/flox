89a17295fadbef012f82c6bc59405b3d9f7427d3
Yannik Sander <yannik@floxdev.com>
2024-10-11 15:13:36 +0200

    refactor(containerze): registry argument handling
    * refactor: define a registry argument as a type
    * refactor: factor out creation of registry writer
    * refactor(cli): either load into registry or write to file

--- cli/flox/src/commands/containerize.rs
+++ cli/flox/src/commands/containerize.rs
@@ -1,7 +1,9 @@
+use std::fmt::Display;
 use std::fs;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+use std::str::FromStr;
 
 use anyhow::{Context, Result};
 use bpaf::Bpaf;
@@ -14,37 +16,93 @@ use crate::subcommand_metric;
 use crate::utils::dialog::{Dialog, Spinner};
 use crate::utils::message;
 
-fn is_accepted_registry(reg: &Option<String>) -> bool {
-    return match reg.as_deref() {
-        Some("docker") | Some("podman") => true,
-        Some(_) => false,
-        None => true, // Since it's optional, it's fine to not have it
-    };
-}
-
 // Containerize an environment
 #[derive(Bpaf, Clone, Debug)]
 pub struct Containerize {
     #[bpaf(external(environment_select), fallback(Default::default()))]
     environment: EnvironmentSelect,
 
-    /// Path to write the container to (pass '-' to write to stdout)
-    #[bpaf(short, long, argument("path"))]
-    output: Option<PathBuf>,
-
     /// Tag to apply to the container, defaults to 'latest'
     #[bpaf(short, long, argument("tag"))]
     tag: Option<String>,
 
-    /// Which container registry to load the container into
     #[bpaf(
-        short,
-        long("load-into-registry"),
-        argument("registry"),
-        guard(is_accepted_registry, "'--load-into-registry' must be docker or podman")
+        external(output_file_or_registry),
+        fallback(OutputFileOrRegistry::OutputFile(None))
     )]
-    load_into_registry: Option<String>,
+    output: OutputFileOrRegistry,
 }
+
+#[derive(Debug, Clone, Bpaf)]
+enum OutputFileOrRegistry {
+    OutputFile(
+        #[bpaf(
+            short('o'),
+            long("output"),
+            argument("path"),
+            help("Path to write the container to (pass '-' to write to stdout)")
+        )]
+        Option<PathBuf>,
+    ),
+    Registry(
+        #[bpaf(
+            short('l'),
+            long("load-into-registry"),
+            argument("registry"),
+            help("Which container registry to load the container into")
+        )]
+        Registry,
+    ),
+}
+
+/// The container registry to load the container into
+/// Currently only supports Docker and Podman
+#[derive(Debug, Clone)]
+enum Registry {
+    Docker,
+    Podman,
+}
+
+impl Registry {
+    /// Get a writer to the registry,
+    /// Essentially spawns a `docker load` or `podman load` process
+    /// and returns a handle to its stdin.
+    fn to_writer(&self) -> Result<impl Write> {
+        let cmd = match self {
+            Registry::Docker => "docker",
+            Registry::Podman => "podman",
+        };
+
+        let mut child = Command::new(cmd)
+            .arg("load")
+            .stdin(Stdio::piped())
+            .spawn()?;
+
+        Ok(child.stdin.take().expect("stdin is piped"))
+    }
+}
+
+impl Display for Registry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Registry::Docker => write!(f, "Docker registry"),
+            Registry::Podman => write!(f, "Podman registry"),
+        }
+    }
+}
+
+impl FromStr for Registry {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "docker" => Ok(Registry::Docker),
+            "podman" => Ok(Registry::Podman),
+            _ => Err("Registry must be 'docker' or 'podman'".to_string()),
+        }
+    }
+}
+
 impl Containerize {
     #[instrument(name = "containerize", skip_all)]
     pub async fn handle(self, flox: Flox) -> Result<()> {
@@ -55,34 +113,28 @@ impl Containerize {
             .detect_concrete_environment(&flox, "Upgrade")?
             .into_dyn_environment();
 
-        let output_path = match self.output {
-            Some(output) => output,
-            None => std::env::current_dir()
-                .context("Could not get current directory")?
-                .join(format!("{}-container.tar", env.name())),
-        };
-
         let output_tag: &str = match self.tag {
             Some(tag) => &tag.to_string(),
             None => "latest",
         };
 
-        let (output, output_name): (Box<dyn Write + Send>, String) =
-            if let Some(registry) = self.load_into_registry {
-                let command = Command::new(&registry)
-                    .arg("load")
-                    .stdin(Stdio::piped())
-                    .spawn()
-                    .context("Could not start registry load command")?;
-
-                (Box::new(command.stdin.unwrap()), registry.to_string())
-            } else if output_path == Path::new("-") {
+        let (output, output_name): (Box<dyn Write + Send>, String) = match self.output {
+            // If the output file is '-', we write to stdout
+            OutputFileOrRegistry::OutputFile(Some(file)) if file == Path::new("-") => {
                 debug!("output=stdout");
 
                 (Box::new(std::io::stdout()), "stdout".to_string())
-            } else {
-                debug!("output={}", output_path.display());
-
+            },
+            // If the output file is None, we write to a file in the current directory
+            // with the name of the environment.
+            // Otherwise, we write to the specified file.
+            OutputFileOrRegistry::OutputFile(output_path) => {
+                let output_path = match output_path {
+                    Some(output) => output,
+                    None => std::env::current_dir()
+                        .context("Could not get current directory")?
+                        .join(format!("{}-container.tar", env.name())),
+                };
                 let file = fs::OpenOptions::new()
                     .write(true)
                     .create(true)
@@ -91,7 +143,13 @@ impl Containerize {
                     .context("Could not open output file")?;
 
                 (Box::new(file), output_path.display().to_string())
-            };
+            },
+            // If the registry is specified, we write to the registry directly instead of a file
+            OutputFileOrRegistry::Registry(registry) => {
+                let writer = registry.to_writer()?;
+                (Box::new(writer), registry.to_string())
+            },
+        };
 
         let builder = Dialog {
             message: &format!("Building container for environment {}...", env.name()),
