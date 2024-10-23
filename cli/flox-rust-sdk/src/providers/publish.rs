b856f7c0e96e879a4c2e4f4e67ecd519c6c6584c
Bill LeVine <bill@floxdev.com>
2024-10-23 14:37:25 -0400

    chore: address review comments

--- cli/flox-rust-sdk/src/providers/publish.rs
+++ cli/flox-rust-sdk/src/providers/publish.rs
@@ -22,13 +22,13 @@ pub trait Publish {
     fn publish(
         &self,
         client: Client,
-        builder: &dyn ManifestBuilder,
+        builder: impl ManifestBuilder,
         environment: PublishEnvironment,
         package: String,
     ) -> Result<(), String>;
 }
 
-/// Ensures that the required metadata for publishing is consistent
+/// Ensures that the required metadata for publishing is consistent from the environment
 #[allow(clippy::manual_non_exhaustive)]
 pub struct CheckedEnvironmentMetadata {
     pub git_metadata: GitCommandProvider,
@@ -40,58 +40,14 @@ pub struct CheckedEnvironmentMetadata {
     _private: (),
 }
 
-impl From<&PathEnvironment> for CheckedEnvironmentMetadata {
-    fn from(_value: &PathEnvironment) -> Self {
-        // Ensures current commit is in remote
-        // Gathers of the info needed for publish
-        // ... locked_url, rev, rev_count, rev_date of the build repo from git
-        // ... locked_url, rev, rev_count, rev_date of the base catalog from the
-        //         lockfile of the build repo
-        // ... version, description, name, attr_path from manifest
-        // ... catalog from current git auth handle
-        todo!()
-    }
-}
-impl From<&ManagedEnvironment> for CheckedEnvironmentMetadata {
-    fn from(_value: &ManagedEnvironment) -> Self {
-        // This will come second, after the path environment is implemented
-        todo!()
-    }
-}
-
-// Represents the metadata for publish that comes from the build
-pub struct PublishableBuild {
+/// Ensures that the required metadata for publishing is consistent from the build process
+#[allow(clippy::manual_non_exhaustive)]
+pub struct CheckedBuildMetadata {
     // Define metadata coming from the build, e.g. outpaths
-}
 
-// Implement .From for various types of builders
-impl From<&dyn ManifestBuilder> for PublishableBuild {
-    fn from(_value: &dyn ManifestBuilder) -> Self {
-        // Build (if needed) and collect the meta data needed to publish
-
-        // Access a `flox build` builder
-        // ... to clone into a sandbox and run
-        // ... `flox activate; flox build;`
-        // prepublish_build();
-        // _sandbox = temp dir
-        // Use GitCommandProvider to get remote and current rev of build_repo
-        // Use GitCommandProvider to clone that remote/rev to _sandbox
-        //   this will ensure it's in the remote
-        // Load the manifest from the .flox of that repo to get the version/description/catalog
-        // Confirm access to remote resources, login, etc, so as to not waste
-        //   time if it's going to fail later or require user interaction to
-        // continue the operation.
-
-        // Obtains info from the build process (the sandboxed one above)
-        // ... base_catalog_{locked_url, rev, rev_count, rev_date}
-        // ... system, drv_path (??), outputs (??)
-        // gather_build_info(_sandbox: &Path) {
-        // Access lockfile of _sandbox to get base_catalog_{locked_url, rev, rev_count, rev_date}
-        // populate the following:
-        //      how do we get the drv_path, outputs(and paths), system from this?
-
-        todo!()
-    }
+    // This field isn't "pub", so no one outside this module can construct this struct. That helps
+    // ensure that we can only make this struct as a result of doing the "right thing."
+    _private: (),
 }
 
 /// The `PublishProvider` is a concrete implementation of the `Publish` trait.
@@ -123,20 +79,17 @@ where
     fn publish(
         &self,
         _client: Client,
-        builder: &dyn ManifestBuilder,
+        builder: impl ManifestBuilder,
         environment: PublishEnvironment,
         _package: String,
     ) -> Result<(), String> {
-        let env = match environment {
+        let _env_meta = match environment {
             PublishEnvironment::Managed(_env) => Err(PublishError::UnsupportedEnvironment),
-            PublishEnvironment::Path(env) => Ok(env),
+            PublishEnvironment::Path(env) => Ok(check_environment_metadata(env)),
         };
 
-        let _checked_meta: CheckedEnvironmentMetadata =
-            CheckedEnvironmentMetadata::from(&env.unwrap());
-
         // How to access "Builder" from obove?
-        let _build_meta: PublishableBuild = PublishableBuild::from(builder);
+        let _build_meta = check_builder_metadata(builder);
 
         // Uses client to...
         // ... check access to the catalog
@@ -145,4 +98,43 @@ where
         // publish_to_catalog()
         todo!()
     }
+}
+
+fn check_builder_metadata(_builder: impl ManifestBuilder) -> Result<CheckedBuildMetadata, String> {
+    // Build (if needed) and collect the meta data needed to publish
+
+    // Access a `flox build` builder
+    // ... to clone into a sandbox and run
+    // ... `flox activate; flox build;`
+    // prepublish_build();
+    // _sandbox = temp dir
+    // Use GitCommandProvider to get remote and current rev of build_repo
+    // Use GitCommandProvider to clone that remote/rev to _sandbox
+    //   this will ensure it's in the remote
+    // Load the manifest from the .flox of that repo to get the version/description/catalog
+    // Confirm access to remote resources, login, etc, so as to not waste
+    //   time if it's going to fail later or require user interaction to
+    // continue the operation.
+
+    // Obtains info from the build process (the sandboxed one above)
+    // ... base_catalog_{locked_url, rev, rev_count, rev_date}
+    // ... system, drv_path (??), outputs (??)
+    // gather_build_info(_sandbox: &Path) {
+    // Access lockfile of _sandbox to get base_catalog_{locked_url, rev, rev_count, rev_date}
+    // populate the following:
+    //      how do we get the drv_path, outputs(and paths), system from this?
+    todo!()
+}
+
+fn check_environment_metadata(
+    _environment: PathEnvironment,
+) -> Result<CheckedEnvironmentMetadata, String> {
+    // Ensures current commit is in remote
+    // Gathers of the info needed for publish
+    // ... locked_url, rev, rev_count, rev_date of the build repo from git
+    // ... locked_url, rev, rev_count, rev_date of the base catalog from the
+    //         lockfile of the build repo
+    // ... version, description, name, attr_path from manifest
+    // ... catalog from current git auth handle
+    todo!()
 }
