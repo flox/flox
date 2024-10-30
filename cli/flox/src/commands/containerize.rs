9caf028beee4c65dfdaaf36c1e146d4870458c41
Yannik Sander <yannik@floxdev.com>
2024-10-30 14:56:01 +0100

    feat(containerize): write container images to file or runtime
    Replace the `--output` flag for separate flags to write to a file (`--file|-f`)
    or a container runtime (`--runtime`).
    Add support for `Docker CLI` compatible runtimes, currently `docker` and `podman`.

--- cli/flox/src/commands/containerize.rs
+++ cli/flox/src/commands/containerize.rs
@@ -1,11 +1,13 @@
+use std::fmt::Display;
 use std::fs;
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+use std::str::FromStr;
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use bpaf::Bpaf;
 use flox_rust_sdk::flox::Flox;
-use log::debug;
 use tracing::instrument;
 
 use super::{environment_select, EnvironmentSelect};
@@ -19,9 +21,9 @@ pub struct Containerize {
     #[bpaf(external(environment_select), fallback(Default::default()))]
     environment: EnvironmentSelect,
 
-    /// Path to write the container to (pass '-' to write to stdout)
-    #[bpaf(short, long, argument("path"))]
-    output: Option<PathBuf>,
+    /// Output to write the container to, defaults to '{environment}-container.tar'
+    #[bpaf(external(output_target), optional)]
+    output: Option<OutputTarget>,
 
     /// Tag to apply to the container, defaults to 'latest'
     #[bpaf(short, long, argument("tag"))]
@@ -34,38 +36,17 @@ impl Containerize {
 
         let mut env = self
             .environment
-            .detect_concrete_environment(&flox, "Upgrade")?
+            .detect_concrete_environment(&flox, "Containerize")?
             .into_dyn_environment();
 
-        let output_path = match self.output {
-            Some(output) => output,
-            None => std::env::current_dir()
-                .context("Could not get current directory")?
-                .join(format!("{}-container.tar", env.name())),
-        };
+        let output = self
+            .output
+            .unwrap_or_else(|| OutputTarget::detect_or_default(env.name().as_ref()));
 
         let output_tag: &str = match self.tag {
             Some(tag) => &tag.to_string(),
             None => "latest",
         };
-
-        let (output, output_name): (Box<dyn Write + Send>, String) =
-            if output_path == Path::new("-") {
-                debug!("output=stdout");
-
-                (Box::new(std::io::stdout()), "stdout".to_string())
-            } else {
-                debug!("output={}", output_path.display());
-
-                let file = fs::OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .truncate(true)
-                    .open(&output_path)
-                    .context("Could not open output file")?;
-
-                (Box::new(file), output_path.display().to_string())
-            };
 
         let builder = Dialog {
             message: &format!("Building container for environment {}...", env.name()),
@@ -75,13 +56,140 @@ impl Containerize {
         .spin()?;
 
         Dialog {
-            message: &format!("Writing container to '{output_name}'"),
+            message: &format!("Writing container to {output}...",),
             help_message: None,
-            typed: Spinner::new(|| builder.stream_container(output)),
+            typed: Spinner::new(|| {
+                let writer = output.to_writer()?;
+                builder.stream_container(writer)?;
+                anyhow::Ok(())
+            }),
         }
         .spin()?;
 
-        message::created(format!("Container written to '{output_name}'"));
+        message::created(format!("Container written to {output}"));
         Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Bpaf)]
+enum OutputTarget {
+    File(
+        #[bpaf(
+            long("file"),
+            short('f'),
+            argument("file"),
+            help("File to write the container image to. '-' to write to stdout.")
+        )]
+        PathBuf,
+    ),
+    Runtime(
+        #[bpaf(
+            long("runtime"),
+            argument("runtime"),
+            help("Container runtime to load the image into. 'docker' or 'podman'")
+        )]
+        Runtime,
+    ),
+}
+
+impl OutputTarget {
+    fn detect_or_default(env_name: impl AsRef<str>) -> Self {
+        OutputTarget::File(PathBuf::from(format!(
+            "{}-container.tar",
+            env_name.as_ref()
+        )))
+    }
+
+    fn to_writer(&self) -> Result<impl Write> {
+        let writer: Box<dyn Write> = match self {
+            OutputTarget::File(path) => {
+                let path = match path {
+                    path if path == Path::new("-") => Path::new("/dev/stdout"),
+                    path => path,
+                };
+
+                let file = fs::OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(path)
+                    .context("Could not open output file")?;
+
+                Box::new(file)
+            },
+            OutputTarget::Runtime(runtime) => Box::new(runtime.to_writer()?),
+        };
+
+        Ok(writer)
+    }
+}
+
+impl Display for OutputTarget {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OutputTarget::File(path) => write!(f, "file '{}'", path.display()),
+            OutputTarget::Runtime(runtime) => write!(f, "{runtime}"),
+        }
+    }
+}
+
+/// The container registry to load the container into
+/// Currently only supports Docker and Podman
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Runtime {
+    Docker,
+    Podman,
+}
+
+impl Runtime {
+    /// Get a writer to the registry,
+    /// Essentially spawns a `docker load` or `podman load` process
+    /// and returns a handle to its stdin.
+    fn to_writer(&self) -> Result<impl Write> {
+        let cmd = match self {
+            Runtime::Docker => "docker",
+            Runtime::Podman => "podman",
+        };
+
+        let mut child = Command::new(cmd)
+            .arg("load")
+            .stdin(Stdio::piped())
+            .spawn()
+            .context(format!("Failed to call runtime {cmd}"))?;
+
+        Ok(child.stdin.take().expect("stdin is piped"))
+    }
+}
+
+impl Display for Runtime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Runtime::Docker => write!(f, "Docker runtime"),
+            Runtime::Podman => write!(f, "Podman runtime"),
+        }
+    }
+}
+
+impl FromStr for Runtime {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "docker" => Ok(Runtime::Docker),
+            "podman" => Ok(Runtime::Podman),
+            _ => Err(anyhow!("Registry must be 'docker' or 'podman'")),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn runtime_parse() {
+        "docker".parse::<Runtime>().unwrap();
+        "podman".parse::<Runtime>().unwrap();
+        assert!("invalid".parse::<Runtime>().is_err());
     }
 }
