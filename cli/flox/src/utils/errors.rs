dc93488828c42a0ee4fdb49c4f1705c16bd1411e
Yannik Sander <yannik@floxdev.com>
2024-11-07 14:48:18 +0100

    refactor: replace `out_path` with `rendered_env_links` in remote env
    feat: migrate gcroots of remote environments
    
    Remote environments use a ManagedEnvironment to manage the environment.
    Previously we linked a gcroot to `<path of managed environment>/run`.
    Note, that this was a single link.
    For consistency with the new more consistent naming scheme,
    we want to create links in a `<path of managed environment>/run` __directory__!
    
    As such we need to migrate the single link by deleting it,
    so we can create a directory in the step following.

--- cli/flox/src/utils/errors.rs
+++ cli/flox/src/utils/errors.rs
@@ -685,6 +685,7 @@ pub fn format_remote_error(err: &RemoteEnvironmentError) -> String {
         RemoteEnvironmentError::ReadInternalOutLink(_) => display_chain(err),
         RemoteEnvironmentError::DeleteOldOutLink(_) => display_chain(err),
         RemoteEnvironmentError::WriteNewOutlink(_) => display_chain(err),
+        RemoteEnvironmentError::CreateGcRootDir(_) => display_chain(err),
     }
 }
 
