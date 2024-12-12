9afbe16ef9eb03c1692e9a3f04d2f40310dd270b
Zach Mitchell <zmitchell@fastmail.com>
2024-12-11 17:09:31 -0700

    feat: use flox-core proc status in watchdog

--- cli/flox-watchdog/src/process.rs
+++ cli/flox-watchdog/src/process.rs
@@ -9,18 +9,17 @@
 //! API, but mostly for build-complexity reasons.
 
 use std::collections::HashSet;
-use std::fs::read_to_string;
 use std::path::PathBuf;
-use std::process::Command;
 use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::{bail, Result};
 use flox_core::activations::{read_activations_json, Activations, AttachedPid, UncheckedVersion};
+use flox_core::proc_status::pid_is_running;
 use fslock::LockFile;
 use time::OffsetDateTime;
-use tracing::{debug, trace, warn};
+use tracing::trace;
 /// How long to wait between watcher updates.
 pub const WATCHER_SLEEP_INTERVAL: Duration = Duration::from_millis(100);
 
@@ -46,22 +45,6 @@ pub trait Watcher {
     /// Returns true if the watcher determines that it's time to perform
     /// cleanup.
     fn should_clean_up(&self) -> Result<bool, Error>;
-}
-
-/// The state that a process is in.
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum ProcStatus {
-    /// The process is running (or runnable, which includes "idle").
-    Running,
-    /// The process has exited, but has not been cleaned up by the parent.
-    Zombie,
-    /// Process is dead and will transition to a zombie or disappear.
-    /// Technically we shouldn't see this, but just in case:
-    /// https://unix.stackexchange.com/a/653370
-    AboutToBeZombie,
-    /// The process has terminated and been cleaned up. This is also the fallback
-    /// for when there is an error reading the process status.
-    Dead,
 }
 
 #[derive(Debug)]
@@ -91,96 +74,6 @@ impl PidWatcher {
         }
     }
 
-    /// Reads the state of a process on macOS using `/bin/ps`, which can report
-    /// whether a process is a zombie. This is a stopgap until we someday use
-    /// `libproc`. Any failure is interpreted as an indication that the process
-    /// is no longer running.
-    #[cfg_attr(not(target_os = "macos"), allow(dead_code))]
-    fn read_pid_status_macos(pid: i32) -> ProcStatus {
-        let stdout = match Command::new("/bin/ps")
-            .args(["-o", "state=", "-p"])
-            .arg(format!("{pid}"))
-            .output()
-        {
-            Ok(output) => output.stdout,
-            Err(err) => {
-                warn!(
-                    %err,
-                    pid,
-                    "failed while calling /bin/ps, treating as not running"
-                );
-                return ProcStatus::Dead;
-            },
-        };
-        if let Some(state) = stdout.first() {
-            match state {
-                // '?' means "unknown" from `ps` included with macOS. Note that
-                // this is *not the same* as `procps` on Linux or from Nixpkgs.
-                b'Z' | b'?' => ProcStatus::Zombie,
-                _ => ProcStatus::Running,
-            }
-        } else {
-            debug!(pid, "no output from /bin/ps, treating as not running");
-            ProcStatus::Dead
-        }
-    }
-
-    /// Tries to read the state of a process on Linux via `/proc`. Any failure
-    /// is interpreted as an indication that the process is no longer running.
-    #[cfg_attr(not(target_os = "linux"), allow(dead_code))]
-    fn read_pid_status_linux(pid: i32) -> ProcStatus {
-        let path = format!("/proc/{pid}/stat");
-        let stat = match read_to_string(path) {
-            Ok(stat) => stat,
-            Err(err) => {
-                trace!(
-                    %err,
-                    pid,
-                    "failed to parse /proc/<pid>/stat, treating as not running"
-                );
-                return ProcStatus::Dead;
-            },
-        };
-        // `/proc/{pid}/stat` has space separated values `pid comm state ...`
-        // and we need to extract state
-        if let Some(state) = stat
-            .split_whitespace()
-            .nth(2)
-            .and_then(|chars| chars.as_bytes().first())
-        {
-            match state {
-                b'X' | b'x' => ProcStatus::AboutToBeZombie,
-                b'Z' => ProcStatus::Zombie,
-                _ => ProcStatus::Running,
-            }
-        } else {
-            warn!(
-                pid,
-                "failed to parse /proc/<pid>/stat, treating as not running"
-            );
-            ProcStatus::Dead
-        }
-    }
-
-    /// Returns the status of the provided PID.
-    fn read_pid_status(pid: i32) -> ProcStatus {
-        #[cfg(not(any(target_os = "linux", target_os = "macos")))]
-        panic!("unsupported operating system");
-
-        #[cfg(target_os = "linux")]
-        let status = Self::read_pid_status_linux(pid);
-
-        #[cfg(target_os = "macos")]
-        let status = Self::read_pid_status_macos(pid);
-
-        status
-    }
-
-    /// Returns whether the process is considered running.
-    pub fn pid_is_running(pid: i32) -> bool {
-        Self::read_pid_status(pid) == ProcStatus::Running
-    }
-
     /// Removes any PIDs that are no longer running from the watchlist.
     fn prune_terminations(&mut self) {
         let now = OffsetDateTime::now_utc();
@@ -188,9 +81,9 @@ impl PidWatcher {
             if let Some(expiration) = attached_pid.expiration {
                 // If the PID has an unreached expiration, retain it even if it
                 // isn't running
-                now < expiration || Self::pid_is_running(attached_pid.pid)
+                now < expiration || pid_is_running(attached_pid.pid)
             } else {
-                Self::pid_is_running(attached_pid.pid)
+                pid_is_running(attached_pid.pid)
             }
         })
     }
@@ -279,6 +172,7 @@ pub mod test {
     use flox_activations::cli::attach::{AttachArgs, AttachExclusiveArgs};
     use flox_activations::cli::{SetReadyArgs, StartOrAttachArgs};
     use flox_core::activations::activations_json_path;
+    use flox_core::proc_status::{read_pid_status, ProcStatus};
 
     use super::*;
 
@@ -313,7 +207,7 @@ pub mod test {
     /// Wait some attempts for the process to reach the desired state
     fn poll_until_state(state: ProcStatus, pid: i32) {
         for _ in 0..10 {
-            if PidWatcher::read_pid_status(pid) == state {
+            if read_pid_status(pid) == state {
                 return;
             }
             std::thread::sleep(Duration::from_millis(10));
@@ -323,16 +217,16 @@ pub mod test {
 
     #[test]
     fn reports_that_pid1_is_running() {
-        assert!(PidWatcher::pid_is_running(1));
+        assert!(pid_is_running(1));
     }
 
     #[test]
     fn detects_running_or_not_running_process() {
         let proc = start_process();
         let pid = proc.id() as i32;
-        assert!(PidWatcher::pid_is_running(pid));
+        assert!(pid_is_running(pid));
         stop_process(proc);
-        assert!(!PidWatcher::pid_is_running(pid));
+        assert!(!pid_is_running(pid));
     }
 
     #[test]
@@ -340,8 +234,8 @@ pub mod test {
         let mut proc = Command::new("true").spawn().unwrap();
         let pid = proc.id() as i32;
         poll_until_state(ProcStatus::Zombie, pid);
-        assert!(!PidWatcher::pid_is_running(pid));
-        assert_eq!(PidWatcher::read_pid_status(pid), ProcStatus::Zombie);
+        assert!(!pid_is_running(pid));
+        assert_eq!(read_pid_status(pid), ProcStatus::Zombie);
         proc.wait().unwrap();
     }
 
