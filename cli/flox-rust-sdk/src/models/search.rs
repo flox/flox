2ea06d1a4f91a6a4ce5a0351741c022028dd564a
Nick Lewis <nick@floxdev.com>
2025-01-16 15:02:00 -0800

    chore: Remove unused fields from SearchResult

--- cli/flox-rust-sdk/src/models/search.rs
+++ cli/flox-rust-sdk/src/models/search.rs
@@ -27,26 +27,12 @@ pub type ResultCount = Option<u64>;
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SearchResult {
-    /// Which input the package came from
-    ///
-    /// This is also abused to be the name of the catalog.
-    /// At some point we should rename this to catalog.
-    pub input: String,
     /// The system that the package can be built for
     pub system: String,
-    /// The part of the attribute path after `<subtree>.<system>`.
-    ///
-    /// For an arbitrary flake this will simply be the name of the package, but
-    /// for nixpkgs this can be something like `python310Packages.flask`
-    pub attr_path: Vec<String>,
     /// The package path including catalog name
     pub pkg_path: String,
-    /// The package name
-    pub pname: Option<String>,
     /// The package version
     pub version: Option<String>,
     /// The package description
     pub description: Option<String>,
-    /// Which license the package is licensed under
-    pub license: Option<String>,
 }
