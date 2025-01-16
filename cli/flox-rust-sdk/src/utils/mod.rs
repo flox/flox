b10f1f144497d3f4ad15913191fd3daf3c8a0292
Yannik Sander <yannik@floxdev.com>
2025-01-16 16:53:57 +0100

    chore: remove unused `find_and_replace` function

--- cli/flox-rust-sdk/src/utils/mod.rs
+++ cli/flox-rust-sdk/src/utils/mod.rs
@@ -15,8 +15,6 @@ pub use flox_core::traceable_path;
 #[cfg(any(test, feature = "tests"))]
 use proptest::prelude::*;
 use thiserror::Error;
-use tokio::fs::OpenOptions;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use walkdir;
 
 use self::errors::IoError;
@@ -31,52 +29,6 @@ pub enum FindAndReplaceError {
     ReadTemplateFile(std::io::Error),
     #[error("Error writing to template file")]
     WriteTemplateFile(std::io::Error),
-}
-
-/// Replace all occurrences of `find` with `replace` in a directory or file
-pub async fn find_and_replace(
-    path: &Path,
-    find: &str,
-    replace: &str,
-) -> Result<(), FindAndReplaceError> {
-    for entry in walkdir::WalkDir::new(path) {
-        let entry = entry.map_err(FindAndReplaceError::WalkDir)?;
-        if entry.file_type().is_file() {
-            let mut file = match tokio::fs::File::open(entry.path()).await {
-                Ok(f) => f,
-                Err(err) => return Err(FindAndReplaceError::OpenTemplateFile(err)),
-            };
-
-            let mut file_contents = String::new();
-            file.read_to_string(&mut file_contents)
-                .await
-                .map_err(FindAndReplaceError::ReadTemplateFile)?;
-
-            // TODO optimize with find or something?
-            if file_contents.contains(find) {
-                let new_contents = file_contents.replace(find, replace);
-                match OpenOptions::new()
-                    .write(true)
-                    .truncate(true)
-                    .open(entry.path())
-                    .await
-                {
-                    Ok(mut f) => f
-                        .write_all(new_contents.as_bytes())
-                        .await
-                        .map_err(FindAndReplaceError::WriteTemplateFile)?,
-                    Err(err) => return Err(FindAndReplaceError::OpenTemplateFile(err)),
-                };
-            }
-        } else {
-            debug!(
-                "Skipping entry that is not a regular file: {}",
-                entry.path().to_string_lossy()
-            );
-        }
-    }
-
-    Ok(())
 }
 
 /// Using fs::copy copies permissions from the Nix store, which we don't want, so open (or
