d57606e776100d7da2222d69adb2f3d4ff2c3850
Bryan Honof <bryan@flox.dev>
2025-01-22 03:47:12 +0100

    feat(containerize): add single-user install capability
    flox containerize expected to always run as root inside the container, since the
    Nix store was owned by root, and the following files were basically missing.
    
        /etc/passwd
        /etc/groups
        /etc/shadow
    
    This commit changes that by extending the container with a "fakeNss" to provide
    those missing files, and also abuse the fact that we can insert custom lines
    during build-time. We derive most information from containerize.config.user
    inside Flox's toml.
    
    All that being said, it allows us to do the following.
    
        $ flox containerize --file='-' | docker load
        ✨ 'tmp.GtqsVDTJut:latest' written to stdout
        Loaded image: tmp.gtqsvdtjut:latest
        $ # Works perfeclty fine when we just run it with the default user root
        $ docker run --rm -it tmp.gtqsvdtjut:latest 'cowsay hello'
         _______
        < hello >
         -------
                \   ^__^
                 \  (oo)\_______
                    (__)\       )\/\
                        ||----w |
                        ||     ||
        $ # There is a "bryan" user on this system, let's try to use that...
        $ docker run --user='bryan' --rm -it tmp.gtqsvdtjut:latest 'cowsay hello'
        Error: failed to acquire lockfile
    
        Caused by:
            Permission denied (os error 13)
        $ # This is expected since the Docker image wasn't build with extra users in
        $ # mind. Let's try changing that with the changes in this commit...
        $ echo '[containerize.config]' >> .flox/env/manifest.toml
        $ echo "user = 'bryan:bryan'" >> .flox/env/manifest.toml
        $ flox containerize --file='-' | docker load
        ✨ 'tmp.Fc9KpHvftX:latest' written to stdout
        Loaded image: tmp.fc9kphvftx:latest
        $ # root still works...
        $ docker run --rm -it tmp.fc9kphvftx:latest 'cowsay hello'
         _______
        < hello >
         -------
                \   ^__^
                 \  (oo)\_______
                    (__)\       )\/\
                        ||----w |
                        ||     ||
        $ # But this now also works...
        $ docker run \
        >  --user='bryan:bryan' \
        >  --rm -it \
        >  tmp.fc9kphvftx:latest 'cowsay hello'
         _______
        < hello >
         -------
                \   ^__^
                 \  (oo)\_______
                    (__)\       )\/\
                        ||----w |
                        ||     ||
    
    Cool, now we can use non-root user inside of the container. Note that the UID
    and GID are currently hard-coded to 501, since I'm not sure the OCI spec can
    support setting both at the same time. It looks to me as if its and either or
    thing.
    
    Nixpkgs' streamLayeredImage now also uses the correct architecture when building
    containers. We're putting binaries coming from the containerPkgs attribute into
    the final  image, so the container's architecture config should be set to
    containerPkgs' architecture as well. Using containerPkgs.go.GOARCH is just a
    shorthand to give us the correct format that the OCI spec is expecting.
    
    I also took a little time to refactor the mkContainer.nix file, the biggest
    change being that I moved some of the attributes that were first inside of a let
    binding, to the function's argument attribute set. This just made it a bit
    easier for me to itterate on this issue, without having to constantly rebuild
    Flox.
    
    Ref: flox/product#822

--- mkContainer/mkContainer.nix
+++ mkContainer/mkContainer.nix
@@ -8,72 +8,152 @@
   # the system to build for
   system,
   containerSystem,
+  environment ? builtins.storePath environmentOutPath,
+  nixpkgsFlake ? builtins.getFlake nixpkgsFlakeRef,
+  pkgs ? nixpkgsFlake.legacyPackages.${system},
+  containerPkgs ? nixpkgsFlake.legacyPackages.${containerSystem},
   containerName ? "flox-env-container",
   containerTag ? null,
   containerCreated ? "now",
-  containerConfig ? null,
+  containerConfigJSON ? "{}",
 }:
 let
-  environment = builtins.storePath environmentOutPath;
-  nixpkgsFlake = builtins.getFlake nixpkgsFlakeRef;
-  pkgs = nixpkgsFlake.legacyPackages.${system};
-  containerPkgs = nixpkgsFlake.legacyPackages.${containerSystem};
-  lib = pkgs.lib;
-  lowPriority =
-    pkg:
-    pkg.overrideAttrs (
-      old:
-      old
-      // {
-        meta = (old.meta or { }) // {
-          priority = 10000;
-        };
-      }
-    );
+  inherit (builtins)
+    fromJSON
+    toString
+    elemAt
+    match
+    ;
+  inherit (pkgs.lib)
+    mapAttrsToList
+    optionalString
+    optionalAttrs
+    optionals
+    toIntBase10
+    assertMsg
+    isValidPosixName
+    isInt
+    ;
+  inherit (pkgs.lib.meta)
+    lowPrio
+    ;
 
-  buildLayeredImageArgs = {
-    name = containerName;
-    tag = containerTag;
-    created = containerCreated;
-    # symlinkJoin fails when drv contains a symlinked bin directory, so wrap in an additional buildEnv
-    contents = pkgs.buildEnv {
-      name = "contents";
-      paths = [
-        environment
-        (lowPriority containerPkgs.bashInteractive) # for a usable shell
-        (lowPriority containerPkgs.coreutils) # for just the basic utils
-      ];
+  containerConfig = fromJSON containerConfigJSON;
+
+  nixStoreOwner = (containerConfig.User or "0:0");
+
+  isNixStoreUserOwned = (null == (match "^(root|0):\?(root|0)\?$" nixStoreOwner));
+
+  nixStoreUserGroup =
+    let
+      userGroupValues =
+        let
+          values = match "^(([_]*[[:alpha:]]+):?|([[:digit:]]+):?)(([_]*[[:alpha:]]+)|([[:digit:]]+))?$" nixStoreOwner;
+        in
+        assert assertMsg (
+          null != values
+        ) "Failed to parse nixStoreOwner, ${nixStoreOwner} did not match the expected pattern";
+        values;
+      uname = elemAt userGroupValues 1;
+      gname = if (null != (elemAt userGroupValues 4)) then (elemAt userGroupValues 4) else "flox";
+      uid =
+        if (null != (elemAt userGroupValues 2)) then toIntBase10 (elemAt userGroupValues 2) else 10000;
+      gid =
+        if (null != (elemAt userGroupValues 5)) then toIntBase10 (elemAt userGroupValues 5) else 10000;
+    in
+    assert assertMsg (null != uname && null != uid) "Either uname or uid must always be set";
+    assert assertMsg (
+      null != gname
+    ) "The group part should not be null, if left empty it must get swallowed";
+    assert assertMsg (isValidPosixName uname) "uname must be a valid POSIX name";
+    assert assertMsg (isValidPosixName gname) "gname must be a valid POSIX name";
+    assert assertMsg (isInt uid) "uid must be an integer";
+    assert assertMsg (isInt gid) "gid must be an integer";
+    {
+      inherit
+        uname
+        gname
+        uid
+        gid
+        ;
     };
-    # Activate script requires writable /tmp.
-    extraCommands = ''
-      mkdir -m 1777 tmp
-      mkdir -p -m 1777 tmp/flox/run
-    '';
-    config = (if containerConfig == null then { } else (builtins.fromJSON containerConfig)) // {
-      # Use activate script as the [one] entrypoint capable of
-      # detecting interactive vs. command activation modes.
-      # Usage:
-      #   podman run -it
-      #     -> launches interactive shell with controlling terminal
-      #   podman run -i <cmd>
-      #     -> invokes interactive command
-      #   podman run -i [SIC]
-      #     -> launches crippled interactive shell with no controlling
-      #        terminal .. kinda useless
-      Entrypoint = [ "${environment}/activate" ];
 
-      Env = lib.mapAttrsToList (name: value: "${name}=${value}") {
-        "FLOX_ENV" = environment;
-        "FLOX_PROMPT_ENVIRONMENTS" = "floxenv";
-        "FLOX_PROMPT_COLOR_1" = "99";
-        "FLOX_PROMPT_COLOR_2" = "141";
-        "_FLOX_ACTIVE_ENVIRONMENTS" = "[]";
-        "FLOX_SOURCED_FROM_SHELL_RC" = "1"; # don't source from shell rc (again)
-        "_FLOX_FORCE_INTERACTIVE" = "1"; # Required when running podman without "-t"
-        "FLOX_SHELL" = "${containerPkgs.bashInteractive}/bin/bash";
-        "FLOX_RUNTIME_DIR" = "/tmp/flox/run";
+  fakeNss = containerPkgs.dockerTools.fakeNss.override {
+    extraPasswdLines = optionals isNixStoreUserOwned [
+      "${nixStoreUserGroup.uname}:x:${toString nixStoreUserGroup.uid}:${toString nixStoreUserGroup.gid}:created by Flox:/var/empty:/bin/sh"
+    ];
+    extraGroupLines = optionals isNixStoreUserOwned [
+      "${nixStoreUserGroup.gname}:x:${toString nixStoreUserGroup.gid}:"
+    ];
+  };
+
+  buildLayeredImageArgs =
+    optionalAttrs (isNixStoreUserOwned) {
+      inherit (nixStoreUserGroup)
+        uname
+        gname
+        uid
+        gid
+        ;
+
+      # chown the /run directory to the nixStoreOwner, so that Nix can run as a
+      # single user installation inside the container
+      fakeRootCommands = ''
+        chown -R ${toString nixStoreUserGroup.uid}:${toString nixStoreUserGroup.gid} /run
+      '';
+      enableFakechroot = true;
+    }
+    // {
+      name = containerName;
+      tag = containerTag;
+      created = containerCreated;
+
+      # Ensures the container configuration contains the correct architecture of
+      # the binaries contained within it. Runtimes can use this to short-circuit
+      # errors when users try to run a container on an incompatible architecture.
+      architecture = containerPkgs.go.GOARCH;
+
+      # Activate script requires writable directory, /run feels like a logical place.
+      extraCommands = ''
+        mkdir -m 1770 run
+        mkdir -p -m 1770 run/flox
+      '';
+
+      # symlinkJoin fails when drv contains a symlinked bin directory, so wrap in an additional buildEnv.
+      contents = pkgs.buildEnv {
+        name = "contents";
+        paths = [
+          fakeNss
+          environment
+          (lowPrio containerPkgs.bashInteractive) # for a usable shell
+          (lowPrio containerPkgs.coreutils) # for just the basic utils
+        ];
+      };
+      config = containerConfig // {
+        # Use activate script as the [one] entrypoint capable of
+        # detecting interactive vs. command activation modes.
+        # Usage:
+        #   podman run -it
+        #     -> launches interactive shell with controlling terminal
+        #   podman run -i <cmd>
+        #     -> invokes interactive command
+        #   podman run -i [SIC]
+        #     -> launches crippled interactive shell with no controlling
+        #        terminal .. kinda useless
+        Entrypoint = [ "${environment}/activate" ];
+
+        Env = mapAttrsToList (name: value: "${name}=${value}") {
+          "FLOX_ENV" = environment;
+          "FLOX_PROMPT_ENVIRONMENTS" = "floxenv";
+          "FLOX_PROMPT_COLOR_1" = "99";
+          "FLOX_PROMPT_COLOR_2" = "141";
+          "_FLOX_ACTIVE_ENVIRONMENTS" = "[]";
+          "FLOX_SOURCED_FROM_SHELL_RC" = "1"; # don't source from shell rc (again)
+          "_FLOX_FORCE_INTERACTIVE" = "1"; # Required when running podman without "-t"
+          "FLOX_SHELL" = "${containerPkgs.bashInteractive}/bin/bash";
+          "FLOX_RUNTIME_DIR" = "/run/flox";
+        };
       };
     };
-  };
 in
 pkgs.dockerTools.streamLayeredImage buildLayeredImageArgs
