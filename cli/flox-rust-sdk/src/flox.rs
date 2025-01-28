cccc725659baabbf2eee833d9a11636bcc0818ac
Yannik Sander <yannik@floxdev.com>
2025-01-28 15:21:02 +0100

    refactor: amend flox version detection and document
    The version is determined by the following rules:
    1. `github:flox/flox#flox`, provides a wrapper that sets `FLOX_VERSION`.
       This is the main production artifact and canonical version.
    2. Our `just` targets will set `FLOX_VERSION` using the current git tag,
       so `just` builds will have the correct updated version _with_ git metadata.
    3. `cargo build` when run outside of `just` will fallback to `0.0.0-dirty`.
       This is the version that also local IDEs / rust-analyzer will use.
       However, binaries built this way may fail to run in some cases,
       e.g. `containerize` on macos which relies on the flox version.
    
    In short, if your work requires the current version of flox,
    use a nix or `just` build artifact.
    If you use flox _inside_ of another flox activation, call it with `just flox`
    or use the wrapped nix build.
    
    build: drop '-dirty' suffix from `just` set FLOX_VERSION
    
    Set the FLOX_VERSION variable so that it can be used in the build/runtime
    It's important to add the git revision to the version string,
    so to that `containerize` can build the correct version of flox in CI.
    While technically we'd want to add `-dirty` to the version string if the
    working directory is dirty, we omit this here because in practice
    it causes tests to fail that expect a FLAKE_VERSION to be "clean",
    and doesn't add practical information.

--- cli/flox-rust-sdk/src/flox.rs
+++ cli/flox-rust-sdk/src/flox.rs
@@ -12,8 +12,33 @@ use crate::data::FloxVersion;
 pub use crate::models::environment_ref::{self, *};
 use crate::providers::{catalog, flake_installable_locker};
 
-pub static FLOX_VERSION_STRING: LazyLock<String> =
-    LazyLock::new(|| std::env::var("FLOX_VERSION").unwrap_or(env!("FLOX_VERSION").to_string()));
+/// The Flox version, this is crate is part of.
+/// This is _also_ used to determine the version of the CLI.
+/// The version is determined by the following rules:
+/// 1. `github:flox/flox#flox`, provides a wrapper that sets `FLOX_VERSION`.
+///    This is the main production artifact and canonical version.
+/// 2. Our `just` targets will set `FLOX_VERSION` using the current git tag,
+///    so `just` builds will have the correct updated version _with_ git metadata.
+/// 3. `cargo build` when run outside of `just` will fallback to `0.0.0-dirty`.
+///    This is the version that also local IDEs / rust-analyzer will use.
+///    However, binaries built this way may fail to run in some cases,
+///    e.g. `containerize` on macos which relies on the flox version.
+pub static FLOX_VERSION_STRING: LazyLock<String> = LazyLock::new(|| {
+    // Runtime provided version,
+    // i.e. the flox cli wrapper of the nix built production flox package.
+    if let Ok(version) = std::env::var("FLOX_VERSION") {
+        return version;
+    };
+
+    // Buildtime provided version, i.e. `just build-flox`.
+    if let Some(version) = option_env!("FLOX_VERSION") {
+        return version.to_string();
+    }
+
+    // Fallback to dev version, to allow building without just,
+    // and default configurations in IDEs.
+    "0.0.0-dirty".to_string()
+});
 pub static FLOX_VERSION: LazyLock<FloxVersion> = LazyLock::new(|| {
     let Ok(version) = (*FLOX_VERSION_STRING).parse() else {
         // Production builds won't panic since we run `flox --version` in pkgs/flox/default.nix.
