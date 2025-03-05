06efbb7dc91e3e822d6e8f4dfa2033c5208243a5
Matthew Kenigsberg <matthew@floxdev.com>
2025-03-04 17:19:40 -0700

    feat: IncludeFetcher
    Add the IncludeFetcher struct, and thread it throughout the rest of the
    codebase.
    
    Path/Managed/Remote environments need to configure our merge code to
    specify whether or not local includes should be allowed, and they need
    to configure the directory relative to which includes should be fetched.
    
    Use IncludeFetcher to accomplish this.
    
    This required returning EnvironmentError in a few places in
    managed_environment in order to return EnvironmentError::InvalidPath.
    Since that error is already returned by ManagedEnvironment::parent_path,
    it doesn't make sense to return a different error for what is
    essentially the same thing.

--- cli/flox-rust-sdk/src/models/environment/path_environment.rs
+++ cli/flox-rust-sdk/src/models/environment/path_environment.rs
@@ -22,6 +22,7 @@ use indoc::formatdoc;
 use tracing::debug;
 
 use super::core_environment::{CoreEnvironment, UpgradeResult};
+use super::fetcher::IncludeFetcher;
 use super::{
     path_hash,
     services_socket_path,
@@ -137,13 +138,30 @@ impl PathEnvironment {
         })
     }
 
+    fn include_fetcher(&self) -> Result<IncludeFetcher, EnvironmentError> {
+        Ok(IncludeFetcher {
+            base_directory: Some(self.parent_path()?),
+        })
+    }
+
     /// Get a view of the environment that can be used to perform operations
     /// on the environment without side effects.
     ///
     /// This method should only be used to create [CoreEnvironment]s for a [PathEnvironment].
     /// To modify the environment, use the [PathEnvironment] methods instead.
-    pub(super) fn into_core_environment(self) -> CoreEnvironment {
-        CoreEnvironment::new(self.path.join(ENV_DIR_NAME))
+    pub(super) fn into_core_environment(self) -> Result<CoreEnvironment, EnvironmentError> {
+        self.as_core_environment()
+    }
+
+    fn as_core_environment(&self) -> Result<CoreEnvironment, EnvironmentError> {
+        Ok(CoreEnvironment::new(
+            self.path.join(ENV_DIR_NAME),
+            self.include_fetcher()?,
+        ))
+    }
+
+    fn as_core_environment_mut(&mut self) -> Result<CoreEnvironment, EnvironmentError> {
+        self.as_core_environment()
     }
 
     pub fn rename(&mut self, new_name: EnvironmentName) -> Result<(), EnvironmentError> {
@@ -175,7 +193,7 @@ impl PathEnvironment {
 impl Environment for PathEnvironment {
     /// This will lock the environment if it is not already locked.
     fn lockfile(&mut self, flox: &Flox) -> Result<Lockfile, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         env_view.ensure_locked(flox)
     }
 
@@ -192,7 +210,7 @@ impl Environment for PathEnvironment {
         packages: &[PackageToInstall],
         flox: &Flox,
     ) -> Result<InstallationAttempt, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         let result = env_view.install(packages, flox)?;
         if let Some(ref store_paths) = result.built_environments {
             self.link(flox, store_paths)?;
@@ -211,7 +229,7 @@ impl Environment for PathEnvironment {
         packages: Vec<String>,
         flox: &Flox,
     ) -> Result<UninstallationAttempt, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         let result = env_view.uninstall(packages, flox)?;
         if let Some(ref store_paths) = result.built_environment_store_paths {
             self.link(flox, store_paths)?;
@@ -222,7 +240,7 @@ impl Environment for PathEnvironment {
 
     /// Atomically edit this environment, ensuring that it still builds
     fn edit(&mut self, flox: &Flox, contents: String) -> Result<EditResult, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         let result = env_view.edit(flox, contents)?;
         if result != EditResult::Unchanged {
             if let Some(ref store_paths) = result.built_environment_store_paths() {
@@ -238,7 +256,7 @@ impl Environment for PathEnvironment {
         flox: &Flox,
         groups_or_iids: &[&str],
     ) -> Result<UpgradeResult, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         let result = env_view.upgrade(flox, groups_or_iids, false)?;
         Ok(result)
     }
@@ -250,7 +268,7 @@ impl Environment for PathEnvironment {
         groups_or_iids: &[&str],
     ) -> Result<UpgradeResult, EnvironmentError> {
         tracing::debug!(to_upgrade = groups_or_iids.join(","), "upgrading");
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         let result = env_view.upgrade(flox, groups_or_iids, true)?;
         if let Some(ref store_paths) = result.store_path {
             self.link(flox, store_paths)?;
@@ -266,7 +284,7 @@ impl Environment for PathEnvironment {
 
     /// Return the deserialized manifest
     fn manifest(&self, _flox: &Flox) -> Result<Manifest, EnvironmentError> {
-        let env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let env_view = self.as_core_environment()?;
         env_view.manifest().map_err(EnvironmentError::Core)
     }
 
@@ -305,7 +323,7 @@ impl Environment for PathEnvironment {
     /// Build the environment
     /// This will lock the environment if it is not already locked.
     fn build(&mut self, flox: &Flox) -> Result<BuildEnvOutputs, EnvironmentError> {
-        let mut env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let mut env_view = self.as_core_environment_mut()?;
         env_view.lock(flox)?;
         let store_paths = env_view.build(flox)?;
         Ok(store_paths)
@@ -421,7 +439,10 @@ impl PathEnvironment {
 
         // Build environment if customization installs at least one package
         if matches!(customization.packages.as_deref(), Some([_, ..])) {
-            let mut env_view = CoreEnvironment::new(environment.path.join(ENV_DIR_NAME));
+            let mut env_view = CoreEnvironment::new(
+                environment.path.join(ENV_DIR_NAME),
+                environment.include_fetcher()?,
+            );
             env_view.lock(flox)?;
             let store_paths = env_view.build(flox)?;
             environment.link(flox, &store_paths)?;
@@ -498,7 +519,7 @@ impl PathEnvironment {
     /// or differs from the definition in the environment,
     /// the environment will be rebuilt.
     fn needs_rebuild(&self) -> Result<bool, EnvironmentError> {
-        let env_view = CoreEnvironment::new(self.path.join(ENV_DIR_NAME));
+        let env_view = self.as_core_environment()?;
         let Some(lockfile_contents) = env_view.existing_lockfile_contents()? else {
             return Ok(true);
         };
@@ -646,7 +667,8 @@ mod tests {
         assert!(env.needs_rebuild().unwrap());
 
         // build the environment -> out link is created -> no rebuild necessary
-        let mut env_view = CoreEnvironment::new(env.path.join(ENV_DIR_NAME));
+        let mut env_view =
+            CoreEnvironment::new(env.path.join(ENV_DIR_NAME), env.include_fetcher().unwrap());
         env_view.lock(&flox).unwrap();
         let store_paths = env_view.build(&flox).unwrap();
         env.link(&flox, &store_paths).unwrap();
