f11d802932a70ffc2f3c95b6dacbcb9582677b0a
Matthew Kenigsberg <matthew@floxdev.com>
2025-04-07 15:20:36 -0600

    fix: progenitor doesn't handle discriminator
    progenitor doesn't know how to handle oneOf that uses a discriminator.
    It currently just deserializes as the first enum variant that fits, and
    for us that means that `"store_type": "meta-only"` will get deserialized
    as CatalogStoreConfig::Null.
    
    Implement CatalogStoreConfig manually to workaround this.

--- cli/catalog-api-v1/src/lib.rs
+++ cli/catalog-api-v1/src/lib.rs
@@ -8,6 +8,81 @@ mod error;
 pub use client::*;
 
 pub mod types {
-    pub use crate::error::MessageType;
     pub use crate::client::types::*;
+    pub use crate::error::MessageType;
+
+    use serde::{Deserialize, Serialize};
+    /// Progenitor doesn't know how to use a discriminator as a tag, so add this
+    /// enum manually.
+    #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+    #[serde(tag = "store_type", rename_all = "kebab-case")]
+    pub enum CatalogStoreConfig {
+        /// The catalog store has not yet been configured
+        Null,
+        /// The user has configured the catalog for metadata only publishes
+        MetaOnly,
+        /// Store to copy to with `nix copy`
+        NixCopy(CatalogStoreConfigNixCopy),
+        /// Not yet supported
+        Publisher,
+    }
+
+    #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+    pub struct CatalogStoreConfigNixCopy {
+        pub egress_uri: String,
+        pub ingress_uri: String,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{CatalogStoreConfig, CatalogStoreConfigNixCopy};
+
+    #[test]
+    fn deserialize_catalog_store_config_null() {
+        let response_string = r#"{
+            "store_type": "null"
+        }"#;
+
+        let store_config = serde_json::from_str::<CatalogStoreConfig>(response_string).unwrap();
+        assert_eq!(store_config, CatalogStoreConfig::Null)
+    }
+
+    #[test]
+    fn deserialize_catalog_store_config_meta_only() {
+        let response_string = r#"{
+            "store_type": "meta-only"
+        }"#;
+
+        let store_config = serde_json::from_str::<CatalogStoreConfig>(response_string).unwrap();
+        assert_eq!(store_config, CatalogStoreConfig::MetaOnly)
+    }
+
+    #[test]
+    fn deserialize_catalog_store_config_nix_copy() {
+        let response_string = r#"{
+           "store_type": "nix-copy",
+           "ingress_uri": "s3://example",
+           "egress_uri": "s3://example"
+        }"#;
+
+        let store_config = serde_json::from_str::<CatalogStoreConfig>(response_string).unwrap();
+        assert_eq!(
+            store_config,
+            CatalogStoreConfig::NixCopy(CatalogStoreConfigNixCopy {
+                ingress_uri: "s3://example".into(),
+                egress_uri: "s3://example".into()
+            })
+        )
+    }
+
+    #[test]
+    fn deserialize_catalog_store_config_publisher() {
+        let response_string = r#"{
+           "store_type": "publisher"
+        }"#;
+
+        let store_config = serde_json::from_str::<CatalogStoreConfig>(response_string).unwrap();
+        assert_eq!(store_config, CatalogStoreConfig::Publisher)
+    }
 }
