a5348b7d37c34e803fa2c2f2d133d9ce50385991
Yannik Sander <yannik@floxdev.com>
2025-04-25 15:28:38 +0200

    feat: Add nix library to reflect on imported packages
    Adds `lib.nef.reflect.*`, functions that allow easy reports of what is being imported via `dirAToAttrs`

--- package-builder/nef/lib/default.nix
+++ package-builder/nef/lib/default.nix
@@ -1,11 +1,10 @@
 {
   overlay = final: prev: {
-    nef =
-      final.makeScope (scope: final.callPackageWith ({ lib = final; } // final // scope))
-        (self: {
-          dirToAttrs = (self.callPackage ./dirToAttrs.nix { }).dirToAttrs;
-          extendAttrSet = (self.callPackage ./extendAttrSet.nix { }).extendAttrSet;
-          mkOverlay = (self.callPackage ./mkOverlay.nix { }).mkOverlay;
-        });
+    nef = final.makeScope (scope: final.callPackageWith ({ lib = final; } // final // scope)) (self: {
+      dirToAttrs = (self.callPackage ./dirToAttrs.nix { }).dirToAttrs;
+      extendAttrSet = (self.callPackage ./extendAttrSet.nix { }).extendAttrSet;
+      mkOverlay = (self.callPackage ./mkOverlay.nix { }).mkOverlay;
+      reflect = self.callPackage ./reflect.nix { };
+    });
   };
 }
