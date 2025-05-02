3ff3dc11e4a9f5f9c58a49f57a369fec4356d5d1
Dan Carley <dan@flox.dev>
2025-05-02 15:07:12 +0100

    chore(catalog): httpmock for read/write fixtures
    Re-purpose the existing environment variables that are used by `mk-data`
    and `bats` to read and write catalog mocks to fixture files, replacing
    our custom mock generation with `httpmock` YAML files.
    
    This will allow us to consolidate on a single approach throughout all of
    our tests. `MockClient` will be removed or replaced later on.
    
    It's worth noting that the mock fixtures are now at the HTTP layer
    rather than the shim layer which ensures that we test more of the
    codepath and resolves the issues where we don't validate that the mock
    response of the shim matches the type of content that we expect, e.g.
    using the wrong resolve response or something that's not a resolve
    response at all.

--- cli/flox/src/utils/init/catalog_client.rs
+++ cli/flox/src/utils/init/catalog_client.rs
@@ -1,15 +1,14 @@
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 
-use anyhow::bail;
 use flox_rust_sdk::providers::catalog::{
     CatalogClient,
     CatalogClientConfig,
+    CatalogMockMode,
     Client,
+    FLOX_CATALOG_DUMP_DATA_VAR,
     FLOX_CATALOG_MOCK_DATA_VAR,
-    MockClient,
 };
-use flox_rust_sdk::utils::logging::traceable_path;
 use tracing::debug;
 
 use crate::config::Config;
@@ -19,58 +18,54 @@ pub const DEFAULT_CATALOG_URL: &str = "https://api.flox.dev";
 
 /// Initialize the Catalog API client
 ///
-/// - Return [None] if the Catalog API is disabled through the feature flag
 /// - Initialize a mock client if the `_FLOX_USE_CATALOG_MOCK` environment variable is set to `true`
 /// - Initialize a real client otherwise
 pub fn init_catalog_client(config: &Config) -> Result<Client, anyhow::Error> {
-    // if $_FLOX_USE_CATALOG_MOCK is set to a path to mock data, use the mock client
-    if let Ok(path_str) = std::env::var(FLOX_CATALOG_MOCK_DATA_VAR) {
-        let path = PathBuf::from(path_str);
-        if !path.exists() {
-            bail!("path to mock data file doesn't exist: {}", path.display());
-        }
+    let extra_headers = {
+        // If metrics are not disabled, pass along the metrics UUID so it can be
+        // sent in catalog request headers, as well as the Sentry span info
+        if !config.flox.disable_metrics {
+            let mut metrics_headers = BTreeMap::new();
+            metrics_headers.insert(
+                "flox-device-uuid".to_string(),
+                read_metrics_uuid(config).unwrap().to_string(),
+            );
 
-        debug!(
-            mock_data_path = traceable_path(&path),
-            "using mock catalog client"
-        );
-        Ok(MockClient::new(Some(path))?.into())
-    } else {
-        let extra_headers = {
-            // If metrics are not disabled, pass along the metrics UUID so it can be
-            // sent in catalog request headers, as well as the Sentry span info
-            if !config.flox.disable_metrics {
-                let mut metrics_headers = BTreeMap::new();
-                metrics_headers.insert(
-                    "flox-device-uuid".to_string(),
-                    read_metrics_uuid(config).unwrap().to_string(),
-                );
-
-                if let Some(span) = sentry::configure_scope(|scope| scope.get_span()) {
-                    for (k, v) in span.iter_headers() {
-                        metrics_headers.insert(k.to_string(), v);
-                    }
+            if let Some(span) = sentry::configure_scope(|scope| scope.get_span()) {
+                for (k, v) in span.iter_headers() {
+                    metrics_headers.insert(k.to_string(), v);
                 }
-                metrics_headers
-            } else {
-                Default::default()
             }
-        };
+            metrics_headers
+        } else {
+            Default::default()
+        }
+    };
 
-        let client_config = CatalogClientConfig {
-            catalog_url: config
-                .flox
-                .catalog_url
-                .clone()
-                .unwrap_or_else(|| DEFAULT_CATALOG_URL.to_string()),
-            floxhub_token: config.flox.floxhub_token.clone(),
-            extra_headers,
-        };
+    let mock_mode = if let Ok(path_str) = std::env::var(FLOX_CATALOG_MOCK_DATA_VAR) {
+        let path = PathBuf::from(path_str);
+        CatalogMockMode::Replay(path)
+    } else if let Ok(path_str) = std::env::var(FLOX_CATALOG_DUMP_DATA_VAR) {
+        let path = PathBuf::from(path_str);
+        CatalogMockMode::Record(path)
+    } else {
+        CatalogMockMode::None
+    };
 
-        debug!(
-            "using catalog client with url: {}",
-            client_config.catalog_url
-        );
-        Ok(CatalogClient::new(client_config).into())
-    }
+    let client_config = CatalogClientConfig {
+        catalog_url: config
+            .flox
+            .catalog_url
+            .clone()
+            .unwrap_or_else(|| DEFAULT_CATALOG_URL.to_string()),
+        floxhub_token: config.flox.floxhub_token.clone(),
+        extra_headers,
+        mock_mode,
+    };
+
+    debug!(
+        "using catalog client with url: {}",
+        client_config.catalog_url
+    );
+    Ok(CatalogClient::new(client_config).into())
 }
