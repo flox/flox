392cb0c0ba119f2f17852abacba953064153551a
Matthew Kenigsberg <matthew@floxdev.com>
2025-07-03 12:28:02 -0600

    fix: set PYTHONPATH in build wrapper
    We currently run profile scripts for the build wrapper, but it
    incidentally doesn't set FLOX_ENV_DIRS and therefore PYTHONPATH.
    
    We want to set PYTHONPATH, but we don't want to set it using layered
    activations; we only want to find modules from the build wrapper.
    
    For that reason, set it using FLOX_ENV rather than FLOX_ENV_DIRS.
    
    Factor out 0500_python.sh into a function that accepts arguments that
    determine wrapper vs normal activation behavior rather than implicitly
    depending on FLOX_ENV_DIRS.

--- assets/environment-interpreter/common/activate.d/helpers.bash
+++ assets/environment-interpreter/common/activate.d/helpers.bash
@@ -1,8 +1,12 @@
-# source_profile_d <profile.d directory>
+# source_profile_d <profile.d directory> <activation mode>
 #
 # source all scripts in <profile.d directory>
 function source_profile_d {
   local _profile_d="${1?}"
+  shift
+  local _flox_env_activation_mode="${1?}"
+  shift
+  local _flox_env_dirs="${1?}"
   shift
 
   # make sure the directory exists
@@ -21,6 +25,11 @@ function source_profile_d {
     # shellcheck disable=SC1090 # from rendered environment
     source "$_profile_d/$profile_script"
   done
+
+  # shellcheck disable=SC1091 # from rendered environment
+  source "$_profile_d/profile.d.functions"
+  setup_python "$_flox_env_activation_mode" "$_flox_env_dirs"
+  unset -f setup_python
 }
 
 # set_manifest_vars <flox_env>
