c82a2d861ca20c68c11513b316d2b2941c76f164
Yannik Sander <yannik@flox.dev>
2025-08-01 13:35:56 +0200

    feat: add switch_generation

--- cli/flox-rust-sdk/src/models/environment/generations.rs
+++ cli/flox-rust-sdk/src/models/environment/generations.rs
@@ -608,6 +608,15 @@ pub struct AddGenerationOptions {
     pub summary: String,
 }
 
+#[derive(Debug, Clone)]
+pub struct SwitchGenerationOptions {
+    pub author: String,
+    pub hostname: String,
+    pub timestamp: DateTime<Utc>,
+    pub summary: String,
+    pub next_generation: GenerationId,
+}
+
 impl AllGenerationsMetadata {
     /// Add metadata for a new generation, as well as consistent history.
     /// The return provides the [GenerationId] of the added generation metadata,
@@ -671,6 +680,47 @@ impl AllGenerationsMetadata {
             .expect("history event should have been inserted");
 
         (next_generation, generation_metadata_ref, history_ref)
+    }
+
+    pub fn switch_generation(
+        &mut self,
+        SwitchGenerationOptions {
+            author,
+            hostname,
+            timestamp,
+            summary,
+            next_generation,
+        }: SwitchGenerationOptions,
+    ) -> Result<(), GenerationsError> {
+        let previous_generation = self.current_gen;
+
+        if Some(next_generation) == previous_generation {
+            return Err(GenerationsError::RollbackToCurrentGeneration);
+        }
+
+        // get the metadata to the switched to generation
+        let Some(next_generation_metadata) = self.generations.get_mut(&next_generation) else {
+            return Err(GenerationsError::GenerationNotFound(*next_generation));
+        };
+
+        // update current active gen
+        self.current_gen = Some(next_generation);
+
+        // update the generation metadata
+        next_generation_metadata.last_active = Some(Utc::now());
+
+        // add action to history
+        self.history.0.push(HistorySpec {
+            author,
+            hostname,
+            timestamp,
+            previous_generation,
+            current_generation: next_generation,
+            kind: HistoryKind::SwitchGeneration,
+            summary,
+        });
+
+        Ok(())
     }
 
     /// Create a new object from its parts,
