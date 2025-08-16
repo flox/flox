flox/flox

commit 435149a1a52d43effa380274af40d91021505e42

fix: wording tweak: active -> live
Use "live" instead of "active" when describing the currently live generation.

Also print "Last Live" in `flox generations list`

Active is overloaded since we have `flox activate`, and "last active"
mean either "last time `flox activate` was run with this generation" or
"last time this generation was the current version of the environment"

cli/flox-rust-sdk/src/models/environment/generations.rs
@@ -653,10 +653,10 @@ impl AllGenerationsMetadata {
         (next_generation, history_ref)
     }
 
-    /// Switch the active marked generation to `next_generation`.
+    /// Switch the live generation to `next_generation`.
     /// `next_generation` must exist, and must be different from the current generation.
     /// To switch, this methods will (1) update [Self::current_gen] to `next_generation`,
-    /// (2) set the [SingleGenerationMetadata::last_active] timestamp of the `next_generation`,
+    /// (2) set the [SingleGenerationMetadata::last_live] timestamp of the `next_generation`,
     /// and record a history item of type [HistoryKind::SwitchGeneration].
     pub fn switch_generation(
         &mut self,
@@ -708,7 +708,7 @@ impl AllGenerationsMetadata {
         &self.history
     }
 
-    /// Get the current active generation id
+    /// Get the current live generation id
     pub fn current_gen(&self) -> Option<GenerationId> {
         self.history
             .iter()
@@ -721,7 +721,7 @@ impl AllGenerationsMetadata {
     /// This function requires a semantically consistent history, i.e.
     ///
     /// * exactly one generation creating event for every generation [1..]
-    /// * previous_generation correctly refers to the previously active generation
+    /// * previous_generation correctly refers to the previously live generation
     ///
     /// These invariants are maintained when using [Self::add_generation]
     /// and [Self::switch_generation].
@@ -734,27 +734,27 @@ impl AllGenerationsMetadata {
                         .previous_generation
                         .and_then(|generation| map.get_mut(&generation))
                         .expect("there must be a previous generation by construction");
-                    prev.last_active = Some(spec.timestamp);
+                    prev.last_live = Some(spec.timestamp);
 
                     let new = map
                         .get_mut(&spec.current_generation)
                         .expect("there must be a current generation by construction");
-                    new.last_active = None;
+                    new.last_live = None;
                 },
                 _ => {
                     // Adding a generation performs an implicit switch.
-                    // Hence, record that the previous generation was only active
+                    // Hence, record that the previous generation was only live
                     // until the creation of the new generation.
                     if let Some(prev) = spec
                         .previous_generation
                         .and_then(|generation| map.get_mut(&generation))
                     {
-                        prev.last_active = Some(spec.timestamp);
+                        prev.last_live = Some(spec.timestamp);
                     }
 
                     map.insert(spec.current_generation, SingleGenerationMetadata {
                         created: spec.timestamp,
-                        last_active: None,
+                        last_live: None,
                         description: spec.summary(),
                     });
                 },
@@ -771,9 +771,9 @@ pub struct SingleGenerationMetadata {
     /// unix timestamp of the creation time of this generation
     pub created: DateTime<Utc>,
 
-    /// unix timestamp of the time when this generation was last set as active
-    /// `None` if this generation has never been set as active
-    pub last_active: Option<DateTime<Utc>>,
+    /// unix timestamp of the time when this generation was last set as live
+    /// `None` if this generation has never been set as live
+    pub last_live: Option<DateTime<Utc>>,
 
     /// log message(s) describing the change from the previous generation
     pub description: String,
@@ -784,7 +784,7 @@ impl SingleGenerationMetadata {
     pub fn new(description: String) -> Self {
         Self {
             created: Utc::now(),
-            last_active: None,
+            last_live: None,
             description,
         }
     }
@@ -862,11 +862,11 @@ pub struct HistorySpec {
     pub timestamp: DateTime<Utc>,
 
     // associated generation(s)
-    /// Currently active generation, e.g. created by the change
+    /// Currently live generation, e.g. created by the change
     /// or switched to.
     pub current_generation: GenerationId,
     /// Previous generation before a new generation was created,
-    /// or the generation active before a generation switch.
+    /// or the generation live before a generation switch.
     pub previous_generation: Option<GenerationId>,
 }
 
@@ -916,7 +916,7 @@ impl HistorySpec {
                     self.current_generation
                 ),
                 None => unreachable!(
-                    "switch implementation prevents switches without a current active generation"
+                    "switch implementation prevents switches without a current live generation"
                 ),
             },
             HistoryKind::Other { summary } => summary.to_string(),
@@ -985,10 +985,10 @@ mod compat {
         #[serde(with = "chrono::serde::ts_seconds")]
         pub created: DateTime<Utc>,
 
-        /// unix timestamp of the time when this generation was last set as active
-        /// `None` if this generation has never been set as active
+        /// unix timestamp of the time when this generation was last set as live
+        /// `None` if this generation has never been set as live
         #[serde(with = "chrono::serde::ts_seconds_option")]
-        pub last_active: Option<DateTime<Utc>>,
+        pub last_live: Option<DateTime<Utc>>,
 
         /// log message(s) describing the change from the previous generation
         pub description: String,
@@ -1075,7 +1075,7 @@ mod compat {
                     .ok_or(GenerationsError::MigrateV1ToV2(
                         "current generation missing".into(),
                     ))?
-                    .last_active
+                    .last_live
                     .ok_or(GenerationsError::MigrateV1ToV2(
                         "current generation missing timestamp".into(),
                     ))?;
@@ -1180,7 +1180,7 @@ mod tests {
 
             assert_eq!(metadata.current_gen(), Some(generation));
             assert_eq!(generation_metadata.created, options.timestamp);
-            assert_eq!(generation_metadata.last_active, None);
+            assert_eq!(generation_metadata.last_live, None);
 
             assert_eq!(history.author, options.author);
             assert_eq!(history.hostname, options.hostname);
@@ -1214,7 +1214,7 @@ mod tests {
         /// Switching generations
         ///
         /// * updates the current generation
-        /// * updares the "last_active" timestamp of the switched to generation
+        /// * updares the "last_live" timestamp of the switched to generation
         /// * adds a history entry for the switch
         #[test]
         fn switch_generation_updates_metadata() {
@@ -1230,12 +1230,12 @@ mod tests {
                     "current gen was not updated"
                 );
                 assert_eq!(
-                    metadata.generations()[&generation_switched_to].last_active,
+                    metadata.generations()[&generation_switched_to].last_live,
                     None,
                     "timestamp was not updated"
                 );
                 assert_eq!(
-                    metadata.generations()[&generation_switched_from].last_active,
+                    metadata.generations()[&generation_switched_from].last_live,
                     Some(switch_generation_options.timestamp),
                     "timestamp was not updated"
                 );
@@ -1541,7 +1541,7 @@ mod tests {
                 1.into(),
                 BTreeMap::from_iter([(1.into(), SingleGenerationMetadata {
                     created: date,
-                    last_active: Some(date),
+                    last_live: Some(date),
                     description: "description".to_string(),
                 })]),
             );
@@ -1565,7 +1565,7 @@ mod tests {
                         1.into(),
                         BTreeMap::from_iter([(1.into(), SingleGenerationMetadata {
                             created: date,
-                            last_active: Some(date),
+                            last_live: Some(date),
                             description: "description".to_string(),
                         })]),
                     ),
@@ -1582,17 +1582,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_active: Some(date),
+                                last_live: Some(date),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_active: Some(date + Duration::hours(1)), // [sic]
+                                last_live: Some(date + Duration::hours(1)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(2),
-                                last_active: Some(date + Duration::hours(2)), // [sic]
+                                last_live: Some(date + Duration::hours(2)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),
@@ -1616,17 +1616,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_active: Some(date + Duration::hours(2)),
+                                last_live: Some(date + Duration::hours(2)),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_active: Some(date + Duration::hours(1)), // [sic]
+                                last_live: Some(date + Duration::hours(1)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(3),
-                                last_active: Some(date + Duration::hours(3)), // [sic]
+                                last_live: Some(date + Duration::hours(3)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),
@@ -1652,17 +1652,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_active: Some(date + Duration::hours(2)),
+                                last_live: Some(date + Duration::hours(2)),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_active: Some(date + Duration::hours(3)), // [sic]
+                                last_live: Some(date + Duration::hours(3)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(2),
-                                last_active: Some(date + Duration::hours(2)), // [sic]
+                                last_live: Some(date + Duration::hours(2)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),

cli/flox/src/commands/generations/list.rs
@@ -49,8 +49,8 @@ impl Display for DisplayMetadata<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let description = &self.metadata.description;
         let created = self.metadata.created;
-        let last_active = if let Some(last_active) = self.metadata.last_active {
-            last_active.to_string()
+        let last_live = if let Some(last_live) = self.metadata.last_live {
+            last_live.to_string()
         } else {
             "Now".to_string()
         };
@@ -58,7 +58,7 @@ impl Display for DisplayMetadata<'_> {
         write!(f, "{}", formatdoc! {"
             Description: {description}
             Created:     {created}
-            Last Active: {last_active}"})
+            Last Live:   {last_live}"})
     }
 }
 
@@ -130,7 +130,7 @@ mod tests {
         let actual = DisplayMetadata {
             metadata: &SingleGenerationMetadata {
                 created: DateTime::default(),
-                last_active: Some(DateTime::default()),
+                last_live: Some(DateTime::default()),
                 description: "Generation description".to_string(),
             },
         }
@@ -139,7 +139,7 @@ mod tests {
         let expected = indoc! {"
             Description: Generation description
             Created:     1970-01-01 00:00:00 UTC
-            Last Active: 1970-01-01 00:00:00 UTC"
+            Last Live:   1970-01-01 00:00:00 UTC"
         };
 
         assert_eq!(actual, expected);
@@ -151,7 +151,7 @@ mod tests {
         let actual = DisplayMetadata {
             metadata: &SingleGenerationMetadata {
                 created: DateTime::default(),
-                last_active: None,
+                last_live: None,
                 description: "Generation description".to_string(),
             },
         }
@@ -160,7 +160,7 @@ mod tests {
         let expected = indoc! {"
             Description: Generation description
             Created:     1970-01-01 00:00:00 UTC
-            Last Active: Now"
+            Last Live:   Now"
         };
 
         assert_eq!(actual, expected);
@@ -194,17 +194,17 @@ mod tests {
             Generation:  1
             Description: mock
             Created:     1970-01-01 01:00:00 UTC
-            Last Active: 1970-01-01 02:00:00 UTC
+            Last Live:   1970-01-01 02:00:00 UTC
 
             Generation:  2 (current)
             Description: mock
             Created:     1970-01-01 02:00:00 UTC
-            Last Active: Now
+            Last Live:   Now
 
             Generation:  3
             Description: mock
             Created:     1970-01-01 03:00:00 UTC
-            Last Active: 1970-01-01 04:00:00 UTC"
+            Last Live:   1970-01-01 04:00:00 UTC"
         };
 
         assert_eq!(actual, expected);

cli/flox/src/commands/generations/mod.rs
@@ -26,7 +26,7 @@ pub enum GenerationsCommands {
     #[bpaf(command)]
     History(#[bpaf(external(history::history))] history::History),
 
-    /// Switch to the previously active generation
+    /// Switch to the previous live generation
     #[bpaf(command)]
     Rollback(#[bpaf(external(rollback::rollback))] rollback::Rollback),
 

cli/flox/src/commands/generations/rollback.rs
@@ -57,7 +57,7 @@ impl Rollback {
     }
 }
 
-/// "previous generation" currently means "previously active"
+/// "previous generation" currently means "previously live"
 /// (as opposed to e.g. originating generation or generation N-1).
 /// That implies that switching to the "previous generation",
 /// i.e. rollback, returns at the original generation.
@@ -69,7 +69,7 @@ fn determine_previous_generation(
     metadata
         .generations()
         .into_iter()
-        .sorted_by_key(|(_id, meta)| meta.last_active)
+        .sorted_by_key(|(_id, meta)| meta.last_live)
         .next_back()
 }
 
@@ -101,11 +101,11 @@ mod tests {
 
     /// Use mock generations that were rolled back once from generation 3 -> genration 2.
     /// By our current definition of "previous generation" we expect another rollback
-    /// to "roll forward" to generation 3, as thats the one previously active.
+    /// to "roll forward" to generation 3, as thats the one previously live.
     #[test]
-    fn rollback_rolls_back_to_newer_generation_if_previously_active() {
+    fn rollback_rolls_back_to_newer_generation_if_previously_live() {
         // e.g. rolled back once 3->2
-        // last created active
+        // last created live
         let mut metadata = AllGenerationsMetadata::default();
         metadata.add_generation(default_add_generation_options());
         metadata.add_generation(default_add_generation_options());

commit 040e26c5416bad50a93e11581f141388438af4eb

fix: v1 generations migrations
435149a1a regressed v1 generations migrations because the old metadata
format uses last_active, not last_live.

Restore last_active in the schema for the old metadata format.

cli/flox-rust-sdk/src/models/environment/generations.rs
@@ -1104,8 +1104,9 @@ mod compat {
 
         /// unix timestamp of the time when this generation was last set as live
         /// `None` if this generation has never been set as live
+        /// last_live is used in the new metadata format
         #[serde(with = "chrono::serde::ts_seconds_option")]
-        pub last_live: Option<DateTime<Utc>>,
+        pub last_active: Option<DateTime<Utc>>,
 
         /// log message(s) describing the change from the previous generation
         pub description: String,
@@ -1193,7 +1194,7 @@ mod compat {
                     .ok_or(GenerationsError::MigrateV1ToV2(
                         "current generation missing".into(),
                     ))?
-                    .last_live
+                    .last_active
                     .ok_or(GenerationsError::MigrateV1ToV2(
                         "current generation missing timestamp".into(),
                     ))?;
@@ -1725,7 +1726,7 @@ mod tests {
                 1.into(),
                 BTreeMap::from_iter([(1.into(), SingleGenerationMetadata {
                     created: date,
-                    last_live: Some(date),
+                    last_active: Some(date),
                     description: "description".to_string(),
                 })]),
             );
@@ -1749,7 +1750,7 @@ mod tests {
                         1.into(),
                         BTreeMap::from_iter([(1.into(), SingleGenerationMetadata {
                             created: date,
-                            last_live: Some(date),
+                            last_active: Some(date),
                             description: "description".to_string(),
                         })]),
                     ),
@@ -1766,17 +1767,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_live: Some(date),
+                                last_active: Some(date),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_live: Some(date + Duration::hours(1)), // [sic]
+                                last_active: Some(date + Duration::hours(1)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(2),
-                                last_live: Some(date + Duration::hours(2)), // [sic]
+                                last_active: Some(date + Duration::hours(2)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),
@@ -1800,17 +1801,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_live: Some(date + Duration::hours(2)),
+                                last_active: Some(date + Duration::hours(2)),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_live: Some(date + Duration::hours(1)), // [sic]
+                                last_active: Some(date + Duration::hours(1)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(3),
-                                last_live: Some(date + Duration::hours(3)), // [sic]
+                                last_active: Some(date + Duration::hours(3)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),
@@ -1836,17 +1837,17 @@ mod tests {
                         BTreeMap::from_iter([
                             (1.into(), SingleGenerationMetadata {
                                 created: date,
-                                last_live: Some(date + Duration::hours(2)),
+                                last_active: Some(date + Duration::hours(2)),
                                 description: "description".to_string(),
                             }),
                             (2.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(1),
-                                last_live: Some(date + Duration::hours(3)), // [sic]
+                                last_active: Some(date + Duration::hours(3)), // [sic]
                                 description: "description".to_string(),
                             }),
                             (3.into(), SingleGenerationMetadata {
                                 created: date + Duration::hours(2),
-                                last_live: Some(date + Duration::hours(2)), // [sic]
+                                last_active: Some(date + Duration::hours(2)), // [sic]
                                 description: "description".to_string(),
                             }),
                         ]),