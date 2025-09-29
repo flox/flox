c86af2e61a5589bdc494f6b3bbd6e6c925e94b9d
floxbot <technology@flox.dev>
2025-09-29 14:09:45 +0000

    build: Update catalog API schema and client to 5768416f

--- cli/catalog-api-v1/src/client.rs
+++ cli/catalog-api-v1/src/client.rs
@@ -304,7 +304,6 @@ pub mod types {
     ///  "type": "object",
     ///  "required": [
     ///    "attribute_path_ct",
-    ///    "catalogs",
     ///    "derivations_ct",
     ///    "latest_rev",
     ///    "pages_ct",
@@ -317,13 +316,6 @@ pub mod types {
     ///    "attribute_path_ct": {
     ///      "title": "Attribute Path Ct",
     ///      "type": "integer"
-    ///    },
-    ///    "catalogs": {
-    ///      "title": "Catalogs",
-    ///      "type": "array",
-    ///      "items": {
-    ///        "type": "string"
-    ///      }
     ///    },
     ///    "derivations_ct": {
     ///      "title": "Derivations Ct",
@@ -392,7 +384,6 @@ pub mod types {
     #[derive(::serde::Deserialize, ::serde::Serialize, Clone, Debug, PartialEq)]
     pub struct CatalogStatus {
         pub attribute_path_ct: i64,
-        pub catalogs: ::std::vec::Vec<::std::string::String>,
         pub derivations_ct: i64,
         pub latest_rev: ::chrono::DateTime<::chrono::offset::Utc>,
         #[serde(default, skip_serializing_if = "::std::option::Option::is_none")]
