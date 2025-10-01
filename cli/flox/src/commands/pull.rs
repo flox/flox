fffc5fd7a7fc7653fdad4bc0c99d42bd1991f11a
Yannik Sander <yannik@flox.dev>
2025-10-01 14:59:09 +0200

    chore: remove pull specific handling of metadata diversion
    `Pull::handle_error` is currently only used to format some errors
    arising from `Pull::pull_new_environment`.
    Handling the `Diverged` case 1. does not make sense in the context of
    pulling a new environment and 2. would not need to be different
    from the generic handling.

--- cli/flox/src/commands/pull.rs
+++ cli/flox/src/commands/pull.rs
@@ -523,10 +523,6 @@ impl Pull {
                     let message = "You do not have permission to pull this environment";
                     anyhow::Error::msg(message)
                 },
-                ManagedEnvironmentError::Diverged { .. } => {
-                    let message = "The environment has diverged from the remote version";
-                    anyhow::Error::msg(message)
-                },
                 ManagedEnvironmentError::UpstreamNotFound {
                     env_ref,
                     upstream: _,
