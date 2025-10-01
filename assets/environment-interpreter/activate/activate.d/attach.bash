a3aeed62ee1aafe2a83d5f7bd764a19b18407616
Dan Carley <dan@flox.dev>
2025-10-01 15:31:20 +0100

    fix(activate): Omit message on activate -- command
    We shouldn't print a message on `activate -- command` because it:
    
    1. tells you to use `exit` to leave an activation which you don't have
       an interactive shell for
    2. also tells you to use `exit` when using `flox/flox-direnv` which
       causes you to exit the entire shell rather than just the activation
    3. clobbers STDERR from the command that's being executed
    
    This was working correctly for "attach":
    
        flox [default term]
        (2) ~/demo/hello
        % flox activate
        ✅ You are now using the environment 'hello'.
        To stop using this environment, type 'exit'
    
        flox [hello default term]
        (3) ~/demo/hello
        % flox activate -- hello
        Hello, world!
    
    But not for "start":
    
        (2) ~/demo/hello
        % flox activate -- hello
        ✅ You are now using the environment 'hello'.
        To stop using this environment, type 'exit'
    
        Hello, world!
    
    Which we regressed when refactoring `start` into a function where `$#`
    is always non-zero. Fix this by detecting the mode earlier on and
    explicitly storing it in a local variable.
    
    I haven't add any new tests because it's pretty fiddly to capture the
    complete matrix of interactive x non-interactive x start x attach,
    whilst also asserting on the complete output when we have a lot of noise
    from the RC files and expect.

--- assets/environment-interpreter/activate/activate.d/attach.bash
+++ assets/environment-interpreter/activate/activate.d/attach.bash
@@ -3,15 +3,15 @@ _sed="@gnused@/bin/sed"
 attach() {
   _flox_activation_state_dir="${1?}"
   shift
+  _flox_shell_mode="${1?}"
+  shift
 
   "$_flox_activate_tracer" "$_activate_d/attach.bash" START
 
-  # If interactive and a command has not been passed, this is an interactive
-  # activate,
-  # and we print a message to the user
+  # Don't clobber STDERR or recommend 'exit' for non-interactive shells.
   # If inside a container, FLOX_ENV_DESCRIPTION won't be set, and we don't need to
   # print a message (although attach isn't reachable anyways)
-  if [ -t 1 ] && [ $# -eq 0 ] && [ -n "${FLOX_ENV_DESCRIPTION:-}" ]; then
+  if [ "${_flox_shell_mode}" = "interactive" ] && [ -n "${FLOX_ENV_DESCRIPTION:-}" ]; then
     echo "✅ Attached to existing activation of environment '$FLOX_ENV_DESCRIPTION'" >&2
     echo "To stop using this environment, type 'exit'" >&2
     echo >&2
