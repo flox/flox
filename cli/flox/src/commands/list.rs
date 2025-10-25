c1d22a66781377aa51bb9dc7e337e9c909d4de81
Swastik Patel <swastikpatel29@gmail.com>
2025-10-25 07:00:48 +0530

    refactor: revert lockfile changes, use the manifest descriptor

--- cli/flox/src/commands/list.rs
+++ cli/flox/src/commands/list.rs
@@ -173,18 +173,12 @@ impl List {
             };
 
             match p {
-                PackageToList::Catalog(_, p) => {
-                    let path_to_display = if let Some(catalog) = &p.catalog {
-                        format!("{}/{}", catalog, p.attr_path)
-                    } else {
-                        p.attr_path.clone()
-                    };
-
+                PackageToList::Catalog(descriptor, p) => {
                     writeln!(
                         &mut out,
                         "{id}: {path} ({version}{upgrade_available})",
                         id = p.install_id,
-                        path = path_to_display,
+                        path = descriptor.pkg_path,
                         version = p.version,
                     )?;
                 },
@@ -239,7 +233,7 @@ impl List {
             };
 
             let message = match package {
-                PackageToList::Catalog(_, locked) => {
+                PackageToList::Catalog(descriptor, locked) => {
                     let mut sorted_outputs = locked
                         .outputs
                         .keys()
@@ -250,12 +244,6 @@ impl List {
                         "[ ]".to_string()
                     } else {
                         format!("[ {} ]", sorted_outputs.join(", "))
-                    };
-
-                    let path_to_display = if let Some(catalog) = &locked.catalog {
-                        format!("{}/{}", catalog, locked.attr_path)
-                    } else {
-                        locked.attr_path.clone()
                     };
 
                     formatdoc! {"
@@ -272,7 +260,7 @@ impl List {
                         ",
                         name = &locked.install_id,
                         pname = &locked.pname,
-                        attr_path = path_to_display,
+                        attr_path = &descriptor.pkg_path,
                         priority = locked.priority,
                         version = &locked.version,
                         description = locked.description.as_deref().unwrap_or("N/A"),
