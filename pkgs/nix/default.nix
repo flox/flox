e42e0f6099c2c9df59e5b974fda3863cde531731
Yannik Sander <yannik@flox.dev>
2025-11-05 16:36:09 +0100

    build: drop `postFixup` hook for `nix`
    Upstream Nix has fully adopted meson to structure libraries
    and generate pkgconfig files for all linkable components.

--- pkgs/nix/default.nix
+++ pkgs/nix/default.nix
@@ -31,40 +31,6 @@ nixVersions."${nixVersion}".overrideAttrs (prev: {
     # E.g:
     # (builtins.path { path = ./patches/pr_<PR>_<description>.patch; })
   ];
-
-  postFixup = ''
-    # Generate a `sed' pattern to fix up public header `#includes'.
-    # All header names separated by '\|'.
-    _patt="$( find "$dev/include/nix" -type f -name '*.h*' -printf '%P\|'; )";
-    # Strip leading/trailing '\|'.
-    _patt="''${_patt%\\|}";
-    _patt="''${_patt#\\|}";
-    _patt="s,#include \+\"\($_patt\)\",#include <nix/\1>,";
-    # Perform the substitution.
-    find "$dev/include/nix" -type f -name '*.h*' -print                        \
-      |xargs sed -i                                                            \
-                 -e "$_patt"                                                   \
-                 -e 's,#include \+"\(nlohmann/json_fwd\.hpp\)",#include <\1>,';
-
-    # Fixup `pkg-config' files.
-    find "$dev" -type f -name '*.pc'                       \
-      |xargs sed -i -e 's,\(-I\''${includedir}\)/nix,\1,'  \
-                    -e 's,-I,-isystem ,';
-
-    # Create `nix-fetchers.pc'.
-    cat <<EOF > "$dev/lib/pkgconfig/nix-fetchers.pc"
-    prefix=$out
-    libdir=$out/lib
-    includedir=$dev/include
-
-    Name: Nix
-    Description: Nix Package Manager
-    Version: ${prev.version}
-    Requires: nix-store bdw-gc
-    Libs: -L\''${libdir} -lnixfetchers
-    Cflags: -isystem \''${includedir} -std=c++2a
-    EOF
-  '';
 })
 # ---------------------------------------------------------------------------- #
 #
