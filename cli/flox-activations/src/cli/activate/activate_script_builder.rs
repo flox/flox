11d940287b4d1275708d87439a9958b355eb2892
Matthew Kenigsberg <matthew@flox.dev>
2025-11-07 18:42:22 -0700

    refactor: split out activate_script_builder

---
+++ cli/flox-activations/src/cli/activate/activate_script_builder.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+use std::process::Command;
+
+use flox_core::activate::context::ActivateCtx;
+use flox_core::activate::vars::{FLOX_ACTIVE_ENVIRONMENTS_VAR, FLOX_RUNTIME_DIR_VAR};
+use flox_core::util::default_nix_env_vars;
+pub const FLOX_ENV_LOG_DIR_VAR: &str = "_FLOX_ENV_LOG_DIR";
+pub const FLOX_PROMPT_ENVIRONMENTS_VAR: &str = "FLOX_PROMPT_ENVIRONMENTS";
+/// This variable is used to communicate what socket to use to the activate
+/// script.
+pub const FLOX_SERVICES_SOCKET_VAR: &str = "_FLOX_SERVICES_SOCKET";
+
+pub const FLOX_SERVICES_TO_START_VAR: &str = "_FLOX_SERVICES_TO_START";
+pub const FLOX_ACTIVATE_START_SERVICES_VAR: &str = "FLOX_ACTIVATE_START_SERVICES";
+
+pub(super) fn assemble_command_for_activate_script(data: ActivateCtx) -> Command {
+    let mut exports = HashMap::from([
+        (FLOX_ACTIVE_ENVIRONMENTS_VAR, data.flox_active_environments),
+        ("FLOX_PROMPT_COLOR_1", data.prompt_color_1),
+        ("FLOX_PROMPT_COLOR_2", data.prompt_color_2),
+        // Set `FLOX_PROMPT_ENVIRONMENTS` to the constructed prompt string,
+        // which may be ""
+        (FLOX_PROMPT_ENVIRONMENTS_VAR, data.flox_prompt_environments),
+        ("_FLOX_SET_PROMPT", data.set_prompt.to_string()),
+        ("_FLOX_ACTIVATE_STORE_PATH", data.flox_activate_store_path),
+        (
+            // TODO: we should probably figure out a more consistent way to
+            // pass this since it's also passed for `flox build`
+            FLOX_RUNTIME_DIR_VAR,
+            data.flox_runtime_dir,
+        ),
+        ("_FLOX_ENV_CUDA_DETECTION", data.flox_env_cuda_detection),
+        (
+            FLOX_ACTIVATE_START_SERVICES_VAR,
+            data.flox_activate_start_services.to_string(),
+        ),
+    ]);
+    if let Some(log_dir) = data.flox_env_log_dir.as_ref() {
+        exports.insert(FLOX_ENV_LOG_DIR_VAR, log_dir.clone());
+    }
+    if let Some(socket_path) = data.flox_services_socket.as_ref() {
+        exports.insert(FLOX_SERVICES_SOCKET_VAR, socket_path.clone());
+    }
+    if let Some(services_to_start) = data.flox_services_to_start {
+        exports.insert(FLOX_SERVICES_TO_START_VAR, services_to_start);
+    }
+
+    exports.extend(default_nix_env_vars());
+
+    let activate_path = data.interpreter_path.join("activate_temporary");
+    let mut command = Command::new(activate_path);
+    command.envs(exports);
+
+    command.arg("--env").arg(&data.env);
+    if let Some(env_project) = data.env_project.as_ref() {
+        command
+            .arg("--env-project")
+            .arg(env_project.to_string_lossy().to_string());
+    }
+    command
+        .arg("--env-cache")
+        .arg(data.env_cache.to_string_lossy().to_string());
+    command.arg("--env-description").arg(data.env_description);
+
+    // Pass down the activation mode
+    command.arg("--mode").arg(data.mode);
+
+    if let Some(watchdog_bin) = data.watchdog_bin.as_ref() {
+        command
+            .arg("--watchdog")
+            .arg(watchdog_bin.to_string_lossy().to_string());
+    }
+
+    command.arg("--shell").arg(data.shell.exe_path());
+
+    command
+}
--- cli/flox-activations/src/cli/activate/mod.rs
+++ cli/flox-activations/src/cli/activate/mod.rs
@@ -1,15 +1,14 @@
-use std::collections::HashMap;
+mod activate_script_builder;
 use std::fs;
 use std::os::unix::process::CommandExt;
 use std::path::PathBuf;
 use std::process::Command;
 
+use activate_script_builder::assemble_command_for_activate_script;
 use anyhow::{Context, Result};
 use clap::Args;
 use flox_core::activate::context::{ActivateCtx, InvocationType};
-use flox_core::activate::vars::{FLOX_ACTIVE_ENVIRONMENTS_VAR, FLOX_RUNTIME_DIR_VAR};
 use flox_core::shell::ShellWithPath;
-use flox_core::util::default_nix_env_vars;
 use indoc::formatdoc;
 use itertools::Itertools;
 use log::debug;
@@ -25,15 +24,6 @@ pub struct ActivateArgs {
     #[arg(allow_hyphen_values = true)]
     pub cmd: Option<Vec<String>>,
 }
-
-pub const FLOX_ENV_LOG_DIR_VAR: &str = "_FLOX_ENV_LOG_DIR";
-pub const FLOX_PROMPT_ENVIRONMENTS_VAR: &str = "FLOX_PROMPT_ENVIRONMENTS";
-/// This variable is used to communicate what socket to use to the activate
-/// script.
-pub const FLOX_SERVICES_SOCKET_VAR: &str = "_FLOX_SERVICES_SOCKET";
-
-pub const FLOX_SERVICES_TO_START_VAR: &str = "_FLOX_SERVICES_TO_START";
-pub const FLOX_ACTIVATE_START_SERVICES_VAR: &str = "FLOX_ACTIVATE_START_SERVICES";
 
 impl ActivateArgs {
     pub fn handle(self) -> Result<(), anyhow::Error> {
@@ -75,7 +65,7 @@ impl ActivateArgs {
             .invocation_type
             .expect("invocation type should have been some");
 
-        let activate_script_command = Self::assemble_command_for_activate_script(data.clone());
+        let activate_script_command = assemble_command_for_activate_script(data.clone());
 
         // when output is not a tty, and no command is provided
         // we just print an activation script to stdout
@@ -166,69 +156,6 @@ impl ActivateArgs {
         print!("{script}");
 
         Ok(())
-    }
-
-    fn assemble_command_for_activate_script(data: ActivateCtx) -> Command {
-        let mut exports = HashMap::from([
-            (FLOX_ACTIVE_ENVIRONMENTS_VAR, data.flox_active_environments),
-            ("FLOX_PROMPT_COLOR_1", data.prompt_color_1),
-            ("FLOX_PROMPT_COLOR_2", data.prompt_color_2),
-            // Set `FLOX_PROMPT_ENVIRONMENTS` to the constructed prompt string,
-            // which may be ""
-            (FLOX_PROMPT_ENVIRONMENTS_VAR, data.flox_prompt_environments),
-            ("_FLOX_SET_PROMPT", data.set_prompt.to_string()),
-            ("_FLOX_ACTIVATE_STORE_PATH", data.flox_activate_store_path),
-            (
-                // TODO: we should probably figure out a more consistent way to
-                // pass this since it's also passed for `flox build`
-                FLOX_RUNTIME_DIR_VAR,
-                data.flox_runtime_dir,
-            ),
-            ("_FLOX_ENV_CUDA_DETECTION", data.flox_env_cuda_detection),
-            (
-                FLOX_ACTIVATE_START_SERVICES_VAR,
-                data.flox_activate_start_services.to_string(),
-            ),
-        ]);
-        if let Some(log_dir) = data.flox_env_log_dir.as_ref() {
-            exports.insert(FLOX_ENV_LOG_DIR_VAR, log_dir.clone());
-        }
-        if let Some(socket_path) = data.flox_services_socket.as_ref() {
-            exports.insert(FLOX_SERVICES_SOCKET_VAR, socket_path.clone());
-        }
-        if let Some(services_to_start) = data.flox_services_to_start {
-            exports.insert(FLOX_SERVICES_TO_START_VAR, services_to_start);
-        }
-
-        exports.extend(default_nix_env_vars());
-
-        let activate_path = data.interpreter_path.join("activate_temporary");
-        let mut command = Command::new(activate_path);
-        command.envs(exports);
-
-        command.arg("--env").arg(&data.env);
-        if let Some(env_project) = data.env_project.as_ref() {
-            command
-                .arg("--env-project")
-                .arg(env_project.to_string_lossy().to_string());
-        }
-        command
-            .arg("--env-cache")
-            .arg(data.env_cache.to_string_lossy().to_string());
-        command.arg("--env-description").arg(data.env_description);
-
-        // Pass down the activation mode
-        command.arg("--mode").arg(data.mode);
-
-        if let Some(watchdog_bin) = data.watchdog_bin.as_ref() {
-            command
-                .arg("--watchdog")
-                .arg(watchdog_bin.to_string_lossy().to_string());
-        }
-
-        command.arg("--shell").arg(data.shell.exe_path());
-
-        command
     }
 
     /// Quote run args so that words don't get split,
