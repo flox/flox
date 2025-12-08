0cd6dcfb199f2f1c72faf059b0b69c8c260cd5c9
Yannik Sander <yannik@flox.dev>
2025-12-08 14:00:31 +0100

    feat: add floxmeta lock acquisition
    Copies the lock acquisition logic from `ManagedEnvironment`
    since the lock specifically for the floxmeta creation
    which will move into this module in the following commits.

--- cli/flox-rust-sdk/src/models/environment/floxmeta_branch.rs
+++ cli/flox-rust-sdk/src/models/environment/floxmeta_branch.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use fslock::LockFile;
 use thiserror::Error;
 
 use crate::models::floxmeta::FloxMeta;
@@ -38,7 +41,38 @@ pub struct FloxmetaBranch {
 }
 
 #[derive(Debug, Error)]
-pub enum FloxmetaBranchError {}
+pub enum FloxmetaBranchError {
+    #[error("failed to create floxmeta directory")]
+    CreateFloxmetaDir(#[source] std::io::Error),
+
+    #[error("failed to lock floxmeta git repo")]
+    LockFloxmeta(#[source] fslock::Error),
+}
+
+impl FloxmetaBranch {}
+
+/// Acquire exclusive lock on floxmeta directory
+#[tracing::instrument(fields(
+    progress = "Waiting for lock to open or create Flox remote metadata"
+))]
+fn acquire_floxmeta_lock(floxmeta_dir: &Path) -> Result<LockFile, FloxmetaBranchError> {
+    let parent = floxmeta_dir.parent().expect("path is non-empty");
+    std::fs::create_dir_all(parent).map_err(FloxmetaBranchError::CreateFloxmetaDir)?;
+    // TODO: use with_extension once we update our rustc
+    let mut lock = LockFile::open(
+        &floxmeta_dir.with_file_name(
+            floxmeta_dir
+                .file_name()
+                .expect("path is non-empty")
+                .to_string_lossy()
+                .into_owned()
+                + ".lock",
+        ),
+    )
+    .map_err(FloxmetaBranchError::LockFloxmeta)?;
+    lock.lock().map_err(FloxmetaBranchError::LockFloxmeta)?;
+    Ok(lock)
+}
 
 #[cfg(test)]
 mod tests {
