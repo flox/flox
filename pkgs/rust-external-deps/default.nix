50ec71bbf4a63752819feef97d3e9bbd00359ee7
Yannik Sander <yannik@flox.dev>
2025-12-29 18:07:42 +0100

    build: use nix provided lld on x86_64-linux
    With rust 1.90.0 rustc uses lld to link binaries on linux x86_64
    (<https://github.com/rust-lang/rust/pull/140525>).
    However, unlike the nix provided `ld` used before
    (and still on other platforms), the lld shipping with rust
    is not adjusting the RPATH of the linked binaries.
    Thus on nixos, we start to see
    
    ```
    error while loading shared libraries: libssl.so.3: cannot open shared object file: No such file or directory
    ```
    
    As mentioned in the rust-lang PR
    
    > To opt out of using LLD, RUSTFLAGS="-Clinker-features=-lld"
    > would be used. To opt out of using rust-lld,
    > falling back to the LLD installed on the system
    > RUSTFLAGS="-Clink-self-contained=-linker" would be used.
    
    we set `-Clink-self-contained=-linker` and supply (the wrapped) lld
    via `llvmPackages.bintools`.

--- pkgs/rust-external-deps/default.nix
+++ pkgs/rust-external-deps/default.nix
@@ -5,33 +5,49 @@
   pkg-config,
   pkgsFor,
   rust-toolchain,
+  stdenv,
   flox-src,
+  llvmPackages,
 }:
 let
   # crane (<https://crane.dev/>) library for building rust packages
   craneLib = (inputs.crane.mkLib pkgsFor).overrideToolchain rust-toolchain.toolchain;
 
+  envs =
+    { }
+    // lib.optionalAttrs (stdenv.hostPlatform.system == "x86_64-linux") {
+      RUSTFLAGS = "-Clink-self-contained=-linker";
+    };
+
   # incremental build of third party crates
-  cargoDepsArtifacts = craneLib.buildDepsOnly {
-    pname = "flox-external-crates";
+  cargoDepsArtifacts =
+    craneLib.buildDepsOnly {
+      pname = "flox-external-crates";
 
-    # We don't want to version the 3rd party crates,
-    #.to avoid cache invalidation.
-    # In particular, we don't want to tie the version of the 3rd party crates
-    # to the version of the flox-cli, as that means that we would have to
-    # rebuild the 3rd party crates every time we update the flox-cli,
-    # even though the dependencies of the flox-cli haven't changed.
-    version = "unversioned";
+      # We don't want to version the 3rd party crates,
+      #.to avoid cache invalidation.
+      # In particular, we don't want to tie the version of the 3rd party crates
+      # to the version of the flox-cli, as that means that we would have to
+      # rebuild the 3rd party crates every time we update the flox-cli,
+      # even though the dependencies of the flox-cli haven't changed.
+      version = "unversioned";
 
-    src = craneLib.cleanCargoSource (craneLib.path flox-src);
+      src = craneLib.cleanCargoSource (craneLib.path flox-src);
 
-    # runtime dependencies of the dependent crates
-    buildInputs = [
-      # reqwest -> hyper -> openssl-sys
-      openssl.dev
-    ];
+      # runtime dependencies of the dependent crates
+      buildInputs = [
+        # reqwest -> hyper -> openssl-sys
+        openssl.dev
+      ];
 
-    nativeBuildInputs = [ pkg-config ];
-  };
+      nativeBuildInputs = [
+        pkg-config
+      ]
+      ++ lib.optional (stdenv.hostPlatform.system == "x86_64-linux") [ llvmPackages.bintools ];
+      passthru = {
+        inherit envs;
+      };
+    }
+    // envs;
 in
 cargoDepsArtifacts
