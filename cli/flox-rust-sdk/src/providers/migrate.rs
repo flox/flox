b8dcfdf6e3ce602c4c6724ba803698b34bc71320
Zach Mitchell <zmitchell@fastmail.com>
2026-01-08 11:31:36 -0700

    test: add failing migration tests

---
+++ cli/flox-rust-sdk/src/providers/migrate.rs
@@ -0,0 +1,89 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn detects_writable_local_env() {
+        todo!()
+    }
+
+    #[test]
+    fn detects_writable_remote_env() {
+        todo!()
+    }
+
+    #[test]
+    fn detects_writable_managed_env() {
+        todo!()
+    }
+
+    #[test]
+    fn error_when_manifest_is_v1_but_lockfile_contains_v2() {
+        todo!()
+    }
+
+    #[test]
+    fn writable_v1_env_reported_as_migratable() {
+        todo!()
+    }
+
+    #[test]
+    fn readonly_v1_env_reported_as_not_migratable() {
+        todo!()
+    }
+
+    #[test]
+    fn writable_v2_env_reported_as_no_migration_needed() {
+        todo!()
+    }
+
+    #[test]
+    fn readonly_v2_env_reported_as_no_migration_needed() {
+        todo!()
+    }
+
+    #[test]
+    fn identifies_package_that_needs_migration() {
+        todo!()
+    }
+
+    #[test]
+    fn identifies_package_that_doesnt_need_migration() {
+        todo!()
+    }
+
+    #[test]
+    fn migrated_package_contains_all_outputs() {
+        todo!()
+    }
+
+    #[test]
+    fn package_not_needing_migration_is_untouched() {
+        todo!()
+    }
+
+    #[test]
+    fn migration_updates_manifest_version() {
+        todo!()
+    }
+
+    #[test]
+    fn can_migrate_local_environment() {
+        todo!()
+    }
+
+    #[test]
+    fn can_migrate_remote_environment() {
+        todo!()
+    }
+
+    #[test]
+    fn can_migrate_managed_environment() {
+        todo!()
+    }
+
+    #[test]
+    fn migration_creates_new_generation_for_floxhub_env() {
+        todo!()
+    }
+}
