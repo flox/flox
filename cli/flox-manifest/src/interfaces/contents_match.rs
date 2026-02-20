56eaa977842ad5b4d65223363a4243452f4178ca
Zach Mitchell <zmitchell@fastmail.com>
2026-02-20 15:39:50 -0700

    fix: normalize TOML contents before comparing
    In some tests we use `version = 1` as a dummy manifest. This revealed
    an unexpected behavior in `toml_edit`. Apparently `DocumentMut` will
    append newlines "where necessary" when it converts the parsed document
    into a string. Those newlines may not be in the original source that
    was parsed, which causes comparisons (and roundtrips)
    to fail.
    
    To mitigate this, we normalize contents through `DocumentMut` before
    comparisons. This does introduce a failure path where the compared
    source wasn't previously parsed as TOML, but if it doesn't parse as
    TOML then it wasn't going to match the valid TOML manifest anyway, so
    this is acceptable.

--- cli/flox-manifest/src/interfaces/contents_match.rs
+++ cli/flox-manifest/src/interfaces/contents_match.rs
@@ -1,19 +1,42 @@
+use std::str::FromStr;
+
+use toml_edit::DocumentMut;
+
 use crate::{Manifest, Migrated, Validated};
 
 pub trait ContentsMatch {
     fn contents_match(&self, contents: impl AsRef<str>) -> bool;
 }
 
+// NOTE: DocumentMut::to_string adds a trailing newline for certain TOML
+//       items, so when comparing you need to normalize through DocumentMut,
+//       otherwise you'll false negatives on matches.
+
 impl ContentsMatch for Manifest<Validated> {
     fn contents_match(&self, contents: impl AsRef<str>) -> bool {
         let self_contents = self.inner.raw.to_string();
-        self_contents.as_str() == contents.as_ref()
+        let other_contents = DocumentMut::from_str(contents.as_ref());
+        other_contents.is_ok_and(|contents| contents.to_string() == self_contents)
     }
 }
 
 impl ContentsMatch for Manifest<Migrated> {
     fn contents_match(&self, contents: impl AsRef<str>) -> bool {
         let self_contents = self.inner.migrated_raw.to_string();
-        self_contents.as_str() == contents.as_ref()
+        let other_contents = DocumentMut::from_str(contents.as_ref());
+        other_contents.is_ok_and(|contents| contents.to_string() == self_contents)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prove_that_documentmut_doesnt_round_trip() {
+        let input = "version = 1";
+        let doc = DocumentMut::from_str(input).unwrap();
+        let output = doc.to_string();
+        assert_ne!(output.as_str(), input);
     }
 }
