803492806844ea0bca888456338a7f87b5f91da2
Yannik Sander <yannik@flox.dev>
2026-04-09 14:12:49 +0200

    refactor(nef): factored out the `fetch*Catalog` functions into a toplevel `let`
    @mkenigs pointed out rightfully that multiple levels of `let` bindings
    and reusing ambient bindings in place of function args becomes hard to read and reason about.
    
    I kept the `{}.${type}`match style to remain consistency with other moduels in the nef libs.

--- package-builder/nef/lib/instantiate.nix
+++ package-builder/nef/lib/instantiate.nix
@@ -1,4 +1,104 @@
 { lib }:
+let
+  fetchSource =
+    source:
+    let
+      lockedWithoutDir = builtins.removeAttrs source [ "dir" ];
+      sourceInfo = builtins.addErrorContext "while fetching source '${builtins.flakeRefToString lockedWithoutDir}'" (
+        builtins.fetchTree lockedWithoutDir
+      );
+    in
+    sourceInfo // lib.optionalAttrs (source ? dir) { inherit (source) dir; };
+
+  # {
+  #   hash = "sha256-/UmRJVt7XpE27LGxS2hgGKWsErTx1oe65jhwWNPsnYs=";
+  #   locked = {
+  #     lastModified = 1769623709;
+  #     ref = "refs/heads/main";
+  #     rev = "b59e1a5750b5714c88fb6a7f3232398107704f7b";
+  #     revCount = 4504;
+  #     type = "git";
+  #     url = "https://github.com/flox/flox";
+  #   };
+  #   original = {
+  #     type = "git";
+  #     url = "https://github.com/flox/flox";
+  #   };
+  #   storePath = "/nix/store/df0qd3gnkix513br8az06yrnspg28530-source";
+  #   type = "nix";
+  # }
+  fetchNixCatalog =
+    nixpkgs: lockedCatalogSpec:
+    let
+      sourceInfo = fetchSource lockedCatalogSpec.locked;
+    in
+    lib.nef.instantiate.instantiateFromSourceInfo {
+      inherit nixpkgs sourceInfo;
+
+    }
+    // {
+      inherit (lockedCatalogSpec) type;
+    };
+
+  # Fetch a floxhub based catalog
+  #
+  # {
+  #  packages = {
+  #    hello = {
+  #      source = {
+  #        ref = "refs/heads/main";
+  #        rev = "b59e1a5750b5714c88fb6a7f3232398107704f7b";
+  #        type = "git";
+  #        url = "https://github.com/flox/flox";
+  #      };
+  #      type = "package";
+  #    };
+  #    type = "package_set";
+  #  };
+  #  type = "floxhub";
+  # };
+  fetchFloxHubCatalog =
+    nixpkgs: lockedCatalogSpec:
+    let
+      # process a package node
+      processPackageNode =
+        path: lockedPackageSpec:
+        {
+          "nef" =
+            let
+              sourceInfo = fetchSource lockedPackageSpec.source;
+              instantiatedCatalog = lib.nef.instantiate.instantiateFromSourceInfo {
+                inherit nixpkgs sourceInfo;
+              };
+              instantiatedPackage = lib.getAttrFromPath path instantiatedCatalog.reflect.packages;
+            in
+            # Return the instantiated environment
+            # The catalog overlay will use .reflect.packages
+            instantiatedPackage;
+          "manifest" = throw "Manifest build type not supported in Nix expressions";
+        }
+        .${lockedPackageSpec.build_type};
+
+      # recurse into a package set
+      processPackageSetNode =
+        path: node: lib.mapAttrs (name: entry: processNode (path ++ [ name ]) entry) node.entries;
+
+      processNode =
+        path: node:
+        {
+          "package" = builtins.addErrorContext "while instantiating package '${lib.showAttrPath path}'" (
+            processPackageNode path node
+          );
+          "package_set" = processPackageSetNode path node;
+        }
+        .${node.type};
+    in
+    # Use mapAttrsRecursiveCond to process only package nodes
+    {
+      packages = processNode [ ] lockedCatalogSpec.packages;
+      inherit (lockedCatalogSpec) type;
+    };
+in
 {
 
   /**
@@ -69,100 +169,11 @@
   */
   instantiateCatalog =
     nixpkgs: lockedCatalogSpec:
-
-    # "catalogs": {
-    #   "foo": {
-    #     "hash": "sha256-/UmRJVt7XpE27LGxS2hgGKWsErTx1oe65jhwWNPsnYs=",
-    #     "locked": {
-    #       "lastModified": 1769623709,
-    #       "ref": "refs/heads/main",
-    #       "rev": "b59e1a5750b5714c88fb6a7f3232398107704f7b",
-    #       "revCount": 4504,
-    #       "type": "git",
-    #       "url": "https://github.com/flox/flox"
-    #     },
-    #     "original": {
-    #       "type": "git",
-    #       "url": "https://github.com/flox/flox"
-    #     },
-    #     "storePath": "/nix/store/df0qd3gnkix513br8az06yrnspg28530-source"
-    #   },
-    #   [...]
-    # }
-    let
-      fetchNixCatalog =
-        let
-          sourceInfo =
-            let
-              lockedWithoutDir = builtins.removeAttrs lockedCatalogSpec.locked [ "dir" ];
-              sourceInfo = builtins.addErrorContext "while fetching source '${builtins.flakeRefToString lockedWithoutDir}'" (
-                builtins.fetchTree lockedWithoutDir
-              );
-            in
-            sourceInfo
-            // lib.optionalAttrs (lockedCatalogSpec.locked ? dir) { inherit (lockedCatalogSpec.locked) dir; };
-        in
-        lib.nef.instantiate.instantiateFromSourceInfo {
-          inherit nixpkgs sourceInfo;
-        };
-      fetchFloxHubCatalog =
-        let
-          # Function to process a package node
-          processPackageNode =
-            path: lockedPackageSpec:
-            let
-              byBuildType = {
-                "nef" =
-                  let
-                    sourceInfo =
-                      let
-                        lockedWithoutDir = builtins.removeAttrs lockedPackageSpec.source [ "dir" ];
-                        sourceInfo = builtins.addErrorContext "while fetching source '${builtins.flakeRefToString lockedWithoutDir}'" (
-                          builtins.fetchTree lockedWithoutDir
-                        );
-                      in
-                      sourceInfo
-                      // lib.optionalAttrs (lockedPackageSpec.source ? dir) { inherit (lockedPackageSpec.source) dir; };
-
-                    instantiatedCatalog = lib.nef.instantiate.instantiateFromSourceInfo {
-                      inherit nixpkgs sourceInfo;
-                    };
-                    instantiatedPackage = lib.getAttrFromPath path instantiatedCatalog.reflect.packages;
-                  in
-                  # Return the instantiated environment
-                  # The catalog overlay will use .reflect.packages
-                  instantiatedPackage;
-                "manifest" = throw "Manifest build type not supported in Nix expressions";
-              };
-            in
-            byBuildType.${lockedPackageSpec.build_type};
-          processPackageSetNode =
-            path: node: lib.mapAttrs (name: entry: matchNode (path ++ [ name ]) entry) node.entries;
-          matchNode =
-            path: node:
-            {
-              "package" = builtins.addErrorContext "while instantiating package '${lib.showAttrPath path}'" (
-                processPackageNode path node
-              );
-              "package_set" = processPackageSetNode path node;
-            }
-            .${node.type};
-        in
-        # Use mapAttrsRecursiveCond to process only package nodes
-        {
-          packages = matchNode [ ] lockedCatalogSpec.packages;
-        };
-    in
     {
-      inherit (lockedCatalogSpec) type;
+      "nix" = fetchNixCatalog nixpkgs lockedCatalogSpec;
+      "floxhub" = fetchFloxHubCatalog nixpkgs lockedCatalogSpec;
     }
-    // (
-      {
-        "nix" = fetchNixCatalog;
-        "floxhub" = fetchFloxHubCatalog;
-      }
-      .${lockedCatalogSpec.type}
-    );
+    .${lockedCatalogSpec.type};
 
   /**
     Instantiate multiple catalogs in an attribute set, as provided in a catalog lock file.
