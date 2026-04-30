flox/flox

commit 7d8b503ccbbb71b2896a7c88d7627e8ca2fe33d7

fix(just): Allow quoted args
Using the fix from:

- https://just.systems/man/en/avoiding-argument-splitting.html#positional-arguments

I've worked around this for a long time by replacing the spaces with
dots to create a single argument regex but it's annoying and surprising
for other users.

Before:

    % just integ-tests -- -f 'flox list --config'
    …
    ~/projects/flox/flox/cli ~/projects/flox/flox
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.29s
    flox-cli-tests: Running test suite with:
      PROJECT_TESTS_DIR:        /Users/dcarley/projects/flox/flox/cli/tests
      bats                      /nix/store/nv7hpfmc5vg99s39z6m0dxh7wvj7gw04-bats-with-libraries-1.12.0/bin/bats
      bats options              --print-output-on-failure --verbose-run --timing --jobs 4 --no-parallelize-across-files -f flox list --config
      bats tests                /var/folders/8q/spckhr654cv4xrcv0fxsrlvc0000gn/T/nix-shell.yXueWu/flox-cli-tests-gjlP3k
    Error: Bad command line option '--config'
    Usage: bats [OPTIONS] <tests>
           bats [-h | -v]

After:

    % just integ-tests -- -f 'flox list --config'
    …
    ~/projects/flox/flox/cli ~/projects/flox/flox
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.18s
    flox-cli-tests: Running test suite with:
      PROJECT_TESTS_DIR:        /Users/dcarley/projects/flox/flox/cli/tests
      bats                      /nix/store/nv7hpfmc5vg99s39z6m0dxh7wvj7gw04-bats-with-libraries-1.12.0/bin/bats
      bats options              --print-output-on-failure --verbose-run --timing --jobs 4 --no-parallelize-across-files -f flox list --config
      bats tests                /var/folders/8q/spckhr654cv4xrcv0fxsrlvc0000gn/T/nix-shell.yXueWu/flox-cli-tests-xIVR7N
    …
    list.bats
     ✓ 'flox list --config' shows manifest content [1498]
     ✓ 'flox list --config' shows manifest content for composed environments [1332]
     ✓ 'flox list --config' shows notices about overrides [1666]

Justfile
@@ -10,6 +10,8 @@
 #
 # ---------------------------------------------------------------------------- #
 
+set positional-arguments
+
 nix_options := "--extra-experimental-features nix-command \
                 --extra-experimental-features flakes"
 INPUT_DATA := "${PWD}/test_data/input_data"
@@ -214,8 +216,7 @@ gen-unit-data-for-publish floxhub_repo_path force="":
 # Run the CLI integration test suite using locally built binaries
 # This is equivalent to the "local" jobs in CI.
 @integ-tests +bats_args="": build
-    flox-cli-tests \
-        {{bats_args}}
+    flox-cli-tests "$@"
 
 # Run the CLI integration test suite using Nix-built binaries
 # This is equivalent to the "remote" jobs in CI.
@@ -223,8 +224,7 @@ gen-unit-data-for-publish floxhub_repo_path force="":
     nix run \
         --accept-flake-config \
         --extra-experimental-features 'nix-command flakes' \
-        .#flox-cli-tests \
-        {{bats_args}}
+        .#flox-cli-tests -- "$@"
 
 @ut regex="" record="false":
     _FLOX_UNIT_TEST_RECORD={{record}} {{cargo_test_invocation}} {{regex}}

commit 00b2ff6df43540e7697ca9bee411f5c0ea172872

test: allow 0 args for Justfile targets
Fix running `just integ-tests` and `just nix-integ-tests` with 0
arguments.

The fix for quoting args to Just targets in
7d8b503ccbbb71b2896a7c88d7627e8ca2fe33d7
regressed passing 0 args.

just integ-tests and just nix-integ-tests currently fail with:
```
flox-cli-tests ERROR: Unrecognized arg(s) ''
Usage: /nix/store/irxm6iaxg10clmi6v58gbv6dkxlc5sv2-flox-cli-tests/bin/flox-cli-tests [--tests <TESTS_DIR>| -T <TESTS_DIR>]           [--watch | -W]           [--help | -h] -- [BATS ARGUMENTS]

Available options:
    -T, --tests          Path to folder of tests (Default: /Users/matthew/flox/cli/tests)
    -c, --ci-runner      Which runner this job is on, if any
    -W, --watch          Run tests in a continuous watch mode
    -h, --help           Prints help information
error: Recipe `integ-tests` failed on line 236 with exit code 1
```

After this fix, running with 0 args works:
just integ-tests
just nix-integ-tests

And the case fixed by that commit still works:
just integ-tests -- -f 'flox list --config'
just nix-integ-tests -- -f 'flox list --config'

Justfile
@@ -232,12 +232,12 @@ gen-unit-data-for-publish floxhub_repo_path force="":
 
 # Run the CLI integration test suite using locally built binaries
 # This is equivalent to the "local" jobs in CI.
-@integ-tests +bats_args="": build
+@integ-tests *bats_args: build
     flox-cli-tests "$@"
 
 # Run the CLI integration test suite using Nix-built binaries
 # This is equivalent to the "remote" jobs in CI.
-@nix-integ-tests +bats_args="":
+@nix-integ-tests *bats_args:
     nix run \
         --accept-flake-config \
         --extra-experimental-features 'nix-command flakes' \