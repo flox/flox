c66e6339b123b6ac35916f70c0819a19752391ec
Matthew Kenigsberg <matthew@flox.dev>
2026-05-21 13:59:56 -0600

    fixup! refactor(deactivate): make exit an alias of deactivate

--- cli/flox/src/commands/deactivate.rs
+++ cli/flox/src/commands/deactivate.rs
@@ -1,6 +1,6 @@
 use std::io::{BufWriter, stdout};
 
-use anyhow::{Result, bail};
+use anyhow::Result;
 use bpaf::Bpaf;
 use flox_rust_sdk::flox::Flox;
 use flox_rust_sdk::utils::FLOX_INTERPRETER;
@@ -21,9 +21,7 @@ pub struct Deactivate {
 impl Deactivate {
     pub fn handle(self, flox: Flox) -> Result<()> {
         if !flox.features.auto_activate {
-            bail!(
-                "'flox deactivate' requires the auto_activate feature flag. Set FLOX_FEATURES_AUTO_ACTIVATE=true."
-            );
+            return self.old_exit(flox);
         }
 
         subcommand_metric!("deactivate");
@@ -63,5 +61,28 @@ impl Deactivate {
 
             Ok(())
         }
+    }
+
+    pub fn old_exit(self, _flox: Flox) -> Result<()> {
+        subcommand_metric!("exit");
+
+        let active_environments = activated_environments();
+        let last_active = active_environments.last_active();
+
+        let Some(last_active) = last_active else {
+            message::info(indoc! {"
+                No environment active!
+                Exit active environments by typing 'exit' to exit your current shell or close your terminal.
+                Environments can be activated using `flox activate`.
+            "});
+
+            return Ok(());
+        };
+
+        message::info(formatdoc! {"
+            Exit the currently active environment {} by typing 'exit' to exit your current shell or close your terminal.
+        ", uninitialized_environment_description(&last_active)?});
+
+        Ok(())
     }
 }
